7432ca9515b824a04f31961dcccf7b8f4caa92e4
hikalkan <hi_kalkan@yahoo.com>
2013-08-30 00:30:15 +0300

    Drop & re-create database application
    Created a simple console application to drop & re-create database

--- src/ExamCenter/ExamCenter.Data/RunMigrations.bat
+++ src/ExamCenter/ExamCenter.Data/RunMigrations.bat
@@ -1,2 +1,1 @@
 ..\..\packages\FluentMigrator.1.1.1.0\tools\Migrate.exe /connection "Server=localhost; Database=ExamCenter; Trusted_Connection=True;" /db sqlserver /target "bin\Debug\ExamCenter.Data.dll"
-pause
