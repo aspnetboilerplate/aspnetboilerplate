50eb541de25f9878051a52c6b33d1cdbf827f64b
hikalkan <hi_kalkan@yahoo.com>
2013-08-29 21:14:26 +0300

    Fixed nuget packages.

--- src/Abp/Core/Abp.Core.Data/RunMigrations.bat
+++ src/Abp/Core/Abp.Core.Data/RunMigrations.bat
@@ -1,2 +1,2 @@
-..\..\..\packages\FluentMigrator.Tools.1.1.1.0\tools\AnyCPU\40\Migrate.exe /connection "Server=localhost; Database=ExamCenter; Trusted_Connection=True;" /db sqlserver /target "bin\Debug\Abp.Core.Data.dll"
+..\..\..\packages\FluentMigrator.1.1.1.0\tools\Migrate.exe /connection "Server=localhost; Database=ExamCenter; Trusted_Connection=True;" /db sqlserver /target "bin\Debug\Abp.Core.Data.dll"
 pause
