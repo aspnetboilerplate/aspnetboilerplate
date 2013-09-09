42b4214f6a1d56d6560be86f04cb3ef81e57cf84
hikalkan <hi_kalkan@yahoo.com>
2013-09-09 21:30:36 +0300

    Generating dynamic service

--- src/Abp/Framework/Abp.Web.Mvc/Controllers/Dyanmic/DynamicScriptGenerator.cs
+++ src/Abp/Framework/Abp.Web.Mvc/Controllers/Dyanmic/DynamicScriptGenerator.cs
@@ -12,7 +12,7 @@ namespace Abp.Web.Mvc.Controllers.Dyanmic
         public string GenerateFor(DynamicApiControllerInfo controllerInfo)
         {
             var script = new StringBuilder();
-            
+
             //Module dependencies and start
             script.AppendLine("define(['jquery', 'abp/abp'], function ($, abp) {");
             script.AppendLine();
@@ -26,7 +26,7 @@ namespace Abp.Web.Mvc.Controllers.Dyanmic
 
             //Return value of the module
             script.AppendLine("    return {");
-            
+
             var methodNo = 0;
             foreach (var methodInfo in controllerInfo.Methods.Values)
             {
@@ -44,30 +44,36 @@ namespace Abp.Web.Mvc.Controllers.Dyanmic
 
         private void AppendMethod(StringBuilder script, DynamicApiControllerInfo controllerInfo, DynamicApiMethodInfo methodInfo)
         {
-            script.AppendLine("    var " + methodInfo.Name.ToCamelCase() + " = function(" + GenerateMethodParams(methodInfo.Method) + ")");
+            script.AppendLine("    var " + methodInfo.Name.ToCamelCase() + " = function(" + GenerateMethodParams(methodInfo.Method) + ") {");
             script.AppendLine("        return abp.ajax({");
             script.AppendLine("            url: '/api/services/" + controllerInfo.Name.ToCamelCase() + "/" + methodInfo.Name.ToCamelCase() + "',");
             script.Append("            type: '" + methodInfo.Verb.ToString().ToUpper() + "'");
 
             var parameters = methodInfo.Method.GetParameters();
-            if (parameters.Length > 0 && methodInfo.Verb == HttpVerb.Get || methodInfo.Verb == HttpVerb.Delete)
+            if (parameters.Length > 0)
             {
                 script.AppendLine(",");
-                script.AppendLine("            data: {");
-
-                for (var i = 0; i < parameters.Length; i++)
+                if (methodInfo.Verb == HttpVerb.Get || methodInfo.Verb == HttpVerb.Delete)
                 {
-                    var parameterInfo = parameters[i];
-                    script.AppendLine("                " + parameterInfo.Name.ToCamelCase() + ": " + parameterInfo.Name.ToCamelCase() + (i < parameters.Length - 1 ? "," : ""));
-                }
+                    script.AppendLine("            data: {");
 
-                script.AppendLine("            }");
+                    for (var i = 0; i < parameters.Length; i++)
+                    {
+                        var parameterInfo = parameters[i];
+                        script.AppendLine("                " + parameterInfo.Name.ToCamelCase() + ": " + parameterInfo.Name.ToCamelCase() + (i < parameters.Length - 1 ? "," : ""));
+                    }
+
+                    script.AppendLine("            }");
+                }
+                else if (methodInfo.Verb == HttpVerb.Post || methodInfo.Verb == HttpVerb.Put)
+                {
+                    script.AppendLine("            data: JSON.stringify(" + parameters[0].Name.ToCamelCase() + ")");
+                }
             }
             else
             {
                 script.AppendLine();
             }
-
 
             script.AppendLine("        });");
             script.AppendLine("    };");
