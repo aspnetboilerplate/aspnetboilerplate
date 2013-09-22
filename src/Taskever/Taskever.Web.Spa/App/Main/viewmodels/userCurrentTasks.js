070c8d06fb30b0fb9aea0a62e9887780b14cb1d7
hikalkan <hi_kalkan@yahoo.com>
2013-09-22 18:01:59 +0300

    Developing Taskever

---
+++ src/Taskever/Taskever.Web.Spa/App/Main/viewmodels/userCurrentTasks.js
@@ -0,0 +1,14 @@
+﻿define(["jquery", "knockout", 'durandal/app', 'plugins/dialog', 'models/dtos', 'services/task'], function ($, ko, app, dialogs, dtos, taskService) {
+    var tasks = ko.mapping.fromJS([]);
+
+    return {
+        tasks: tasks,
+
+        activate: function () {
+            taskService.getMyTasks(tasks)
+                .then(function (data) {
+                    ko.mapping.fromJS(data, tasks);
+                });
+        }
+    };
+});
