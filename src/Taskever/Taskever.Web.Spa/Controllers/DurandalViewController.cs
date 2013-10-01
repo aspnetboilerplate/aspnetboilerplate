66dba6295813e3fb75e6ffca8dbb07e2e6c8c3fa
hikalkan <hi_kalkan@yahoo.com>
2013-10-01 21:29:59 +0300

    Working to create razor views for durandal views

---
+++ src/Taskever/Taskever.Web.Spa/Controllers/DurandalViewController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Taskever.Web.Controllers
+{
+    public class DurandalViewController : Controller
+    {
+        //
+        // GET: /DurandalViewController/
+
+        public ActionResult GetAppView(string module, string view)
+        {
+            return View("/App/" + module + "/views/" + view + ".cshtml");
+        }
+    }
+}
