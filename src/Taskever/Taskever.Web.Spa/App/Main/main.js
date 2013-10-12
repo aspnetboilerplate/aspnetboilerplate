00dd2c8999c4eddfcd02bf3fb04974d75c54466d
hikalkan <hi_kalkan@yahoo.com>
2013-10-12 22:41:12 +0300

    Trying to load cshtml files as durandal views

--- src/Taskever/Taskever.Web.Spa/App/Main/main.js
+++ src/Taskever/Taskever.Web.Spa/App/Main/main.js
@@ -13,7 +13,7 @@ requirejs.config({
 define('jquery', function () { return jQuery; });
 define('knockout', ko);
 
-define(['durandal/system', 'durandal/app', 'durandal/viewLocator'], function (system, app, viewLocator) {
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator', 'durandal/viewEngine'], function (system, app, viewLocator, viewEngine) {
     system.debug(true); //TODO: remove in production code
 
     app.title = 'Task Ever';
@@ -26,6 +26,11 @@ define(['durandal/system', 'durandal/app', 'durandal/viewLocator'], function (sy
 
     app.start().then(function () {
         viewLocator.useConvention();
+        viewEngine.convertViewIdToRequirePath = function (viewId) {
+            console.log(this.viewPlugin + '!/DurandalView/GetAppView?view=' + viewId + '.cshtml');
+            return this.viewPlugin + '!' + viewId + this.viewExtension;
+        };
+
         app.setRoot('viewmodels/layout', 'entrance');
     });
 });
