c1ab90dfada23459f5e3ea6222999cc9a43d5a6d
hikalkan <hi_kalkan@yahoo.com>
2013-12-27 23:16:53 +0200

    Create task merged with edit task!

--- src/Taskever/Taskever.Web.Spa/App/Main/viewmodels/layout.js
+++ src/Taskever/Taskever.Web.Spa/App/Main/viewmodels/layout.js
@@ -8,6 +8,7 @@
                 { route: '', title: 'Home', moduleId: 'viewmodels/home', nav: true },
                 { route: 'user/:id', title: 'User Profile', moduleId: 'viewmodels/user', nav: true },
                 { route: 'friends', title: 'My Friends', moduleId: 'viewmodels/friends', nav: true },
+                { route: 'task/create', moduleId: 'viewmodels/task/edit', nav: true },
                 { route: 'task/:id/edit', moduleId: 'viewmodels/task/edit', nav: true },
                 { route: 'task/:id', moduleId: 'viewmodels/task/view', nav: true },
                 { route: 'settings', moduleId: 'viewmodels/settings', nav: true }
--- src/Taskever/Taskever.Web.Spa/App/Main/viewmodels/task/edit.js
+++ src/Taskever/Taskever.Web.Spa/App/Main/viewmodels/task/edit.js
@@ -7,58 +7,96 @@
 
             // Private fields /////////////////////////////////////////////////////
 
-            var $view;
+            var _$view;
+            var _$form;
 
             // Public fields //////////////////////////////////////////////////////
 
-            that.task = ko.mapping.fromJS({});
+            that.task = null;
             that.users = ko.mapping.fromJS([]); //Users to assign the task
+            that.isEditMode = ko.observable(false);
 
             // Public methods /////////////////////////////////////////////////////
 
             that.canActivate = function (taskId) {
-                return $.Deferred(function (dfd) {
-                    taskService.getTask({
-                        id: taskId
-                    }).done(function (data) {
-                        ko.mapping.fromJS(data.task, that.task);
+                if (taskId) {
+                    that.task = ko.mapping.fromJS({});
+                    that.isEditMode(true);
+                    
+                    return $.Deferred(function (dfd) {
+                        taskService.getTask({
+                            id: taskId
+                        }).done(function (data) {
+                            ko.mapping.fromJS(data.task, that.task);
 
-                        that.task.title(decodeHtml(that.task.title()));
-                        that.task.description(decodeHtml(that.task.description()));
-                        that.task.privacyEditable = ko.observable(that.task.assignedUserId() == session.getCurrentUser().id());
-                        that.task.assignedUserId.subscribe(function (newValue) {
-                            if (newValue == session.getCurrentUser().id()) {
-                                that.task.privacyEditable(true);
-                            } else {
-                                that.task.privacyEditable(false);
-                                that.task.privacy(taskever.taskPrivacy.Protected);
-                            }
-                        });
+                            that.task.title(decodeHtml(that.task.title()));
+                            that.task.description(decodeHtml(that.task.description()));
+                            that.task.privacyEditable = ko.observable(that.task.assignedUserId() == session.getCurrentUser().id());
+                            that.task.assignedUserId.subscribe(function (newValue) {
+                                if (newValue == session.getCurrentUser().id()) {
+                                    that.task.privacyEditable(true);
+                                } else {
+                                    that.task.privacyEditable(false);
+                                    that.task.privacy(taskever.taskPrivacy.Protected);
+                                }
+                            });
 
-                    }).fail(function () {
-                        dfd.resolve(false);
-                    }).then(function () {
-                        friendshipService.getFriendships({
-                            userId: session.getCurrentUser().id(),
-                            canAssignTask: true
-                        }).done(function (result) {
-                            var users = $.map(result.friendships, function (friendship) { return friendship.friend; });
-                            ko.mapping.fromJS(users, that.users);
-                            that.users.unshift(session.getCurrentUser());
-                            dfd.resolve(true);
                         }).fail(function () {
                             dfd.resolve(false);
+                        }).then(function () {
+                            friendshipService.getFriendships({
+                                userId: session.getCurrentUser().id(),
+                                canAssignTask: true
+                            }).done(function (result) {
+                                var users = $.map(result.friendships, function (friendship) { return friendship.friend; });
+                                ko.mapping.fromJS(users, that.users);
+                                that.users.unshift(session.getCurrentUser());
+                                dfd.resolve(true);
+                            }).fail(function () {
+                                dfd.resolve(false);
+                            });
                         });
                     });
-                });
+                } else {
+                    
+                    that.task = {
+                        title: ko.observable(),
+                        description: ko.observable(),
+                        assignedUserId: ko.observable(session.getCurrentUser().id()),
+                        priority: ko.observable(taskever.taskPriority.Normal),
+                        privacy: ko.observable(taskever.taskPrivacy.Protected),
+                        privacyEditable: ko.observable(true)
+                    };
+
+                    that.task.assignedUserId.subscribe(function (newValue) {
+                        if (newValue == session.getCurrentUser().id()) {
+                            that.task.privacyEditable(true);
+                        } else {
+                            that.task.privacyEditable(false);
+                            that.task.privacy(taskever.taskPrivacy.Protected);
+                        }
+                    });
+                    
+                    return friendshipService.getFriendships({
+                        userId: session.getCurrentUser().id(),
+                        status: taskever.friendshipStatus.Accepted,
+                        canAssignTask: true
+                    }).done(function (result) {
+                        var users = $.map(result.friendships, function (friendship) { return friendship.friend; });
+                        ko.mapping.fromJS(users, that.users);
+                        that.users.unshift(session.getCurrentUser());
+                    });
+                }
             };
 
-            that.attached = function(view, parent) {
-                $view = $(view);
+            that.attached = function (view, parent) {
+                _$view = $(view);
+                _$form = _$view.find('form');
+                _$form.validate();
             };
 
             that.updateTask = function () {
-                abp.ui.setBusy($view, {
+                abp.ui.setBusy(_$view, {
                     promise: taskService.updateTask({
                         id: that.task.id(),
                         title: that.task.title(),
@@ -67,7 +105,7 @@
                         priority: that.task.priority(),
                         privacy: that.task.privacy(),
                         state: that.task.state(),
-                    }).done(function() {
+                    }).done(function () {
                         abp.notify.info('Task has been updated', 'Updated');
                         history.navigate('task/' + that.task.id());
                         app.trigger('te.task.update', {
@@ -81,8 +119,25 @@
                 history.navigateBack();
             };
             
+            that.saveNewTask = function () {
+                if (!_$form.valid()) {
+                    return;
+                }
+
+                abp.ui.setBusy(_$view, {
+                    promise: taskService.createTask({
+                        task: ko.mapping.toJS(that.task)
+                    }).done(function (result) {
+                        app.trigger('te.task.new', {
+                            task: ko.mapping.fromJS(result.task)
+                        });
+                        history.navigate('task/' + result.task.id);
+                    })
+                });
+            };
+
             //Private functions
-            
+
             //This method is copied from http://stackoverflow.com/questions/5796718/html-entity-decode
             //TODO: Replace to a nicer one?
             var decodeHtml = (function () {
