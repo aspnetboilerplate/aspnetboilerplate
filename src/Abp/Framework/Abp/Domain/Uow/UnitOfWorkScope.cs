c81f4ee9e963df50be6d44604d97109b0149a46d
hikalkan <hi_kalkan@yahoo.com>
2014-01-18 12:24:13 +0200

    Commited on unit of work

--- src/Abp/Framework/Abp/Domain/Uow/UnitOfWorkScope.cs
+++ src/Abp/Framework/Abp/Domain/Uow/UnitOfWorkScope.cs
@@ -4,12 +4,17 @@ using Abp.Dependency;
 namespace Abp.Domain.Uow
 {
     /// <summary>
-    /// This class is experimental for now. (TODO: Test & make it usable)
+    /// This class is used to create a manual unit of work scope.  
     /// </summary>
+    /// <remarks>
+    /// <see cref="UnitOfWorkAttribute"/> can be used to mark a method as unit of work. Then the marked method
+    /// will be a unit of work scope. But, somethimes you may want to restrict unit of work scope to a part of a method, 
+    /// then use this class.
+    /// </remarks>
     public class UnitOfWorkScope : IDisposable
     {
         /// <summary>
-        /// Gets current instance of the NhUnitOfWork.
+        /// Gets current <see cref="IUnitOfWork"/> instance.
         /// It gets the right instance that is related to current thread.
         /// </summary>
         public static IUnitOfWork Current
@@ -25,6 +30,9 @@ namespace Abp.Domain.Uow
 
         private bool _isCommited;
 
+        /// <summary>
+        /// Create a new unit of work scope.
+        /// </summary>
         public UnitOfWorkScope()
         {
             _unitOfWorkWrapper = IocHelper.ResolveAsDisposable<IUnitOfWork>();
@@ -39,12 +47,16 @@ namespace Abp.Domain.Uow
             }
         }
 
+        /// <summary>
+        /// Commits the unit of work.
+        /// If not commited, it's rolled back on <see cref="Dispose"/>.
+        /// </summary>
         public void Commit()
         {
             _unitOfWorkWrapper.Object.Commit();
             _isCommited = true;
         }
-
+        
         public void Dispose()
         {
             if (!_isCommited)
