171e672667b83393811043020db2c377d996f739
hikalkan <hi_kalkan@yahoo.com>
2014-01-18 15:46:21 +0200

    Refactored

---
+++ src/Abp/Framework/Abp.Web.Api/Controllers/Dynamic/Scripting/ServiceProxiesController.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using Abp.WebApi.Controllers.Dynamic.Formatters;
+
+namespace Abp.WebApi.Controllers.Dynamic.Scripting
+{
+    /// <summary>
+    /// This class is used to create proxies to call dynamic api methods from Javascript clients.
+    /// </summary>
+    public class ServiceProxiesController : AbpApiController
+    {
+        /// <summary>
+        /// Gets javascript proxy for given service name.
+        /// </summary>
+        /// <param name="name">Name of the service</param>
+        public HttpResponseMessage Get(string name)
+        {
+            var script = ScriptProxyManager.GetScript(name);
+            var response = Request.CreateResponse(System.Net.HttpStatusCode.OK, script, new PlainTextFormatter());
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/x-javascript");
+            return response;
+        }
+    }
+}
