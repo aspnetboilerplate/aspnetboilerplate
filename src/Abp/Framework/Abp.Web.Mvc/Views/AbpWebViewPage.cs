05f3035107669381ff51ff2c81eec7d7a9ba8dcd
hikalkan <hi_kalkan@yahoo.com>
2014-01-19 21:00:55 +0200

    Renamed

---
+++ src/Abp/Framework/Abp.Web.Mvc/Views/AbpWebViewPage.cs
@@ -0,0 +1,10 @@
+﻿namespace Abp.Web.Mvc.Views
+{
+    /// <summary>
+    /// Base class for all views in Abp system.
+    /// </summary>
+    public abstract class AbpWebViewPage : AbpWebViewPage<dynamic>
+    {
+
+    }
+}
