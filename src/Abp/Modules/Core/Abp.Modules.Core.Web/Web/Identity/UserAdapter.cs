790964a2a748de73920c01336de3ac8f91994150
hikalkan <hi_kalkan@yahoo.com>
2014-01-26 17:14:22 +0200

    Developing Asp.Net Identity

---
+++ src/Abp/Modules/Core/Abp.Modules.Core.Web/Web/Identity/UserAdapter.cs
@@ -0,0 +1,23 @@
+﻿using Abp.Users;
+using Microsoft.AspNet.Identity;
+
+namespace Abp.Web.Identity
+{
+    public class UserAdapter :IUser<int>
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public UserAdapter()
+        {
+            
+        }
+
+        public UserAdapter(User user)
+        {
+            Id = user.Id;
+            UserName = user.EmailAddress;
+        }
+    }
+}
---
+++ src/Abp/Modules/Core/Abp.Modules.Core.Web/Web/Identity/UserStoreAdapter.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Tasks;
+using Abp.Dependency;
+using Abp.Users;
+using Microsoft.AspNet.Identity;
+
+namespace Abp.Web.Identity
+{
+    public class UserStoreAdapter : IUserStore<UserAdapter, int>
+    {
+        private readonly IUserRepository _userRepository;
+
+        private bool _isDisposed;
+
+        public UserStoreAdapter(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public void Dispose()
+        {
+            if (_isDisposed)
+            {
+                return;
+            }
+
+            _isDisposed = true;
+            IocHelper.Release(this);
+        }
+
+        public Task CreateAsync(UserAdapter user)
+        {
+            return new Task(() => { });
+        }
+
+        public Task UpdateAsync(UserAdapter user)
+        {
+            return new Task(() => { });
+        }
+
+        public Task DeleteAsync(UserAdapter user)
+        {
+            return new Task(() => _userRepository.Delete(user.Id));
+        }
+
+        public Task<UserAdapter> FindByIdAsync(int userId)
+        {
+            return new Task<UserAdapter>(() => new UserAdapter(_userRepository.FirstOrDefault(userId)));
+        }
+
+        public Task<UserAdapter> FindByNameAsync(string userName)
+        {
+            return new Task<UserAdapter>(() => new UserAdapter(_userRepository.FirstOrDefault(u => u.EmailAddress == userName)));
+        }
+    }
+}
