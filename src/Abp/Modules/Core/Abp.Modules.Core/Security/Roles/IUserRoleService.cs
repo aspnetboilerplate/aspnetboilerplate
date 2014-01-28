062afcc0f7737887a215891b7d4eb2cf61684884
hikalkan <hi_kalkan@yahoo.com>
2014-01-29 00:48:16 +0200

    Added missing file

---
+++ src/Abp/Modules/Core/Abp.Modules.Core/Security/Roles/IUserRoleService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Abp.Domain.Services;
+using Abp.Security.Users;
+
+namespace Abp.Security.Roles
+{
+    public interface IUserRoleService : IDomainService
+    {
+        IReadOnlyCollection<Role> GetRolesOfUser(User user);
+    }
+}
