78c3663f4d6bbbcafcff88b494195b694d121fc7
hikalkan <hi_kalkan@yahoo.com>
2014-03-04 09:27:18 +0200

    Log4NetInstaller fix

--- src/Taskever/Taskever/Startup/Dependency/Installers/Log4NetInstaller.cs
+++ src/Taskever/Taskever/Startup/Dependency/Installers/Log4NetInstaller.cs
@@ -7,18 +7,9 @@ namespace Taskever.Startup.Dependency.Installers
 {
     public class Log4NetInstaller : IWindsorInstaller
     {
-        private static bool _isInstalled;
-
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
-            if (_isInstalled)
-            {
-                return;
-            }
-
             container.AddFacility<LoggingFacility>(f => f.UseLog4Net().WithConfig("log4net.config"));
-
-            _isInstalled = true;
         }
     }
 }
