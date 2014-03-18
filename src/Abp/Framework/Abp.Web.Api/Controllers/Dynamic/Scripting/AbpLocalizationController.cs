84a08fc77a8b2be9e17154e14171118bb8162ecd
hikalkan <hi_kalkan@yahoo.com>
2014-03-18 21:44:25 +0200

    Moved localization to web

--- src/Abp/Framework/Abp.Web.Api/Controllers/Dynamic/Scripting/AbpLocalizationController.cs
+++ src/Abp/Framework/Abp.Web.Api/Controllers/Dynamic/Scripting/AbpLocalizationController.cs
@@ -9,45 +9,45 @@ using Abp.WebApi.Controllers.Dynamic.Scripting.Localization;
 
 namespace Abp.WebApi.Controllers.Dynamic.Scripting
 {
-    /// <summary>
-    /// Used to get localization javascript from clients.
-    /// </summary>
-    public class AbpLocalizationController : AbpApiController
-    {
-        private readonly ILocalizationScriptManager _localizationScriptManager;
+    ///// <summary>
+    ///// Used to get localization javascript from clients.
+    ///// </summary>
+    //public class AbpLocalizationController : AbpApiController
+    //{
+    //    private readonly ILocalizationScriptManager _localizationScriptManager;
 
-        public AbpLocalizationController(ILocalizationScriptManager localizationScriptManager)
-        {
-            _localizationScriptManager = localizationScriptManager;
-        }
+    //    public AbpLocalizationController(ILocalizationScriptManager localizationScriptManager)
+    //    {
+    //        _localizationScriptManager = localizationScriptManager;
+    //    }
 
-        public HttpResponseMessage Get()
-        {
-            var script = _localizationScriptManager.GetLocalizationScript();
-            var response = Request.CreateResponse(HttpStatusCode.OK, script, new PlainTextFormatter());
-            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/x-javascript");
-            response.Content.Headers.ContentEncoding.Add("utf-8");
-            return response;
-        }
+    //    public HttpResponseMessage Get()
+    //    {
+    //        var script = _localizationScriptManager.GetLocalizationScript();
+    //        var response = Request.CreateResponse(HttpStatusCode.OK, script, new PlainTextFormatter());
+    //        response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/x-javascript");
+    //        response.Content.Headers.ContentEncoding.Add("utf-8");
+    //        return response;
+    //    }
 
-        [HttpPost]
-        public HttpResponseMessage ChangeLanguage(string lang)
-        {
-            if (!GlobalizationHelper.IsValidCultureCode(lang))
-            {
-                return new HttpResponseMessage(HttpStatusCode.BadRequest);
-            }
+    //    [HttpPost]
+    //    public HttpResponseMessage ChangeLanguage(string lang)
+    //    {
+    //        if (!GlobalizationHelper.IsValidCultureCode(lang))
+    //        {
+    //            return new HttpResponseMessage(HttpStatusCode.BadRequest);
+    //        }
 
-            var response = new HttpResponseMessage();
-            var cookie = new CookieHeaderValue("Abp.Localization.Language", lang)
-                         {
-                             Expires = DateTimeOffset.Now.AddYears(2),
-                             Domain = Request.RequestUri.Host,
-                             Path = "/"
-                         };
+    //        var response = new HttpResponseMessage();
+    //        var cookie = new CookieHeaderValue("Abp.Localization.Language", lang)
+    //                     {
+    //                         Expires = DateTimeOffset.Now.AddYears(2),
+    //                         Domain = Request.RequestUri.Host,
+    //                         Path = "/"
+    //                     };
 
-            response.Headers.AddCookies(new[] { cookie });
-            return response;
-        }
-    }
+    //        response.Headers.AddCookies(new[] { cookie });
+    //        return response;
+    //    }
+    //}
 }
---
+++ src/Abp/Framework/Abp.Web.Mvc/Controllers/Localization/AbpLocalizationController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Abp.Exceptions;
+using Abp.Utils.Helpers;
+using Abp.Web.Mvc.Models;
+using Abp.WebApi.Controllers.Dynamic.Scripting.Localization;
+
+namespace Abp.Web.Mvc.Controllers.Localization
+{
+    public class AbpLocalizationController : AbpController
+    {
+        private readonly ILocalizationScriptManager _localizationScriptManager;
+
+        public AbpLocalizationController(ILocalizationScriptManager localizationScriptManager)
+        {
+            _localizationScriptManager = localizationScriptManager;
+        }
+
+        public ContentResult GetScripts()
+        {
+            var script = _localizationScriptManager.GetLocalizationScript();
+            return Content(script, "application/x-javascript", Encoding.UTF8);
+        }
+
+        public ActionResult ChangeLanguage(string lang, string returnUrl = "")
+        {
+            if (!GlobalizationHelper.IsValidCultureCode(lang))
+            {
+                throw new AbpException("Unknown language: " + lang + ". It must be a valid culture!");
+            }
+
+            Response.Cookies.Add(new HttpCookie("Abp.Localization.Language", lang) { Expires = DateTime.Now.AddYears(2) });
+
+            if (Request.IsAjaxRequest())
+            {
+                return Json(new AbpMvcAjaxResponse(), JsonRequestBehavior.AllowGet);
+            }
+
+            if (!string.IsNullOrWhiteSpace(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+
+            return Redirect("/");
+        }
+    }
+}
