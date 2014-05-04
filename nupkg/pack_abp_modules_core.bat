79a80143e168d90db79a5dbf4f2d2d949f45bb7c
hikalkan <hi_kalkan@yahoo.com>
2014-05-04 22:23:10 +0300

    Added EF nuget packaging

--- nupkg/pack_abp_modules_core.bat
+++ nupkg/pack_abp_modules_core.bat
@@ -1,5 +1,6 @@
 "..\src\.nuget\NuGet.exe" "pack" "..\src\Abp\Modules\Core\Abp.Modules.Core\Abp.Modules.Core.csproj" -Properties Configuration=Release -IncludeReferencedProjects
 "..\src\.nuget\NuGet.exe" "pack" "..\src\Abp\Modules\Core\Abp.Modules.Core.Application\Abp.Modules.Core.Application.csproj" -Properties Configuration=Release -IncludeReferencedProjects
+"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp\Modules\Core\Abp.Modules.Core.Infrastructure.EntityFramework\Abp.Modules.Core.Infrastructure.EntityFramework.csproj" -Properties Configuration=Release -IncludeReferencedProjects
 "..\src\.nuget\NuGet.exe" "pack" "..\src\Abp\Modules\Core\Abp.Modules.Core.Infrastructure.NHibernate\Abp.Modules.Core.Infrastructure.NHibernate.csproj" -Properties Configuration=Release -IncludeReferencedProjects
 "..\src\.nuget\NuGet.exe" "pack" "..\src\Abp\Modules\Core\Abp.Modules.Core.Web\Abp.Modules.Core.Web.csproj" -Properties Configuration=Release -IncludeReferencedProjects
 "..\src\.nuget\NuGet.exe" "pack" "..\src\Abp\Modules\Core\Abp.Modules.Core.Web.Api\Abp.Modules.Core.Web.Api.csproj" -Properties Configuration=Release -IncludeReferencedProjects
