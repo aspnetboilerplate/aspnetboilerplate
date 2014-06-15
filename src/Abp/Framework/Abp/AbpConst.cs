b67a5d75a769f1232375b816a47d1df80fd222df
hikalkan <hi_kalkan@yahoo.com>
2014-06-15 20:39:10 +0300

    Updated to v0.2.3

--- src/Abp/Framework/Abp/AbpConst.cs
+++ src/Abp/Framework/Abp/AbpConst.cs
@@ -2,6 +2,6 @@ namespace Abp
 {
     internal static class AbpConst
     {
-        public const string CurrentVersion = "0.2.2.2";
+        public const string CurrentVersion = "0.2.3.0";
     }
 }
--- src/Abp/Modules/Core/Abp.Modules.Core/AbpCoreModuleConst.cs
+++ src/Abp/Modules/Core/Abp.Modules.Core/AbpCoreModuleConst.cs
@@ -2,6 +2,6 @@
 {
     internal static class AbpCoreModuleConst
     {
-        public const string CurrentVersion = "0.2.2.1";
+        public const string CurrentVersion = "0.2.3.0";
     }
 }
