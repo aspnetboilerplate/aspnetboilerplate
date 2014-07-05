a5f5056cd14766d397004580c278e7f8a88b2b62
hikalkan <hi_kalkan@yahoo.com>
2014-07-05 17:23:17 +0300

    Removed commented codes.

--- src/Abp/Framework/Abp.Infrastructure.EntityFramework/Domain/Repositories/EntityFramework/AbpDbContext.cs
+++ src/Abp/Framework/Abp.Infrastructure.EntityFramework/Domain/Repositories/EntityFramework/AbpDbContext.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Reflection;
+﻿using System.Data.Entity;
 using Abp.Dependency;
-using Abp.Domain.Entities;
 
 namespace Abp.Domain.Repositories.EntityFramework
 {
@@ -22,54 +17,5 @@ namespace Abp.Domain.Repositories.EntityFramework
         {
 
         }
-
-        //public static void AddEntityAssembly(Assembly assembly)
-        //{
-        //    if (_entityAssemblies.Contains(assembly))
-        //    {
-        //        throw new AbpException("This assembly is already added: " + assembly.FullName);
-        //    }
-
-        //    _entityAssemblies.Add(assembly);
-        //}
-
-        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        //{
-        //    base.OnModelCreating(modelBuilder);
-
-        //    foreach (var entityAssembly in _entityAssemblies)
-        //    {
-        //        var entityTypes = entityAssembly.GetTypes()
-        //            .Where(type => (!type.IsAbstract && IsAssignableToGenericType(type, typeof(IEntity<>))))
-        //            .ToList();
-
-        //        foreach (var entityType in entityTypes)
-        //        {
-        //            var method = modelBuilder.GetType().GetMethod("Entity");
-        //            method = method.MakeGenericMethod(new[] { entityType });
-        //            method.Invoke(modelBuilder, null);
-        //        }
-        //    }
-        //}
-
-        //TODO: This method can be used somewhere else
-        //private static bool IsAssignableToGenericType(Type givenType, Type genericType)
-        //{
-        //    var interfaceTypes = givenType.GetInterfaces();
-
-        //    foreach (var it in interfaceTypes)
-        //    {
-        //        if (it.IsGenericType && it.GetGenericTypeDefinition() == genericType)
-        //            return true;
-        //    }
-
-        //    if (givenType.IsGenericType && givenType.GetGenericTypeDefinition() == genericType)
-        //        return true;
-
-        //    Type baseType = givenType.BaseType;
-        //    if (baseType == null) return false;
-
-        //    return IsAssignableToGenericType(baseType, genericType);
-        //}
     }
 }
