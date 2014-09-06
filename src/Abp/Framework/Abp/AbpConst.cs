f9906c1a49af9c86625aa12a78a974b225cc9704
hikalkan <hi_kalkan@yahoo.com>
2014-09-06 12:15:00 +0300

    Updated version to 0.3.2.

--- src/Abp/Framework/Abp/AbpConst.cs
+++ src/Abp/Framework/Abp/AbpConst.cs
@@ -2,6 +2,6 @@ namespace Abp
 {
     internal static class AbpConst
     {
-        public const string CurrentVersion = "0.3.1.3";
+        public const string CurrentVersion = "0.3.2.0";
     }
 }
