d704982fd5d5dd3d2254d6d650942dd24fc0a3f8
hikalkan <hi_kalkan@yahoo.com>
2014-09-28 19:58:14 +0300

    Removed Abp.Application project dependencies and nuget packaging fixed.

--- nupkg/pack_abp_framework.bat
+++ nupkg/pack_abp_framework.bat
@@ -1,8 +1,9 @@
-"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp\Framework\Abp\Abp.csproj" -Properties Configuration=Release -IncludeReferencedProjects
-"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp\Framework\Abp.Application\Abp.Application.csproj" -Properties Configuration=Release -IncludeReferencedProjects
-"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp\Framework\Abp.Infrastructure.EntityFramework\Abp.Infrastructure.EntityFramework.csproj" -Properties Configuration=Release -IncludeReferencedProjects
-"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp\Framework\Abp.Infrastructure.NHibernate\Abp.Infrastructure.NHibernate.csproj" -Properties Configuration=Release -IncludeReferencedProjects
-"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp\Framework\Abp.Web\Abp.Web.csproj" -Properties Configuration=Release -IncludeReferencedProjects
-"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp\Framework\Abp.Web.Api\Abp.Web.Api.csproj" -Properties Configuration=Release -IncludeReferencedProjects
-"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp\Framework\Abp.Web.Mvc\Abp.Web.Mvc.csproj" -Properties Configuration=Release -IncludeReferencedProjects
-"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp\Framework\Abp.Web.Resources\Abp.Web.Resources.csproj" -Properties Configuration=Release -IncludeReferencedProjects
+"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp\Abp.csproj" -Properties Configuration=Release -IncludeReferencedProjects
+"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp.FluentMigrator\Abp.FluentMigrator.csproj" -Properties Configuration=Release -IncludeReferencedProjects
+"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp.EntityFramework\Abp.EntityFramework.csproj" -Properties Configuration=Release -IncludeReferencedProjects
+"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp.NHibernate\Abp.NHibernate.csproj" -Properties Configuration=Release -IncludeReferencedProjects
+"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp.Web\Abp.Web.csproj" -Properties Configuration=Release -IncludeReferencedProjects
+"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp.Web.Api\Abp.Web.Api.csproj" -Properties Configuration=Release -IncludeReferencedProjects
+"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp.Web.Mvc\Abp.Web.Mvc.csproj" -Properties Configuration=Release -IncludeReferencedProjects
+"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp.Web.Resources\Abp.Web.Resources.csproj" -Properties Configuration=Release -IncludeReferencedProjects
+pause
