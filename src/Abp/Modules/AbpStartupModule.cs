3e6c8f3007dd2e6e8e5f3728b7c2bbf5fb220137
hikalkan <hi_kalkan@yahoo.com>
2014-09-28 18:47:25 +0300

    Completely changed locations of projects. Changed package names. Resolved #110, #124, #125 and #127.

---
+++ src/Abp/Modules/AbpStartupModule.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using Abp.Dependency.Conventions;
+using Abp.Domain.Uow;
+using Abp.Events.Bus;
+using Abp.Startup.Application;
+
+namespace Abp.Modules
+{
+    public class AbpStartupModule : AbpModule
+    {
+        public override void PreInitialize()
+        {
+            IocManager.AddConventionalRegisterer(new BasicConventionalRegisterer());
+            UnitOfWorkRegistrer.Initialize(IocManager);
+            ApplicationLayerInterceptorRegisterer.Initialize(IocManager);
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            IocManager.IocContainer.Install(new EventBusInstaller(IocManager));
+
+            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly(),
+                new ConventionalRegistrationConfig
+                {
+                    InstallInstallers = false
+                });
+        }
+    }
+}
---
+++ src/Abp/Startup/Configuration/IAbpStartupConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Abp.Configuration;
+
+namespace Abp.Startup.Configuration
+{
+    /// <summary>
+    /// Used to configure ABP and modules on startup.
+    /// </summary>
+    public interface IAbpStartupConfiguration : IDictionaryBasedConfig
+    {
+        /// <summary>
+        /// Used to set localization configuration.
+        /// </summary>
+        IAbpLocalizationConfiguration Localization { get; }
+
+        /// <summary>
+        /// Used to configure modules.
+        /// Modules can write extension methods to <see cref="IAbpModuleConfigurations"/> to add module specific configurations.
+        /// </summary>
+        IAbpModuleConfigurations Modules { get; }
+    }
+}
