8dd409ed66a1e77247855edd4c8a39e190b62265
Jacques Snyman <jacques@jacquessnyman.co.za>
2014-09-30 08:28:45 +0200

    Added symbols to nuget packaging

--- nupkg/pack_abp_framework.bat
+++ nupkg/pack_abp_framework.bat
@@ -1,8 +1,8 @@
-"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp\Abp.csproj" -Properties Configuration=Release -IncludeReferencedProjects
-"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp.FluentMigrator\Abp.FluentMigrator.csproj" -Properties Configuration=Release -IncludeReferencedProjects
-"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp.EntityFramework\Abp.EntityFramework.csproj" -Properties Configuration=Release -IncludeReferencedProjects
-"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp.NHibernate\Abp.NHibernate.csproj" -Properties Configuration=Release -IncludeReferencedProjects
-"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp.Web\Abp.Web.csproj" -Properties Configuration=Release -IncludeReferencedProjects
-"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp.Web.Api\Abp.Web.Api.csproj" -Properties Configuration=Release -IncludeReferencedProjects
-"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp.Web.Mvc\Abp.Web.Mvc.csproj" -Properties Configuration=Release -IncludeReferencedProjects
-"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp.Web.Resources\Abp.Web.Resources.csproj" -Properties Configuration=Release -IncludeReferencedProjects
+"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp\Abp.csproj" -Properties Configuration=Release -IncludeReferencedProjects -Symbols
+"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp.FluentMigrator\Abp.FluentMigrator.csproj" -Properties Configuration=Release -IncludeReferencedProjects -Symbols
+"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp.EntityFramework\Abp.EntityFramework.csproj" -Properties Configuration=Release -IncludeReferencedProjects -Symbols
+"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp.NHibernate\Abp.NHibernate.csproj" -Properties Configuration=Release -IncludeReferencedProjects -Symbols
+"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp.Web\Abp.Web.csproj" -Properties Configuration=Release -IncludeReferencedProjects -Symbols
+"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp.Web.Api\Abp.Web.Api.csproj" -Properties Configuration=Release -IncludeReferencedProjects -Symbols
+"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp.Web.Mvc\Abp.Web.Mvc.csproj" -Properties Configuration=Release -IncludeReferencedProjects -Symbols
+"..\src\.nuget\NuGet.exe" "pack" "..\src\Abp.Web.Resources\Abp.Web.Resources.csproj" -Properties Configuration=Release -IncludeReferencedProjects -Symbols
