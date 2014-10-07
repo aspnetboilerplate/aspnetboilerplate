ce8413bd6ef7981b7a2b17a496e0befac72c8615
hikalkan <hi_kalkan@yahoo.com>
2014-10-07 13:47:51 +0300

    Registering Dependency Injection for IRepository<TEntity> for all entities as default. Resolved #90.

--- src/Abp.EntityFramework/EntityFramework/AbpEntityFrameworkModule.cs
+++ src/Abp.EntityFramework/EntityFramework/AbpEntityFrameworkModule.cs
@@ -1,7 +1,10 @@
 ﻿using System.Data.Entity.Infrastructure.Interception;
+using System.Linq;
 using System.Reflection;
+using Abp.EntityFramework.Repositories;
 using Abp.EntityFramework.SoftDeleting;
 using Abp.Modules;
+using Abp.Reflection;
 
 namespace Abp.EntityFramework
 {
@@ -10,10 +13,33 @@ namespace Abp.EntityFramework
     /// </summary>
     public class AbpEntityFrameworkModule : AbpModule
     {
+        public IAssemblyFinder AssemblyFinder { private get; set; }
+
+        public AbpEntityFrameworkModule()
+        {
+            AssemblyFinder = DefaultAssemblyFinder.Instance;
+        }
+
         public override void Initialize()
         {
             IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
             DbInterception.Add(new SoftDeleteInterceptor());
+            RegisterGenericRepositories();
+        }
+
+        private void RegisterGenericRepositories()
+        {
+            var dbContextTypes = (
+                from assembly in AssemblyFinder.GetAllAssemblies()
+                from type in assembly.GetTypes()
+                where type.IsPublic && !type.IsAbstract && type.IsClass && typeof (AbpDbContext).IsAssignableFrom(type)
+                select type
+                ).ToList();
+
+            foreach (var dbContextType in dbContextTypes)
+            {
+                EntityFrameworkGenericRepositoryRegistrar.RegisterDbContext(dbContextType, IocManager);
+            }
         }
     }
 }
