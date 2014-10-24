1553235b03b97b002f8d2d950961f0ff914c2586
hikalkan <hi_kalkan@yahoo.com>
2014-10-24 14:46:35 +0300

    Added NameValueDto.

---
+++ src/Abp/Application/Services/Dto/NameValueDto.cs
@@ -0,0 +1,35 @@
+namespace Abp.Application.Services.Dto
+{
+    /// <summary>
+    /// Can be used to send/receive Name/Value pairs.
+    /// </summary>
+    public class NameValueDto : IDto
+    {
+        /// <summary>
+        /// Name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Value.
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Creates a new <see cref="NameValueDto"/>.
+        /// </summary>
+        public NameValueDto()
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="NameValueDto"/>.
+        /// </summary>
+        public NameValueDto(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+}
