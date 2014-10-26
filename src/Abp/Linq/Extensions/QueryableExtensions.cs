4cf83785046084194d03f15408ed9a8628b8dc7b
hikalkan <hi_kalkan@yahoo.com>
2014-10-26 12:31:27 +0200

    Moved SortBy to Abp package with named MultipleOrderBy. Revised #109. Resolved #122.

--- src/Abp/Linq/Extensions/QueryableExtensions.cs
+++ src/Abp/Linq/Extensions/QueryableExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
+using System.Linq.Expressions;
 using Abp.Application.Services.Dto;
 
 namespace Abp.Linq.Extensions
@@ -32,5 +34,76 @@ namespace Abp.Linq.Extensions
         {
             return query.PageBy(pagedResultRequest.SkipCount, pagedResultRequest.MaxResultCount);
         }
+
+        /// <summary>
+        /// Used to order given queryable by multiple sorting directions.
+        /// Some sorting samples:
+        /// "Name"
+        /// "Name ASC"
+        /// "Name, Age DESC"
+        /// "Name ASC,Age"
+        /// ASC and DESC are optional and case-insensitive.
+        /// Adjacent spaces of commas (,) are ignored.
+        /// sorting can be null or empty string. In this case, query is not ordered.
+        /// </summary>
+        public static IQueryable<T> MultipleOrderBy<T>(this IQueryable<T> query, string sorting)
+        {
+            if (query == null)
+            {
+                throw new ArgumentNullException("query");
+            }
+
+            if (string.IsNullOrWhiteSpace(sorting))
+            {
+                return query;
+            }
+
+            var orderingProps = sorting.Split(',').Select(s => s.Trim()).ToList();
+
+            query = query.SingleOrderBy(orderingProps[0], "OrderBy");
+
+            for (var i = 1; i < orderingProps.Count; i++)
+            {
+                query = query.SingleOrderBy(orderingProps[i], "ThenBy");
+            }
+
+            return query;
+        }
+
+        #region Private methods
+
+        private static IQueryable<T> SingleOrderBy<T>(this IQueryable<T> query, string sorting, string orderMethodPrefix)
+        {
+            var itemType = typeof(T);
+            var sortingParts = sorting.Split(' ');
+
+            var sortingPropName = sortingParts[0];
+            var sortingProp = itemType.GetProperty(sortingPropName);
+            if (sortingProp == null)
+            {
+                throw new ArgumentException(string.Format("No property '{0}' on type '{1}'", sortingPropName, itemType.Name));
+            }
+
+            var isDescending = (sortingParts.Length > 1) && sortingParts[1].ToUpper(CultureInfo.InvariantCulture).EndsWith("DESC");
+
+            var parameter = Expression.Parameter(itemType);
+            var propExpress = Expression.Property(parameter, sortingProp);
+            var lambdaBuilder = typeof(Expression)
+                .GetMethods()
+                .First(x => x.Name == "Lambda" && x.ContainsGenericParameters && x.GetParameters().Length == 2)
+                .MakeGenericMethod(typeof(Func<,>).MakeGenericType(itemType, sortingProp.PropertyType));
+
+            var sortLambda = lambdaBuilder
+                .Invoke(null, new object[] { propExpress, new[] { parameter } });
+
+            var sorter = typeof(Queryable)
+                .GetMethods()
+                .FirstOrDefault(x => x.Name == (orderMethodPrefix + (isDescending ? "Descending" : "")) && x.GetParameters().Length == 2)
+                .MakeGenericMethod(new[] { itemType, sortingProp.PropertyType });
+
+            return (IQueryable<T>)sorter.Invoke(null, new[] { query, sortLambda });
+        }
+
+        #endregion
     }
 }
