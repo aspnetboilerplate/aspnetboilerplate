8fc7b1b82377bb6c45ab899a3f9dfc065c0dd068
Adriano Marcondes Machado <adriano.m.machado@hotmail.com>
2014-11-26 12:35:39 -0200

    Added Async Entity Framework repository.

---
+++ src/Abp/Domain/Repositories/IAsyncRepository.cs
@@ -0,0 +1,12 @@
+﻿using Abp.Dependency;
+
+namespace Abp.Domain.Repositories
+{
+    /// <summary>
+    /// This interface must be implemented by all async repositories to identify them by convention.
+    /// Implement generic version instead of this one.
+    /// </summary>
+    public interface IAsyncRepository : ITransientDependency
+    {
+    }
+}
---
+++ src/Abp/Domain/Repositories/IAsyncRepositoryOfTEntity.cs
@@ -0,0 +1,12 @@
+﻿using Abp.Domain.Entities;
+
+namespace Abp.Domain.Repositories
+{
+    /// <summary>
+    /// A shortcut of <see cref="IAsyncRepository{TEntity,TPrimaryKey}"/> for most used primary key type (<see cref="int"/>).
+    /// </summary>
+    /// <typeparam name="TEntity">Entity type</typeparam>
+    public interface IAsyncRepository<TEntity> : IAsyncRepository<TEntity, int> where TEntity : class, IEntity<int>
+    {
+    }
+}
