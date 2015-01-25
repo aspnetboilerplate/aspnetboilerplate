0df8c70d5efb12ab000f7aeb3765e4102726171d
hikalkan <hi_kalkan@yahoo.com>
2015-01-25 12:38:04 +0200

    Moved to its own file.

--- src/Abp/Authorization/IPermissionChecker.cs
+++ src/Abp/Authorization/IPermissionChecker.cs
@@ -1,5 +1,4 @@
 using System.Threading.Tasks;
-using Abp.Threading;
 
 namespace Abp.Authorization
 {
@@ -20,32 +19,5 @@ namespace Abp.Authorization
         /// <param name="userId">Id of the user to check</param>
         /// <param name="permissionName">Name of the permission</param>
         Task<bool> IsGrantedAsync(long userId, string permissionName);
-    }
-
-    /// <summary>
-    /// Extension methods for <see cref="IPermissionChecker"/>
-    /// </summary>
-    public static class PermissionCheckerExtensions
-    {
-        /// <summary>
-        /// Checks if current user is granted for a permission.
-        /// </summary>
-        /// <param name="permissionChecker">Permission checker</param>
-        /// <param name="permissionName">Name of the permission</param>
-        public static bool IsGranted(this IPermissionChecker permissionChecker, string permissionName)
-        {
-            return AsyncHelper.RunSync(() => permissionChecker.IsGrantedAsync(permissionName));
-        }
-
-        /// <summary>
-        /// Checks if a user is granted for a permission.
-        /// </summary>
-        /// <param name="permissionChecker">Permission checker</param>
-        /// <param name="userId">Id of the user to check</param>
-        /// <param name="permissionName">Name of the permission</param>
-        public static bool IsGranted(this IPermissionChecker permissionChecker, long userId, string permissionName)
-        {
-            return AsyncHelper.RunSync(() => permissionChecker.IsGrantedAsync(userId, permissionName));
-        }
     }
 }
