df24beee1bb3e514cde15896afd6afc55f5d80ab
hikalkan <hi_kalkan@yahoo.com>
2015-01-30 01:53:02 +0200

    Added first base test class.

---
+++ src/TestBase/Abp.TestBase/TestBase/AbpIntegratedTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Abp.Dependency;
+using Abp.TestBase.Runtime.Session;
+
+namespace Abp.TestBase
+{
+    public abstract class AbpIntegratedTest
+    {
+        public IIocManager LocalIocManager { get; private set; }
+
+        public TestAbpSession AbpSession { get; set; }
+
+        protected AbpIntegratedTest()
+        {
+            LocalIocManager = new IocManager();
+
+            LocalIocManager.AddConventionalRegistrar(new BasicConventionalRegistrar());
+
+            LocalIocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+
+            AbpSession = LocalIocManager.Resolve<TestAbpSession>();
+        }
+    }
+}
