cd9cc89d5919bea92f841dbd9f22d88342005840
hikalkan <hi_kalkan@yahoo.com>
2015-02-02 18:29:02 +0200

    Refactored tests.

--- src/TestBase/Tests/Abp.TestBase.Tests/Application/Services/Validation_Tests.cs
+++ src/TestBase/Tests/Abp.TestBase.Tests/Application/Services/Validation_Tests.cs
@@ -1,15 +1,18 @@
-﻿using Abp.Dependency;
+﻿using System.ComponentModel.DataAnnotations;
+using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using Abp.Dependency;
 using Abp.Runtime.Validation;
 using Shouldly;
 using Xunit;
 
 namespace Abp.TestBase.Tests.Application.Services
 {
-    public class MyAppService_Tests : AbpIntegratedTestBase
+    public class Validation_Tests : AbpIntegratedTestBase
     {
         private readonly IMyAppService _myAppService;
 
-        public MyAppService_Tests()
+        public Validation_Tests()
         {
             LocalIocManager.Register<IMyAppService, MyAppService>(DependencyLifeStyle.Transient);
             _myAppService = LocalIocManager.Resolve<IMyAppService>();
@@ -28,5 +31,34 @@ namespace Abp.TestBase.Tests.Application.Services
             Assert.Throws<AbpValidationException>(() => _myAppService.MyMethod(new MyMethodInput())); //MyStringValue is not supplied!
             Assert.Throws<AbpValidationException>(() => _myAppService.MyMethod(new MyMethodInput { MyStringValue = "a" })); //MyStringValue's min length should be 3!
         }
+
+        #region Nested Classes
+
+        public interface IMyAppService
+        {
+            MyMethodOutput MyMethod(MyMethodInput input);
+        }
+
+        public class MyAppService : IMyAppService, IApplicationService
+        {
+            public MyMethodOutput MyMethod(MyMethodInput input)
+            {
+                return new MyMethodOutput { Result = 42 };
+            }
+        }
+
+        public class MyMethodInput : IInputDto
+        {
+            [Required]
+            [MinLength(3)]
+            public string MyStringValue { get; set; }
+        }
+
+        public class MyMethodOutput : IOutputDto
+        {
+            public int Result { get; set; }
+        }
+
+        #endregion
     }
 }
