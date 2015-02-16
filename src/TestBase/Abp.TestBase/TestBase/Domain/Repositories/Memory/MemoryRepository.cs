9da08f74f84b17fac825dfad6350d16aa1151da6
hikalkan <hi_kalkan@yahoo.com>
2015-02-16 14:34:51 +0200

    Creating a memory repository.

---
+++ src/TestBase/Abp.TestBase/TestBase/Domain/Repositories/Memory/MemoryRepository.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Abp.Domain.Entities;
+using Abp.Domain.Repositories;
+
+namespace Abp.TestBase.Domain.Repositories.Memory
+{
+    //TODO: Implement thread-safety..?
+    public class MemoryRepository<TEntity, TPrimaryKey> : IRepository<TEntity, TPrimaryKey>
+        where TEntity : class, IEntity<TPrimaryKey>
+    {
+        protected MemoryDatabase Database { get; private set; }
+
+        public List<TEntity> Table { get { return Database.Set<TEntity>(); } }
+
+        public MemoryRepository(MemoryDatabase database)
+        {
+            Database = database;
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return Table.AsQueryable();
+        }
+
+        public List<TEntity> GetAllList()
+        {
+            return GetAll().ToList();
+        }
+
+        public Task<List<TEntity>> GetAllListAsync()
+        {
+            return Task.FromResult(GetAllList());
+        }
+
+        public List<TEntity> GetAllList(Expression<Func<TEntity, bool>> predicate)
+        {
+            return GetAll().Where(predicate).ToList();
+        }
+
+        public Task<List<TEntity>> GetAllListAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Task.FromResult(GetAllList(predicate));
+        }
+
+        public T Query<T>(Func<IQueryable<TEntity>, T> queryMethod)
+        {
+            return queryMethod(GetAll());
+        }
+
+        public TEntity Get(TPrimaryKey id)
+        {
+            var entity = FirstOrDefault(id);
+            if (entity == null)
+            {
+                throw new AbpException("There is no such an entity with given primary key. Entity type: " + typeof(TEntity).FullName + ", primary key: " + id);
+            }
+
+            return entity;
+        }
+
+        public async Task<TEntity> GetAsync(TPrimaryKey id)
+        {
+            var entity = await FirstOrDefaultAsync(id);
+            if (entity == null)
+            {
+                throw new AbpException("There is no such an entity with given primary key. Entity type: " + typeof(TEntity).FullName + ", primary key: " + id);
+            }
+
+            return entity;
+        }
+
+        public virtual TEntity Single(Expression<Func<TEntity, bool>> predicate)
+        {
+            return GetAll().Single(predicate);
+        }
+
+        public virtual Task<TEntity> SingleAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Task.FromResult(Single(predicate));
+        }
+
+        public virtual TEntity FirstOrDefault(TPrimaryKey id)
+        {
+            return GetAll()
+                .FirstOrDefault(CreateEqualityExpression(id));
+        }
+
+        public virtual Task<TEntity> FirstOrDefaultAsync(TPrimaryKey id)
+        {
+            return Task.FromResult(FirstOrDefault(id));
+        }
+
+        public virtual TEntity FirstOrDefault(Expression<Func<TEntity, bool>> predicate)
+        {
+            return GetAll().FirstOrDefault(predicate);
+        }
+
+        public virtual Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Task.FromResult(FirstOrDefault(predicate));
+        }
+
+        public TEntity Load(TPrimaryKey id)
+        {
+            return Get(id);
+        }
+
+        public TEntity Insert(TEntity entity)
+        {
+            Table.Add(entity); //TODO: Generate Id!
+            return entity;
+        }
+
+        public Task<TEntity> InsertAsync(TEntity entity)
+        {
+            return Task.FromResult(Insert(entity));
+        }
+
+        public virtual TPrimaryKey InsertAndGetId(TEntity entity)
+        {
+            Insert(entity);
+            return entity.Id;
+        }
+
+        public virtual Task<TPrimaryKey> InsertAndGetIdAsync(TEntity entity)
+        {
+            return Task.FromResult(InsertAndGetId(entity));
+        }
+
+        public virtual TEntity InsertOrUpdate(TEntity entity)
+        {
+            return EqualityComparer<TPrimaryKey>.Default.Equals(entity.Id, default(TPrimaryKey))
+                ? Insert(entity)
+                : Update(entity);
+        }
+
+        public virtual async Task<TEntity> InsertOrUpdateAsync(TEntity entity)
+        {
+            return EqualityComparer<TPrimaryKey>.Default.Equals(entity.Id, default(TPrimaryKey))
+                ? await InsertAsync(entity)
+                : await UpdateAsync(entity);
+        }
+
+        public virtual TPrimaryKey InsertOrUpdateAndGetId(TEntity entity)
+        {
+            entity = InsertOrUpdate(entity);
+            return entity.Id;
+        }
+
+        public virtual Task<TPrimaryKey> InsertOrUpdateAndGetIdAsync(TEntity entity)
+        {
+            return Task.FromResult(InsertOrUpdateAndGetId(entity));
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            var index = Table.FindIndex(e => EqualityComparer<TPrimaryKey>.Default.Equals(e.Id, entity.Id));
+            if (index >= 0)
+            {
+                Table[index] = entity;
+            }
+
+            return entity;
+        }
+
+        public Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            return Task.FromResult(Update(entity));
+        }
+
+        public void Delete(TEntity entity)
+        {
+            Delete(entity.Id);
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            Delete(entity);
+        }
+
+        public void Delete(TPrimaryKey id)
+        {
+            var index = Table.FindIndex(e => EqualityComparer<TPrimaryKey>.Default.Equals(e.Id, id));
+            if (index >= 0)
+            {
+                Table.RemoveAt(index);
+            }
+        }
+
+        public async Task DeleteAsync(TPrimaryKey id)
+        {
+            Delete(id);
+        }
+
+        public void Delete(Expression<Func<TEntity, bool>> predicate)
+        {
+            foreach (var entity in GetAll().Where(predicate).ToList())
+            {
+                Delete(entity);
+            }
+        }
+
+        public async Task DeleteAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            Delete(predicate);
+        }
+
+        public int Count()
+        {
+            return Table.Count;
+        }
+
+        public Task<int> CountAsync()
+        {
+            return Task.FromResult(Count());
+        }
+
+        public int Count(Expression<Func<TEntity, bool>> predicate)
+        {
+            return GetAll().Count(predicate);
+        }
+
+        public Task<int> CountAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Task.FromResult(Count(predicate));
+        }
+
+        public long LongCount()
+        {
+            return Table.LongCount();
+        }
+
+        public Task<long> LongCountAsync()
+        {
+            return Task.FromResult(LongCount());
+        }
+
+        public long LongCount(Expression<Func<TEntity, bool>> predicate)
+        {
+            return GetAll().LongCount(predicate);            
+        }
+
+        public Task<long> LongCountAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Task.FromResult(LongCount(predicate));
+        }
+
+        private static Expression<Func<TEntity, bool>> CreateEqualityExpression(TPrimaryKey id)
+        {
+            var lambdaParam = Expression.Parameter(typeof(TEntity));
+
+            var lambdaBody = Expression.Equal(
+                Expression.PropertyOrField(lambdaParam, "Id"),
+                Expression.Constant(id, typeof(TPrimaryKey))
+                );
+
+            return Expression.Lambda<Func<TEntity, bool>>(lambdaBody, lambdaParam);
+        }
+    }
+}
