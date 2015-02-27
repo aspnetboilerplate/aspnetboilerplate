2dda5913ef204484444e1638f7f7c1a0e110aaff
hikalkan <hi_kalkan@yahoo.com>
2015-02-27 18:58:42 +0200

    Incremented version to publish nuget package.

--- src/Abp/AbpConsts.cs
+++ src/Abp/AbpConsts.cs
@@ -8,6 +8,6 @@ namespace Abp
         /// <summary>
         /// Current version of the ABP.
         /// </summary>
-        public const string CurrentVersion = "0.5.3.0";
+        public const string CurrentVersion = "0.5.4.0";
     }
 }
