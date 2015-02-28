6f44dd0f6b0c43ce3ed301571682235fe75822ff
hikalkan <hi_kalkan@yahoo.com>
2015-02-28 22:42:54 +0200

    Added AbpSessionExtensions. Resolved #363.

---
+++ src/Abp/Runtime/Session/AbpSessionExtensions.cs
@@ -0,0 +1,41 @@
+﻿namespace Abp.Runtime.Session
+{
+    /// <summary>
+    /// Extension methods for <see cref="IAbpSession"/>.
+    /// </summary>
+    public static class AbpSessionExtensions
+    {
+        /// <summary>
+        /// Gets current User's Id.
+        /// Throws <see cref="AbpException"/> if <see cref="IAbpSession.UserId"/> is null.
+        /// </summary>
+        /// <param name="session">Session object.</param>
+        /// <returns>Current User's Id.</returns>
+        public static long GetUserId(this IAbpSession session)
+        {
+            if (!session.UserId.HasValue)
+            {
+                throw new AbpException("Session.UserId is null! Probably, user is not logged in.");
+            }
+
+            return session.UserId.Value;
+        }
+
+        /// <summary>
+        /// Gets current Tenant's Id.
+        /// Throws <see cref="AbpException"/> if <see cref="IAbpSession.TenantId"/> is null.
+        /// </summary>
+        /// <param name="session">Session object.</param>
+        /// <returns>Current Tenant's Id.</returns>
+        /// <exception cref="AbpException"></exception>
+        public static int GetTenantId(this IAbpSession session)
+        {
+            if (!session.TenantId.HasValue)
+            {
+                throw new AbpException("Session.TenantId is null! Possible problems: User is not logged in, current user in not tenant user or this is not a multi-tenant application.");
+            }
+
+            return session.TenantId.Value;
+        }
+    }
+}
