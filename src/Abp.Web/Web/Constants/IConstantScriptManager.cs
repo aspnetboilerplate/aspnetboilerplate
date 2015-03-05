a72324acb76dd215926bde93e7a91fba2494e125
jefftindall <github@jefftindall.com>
2015-03-05 13:42:24 -0500

    PublishAttribute can be used to mark enums and classes to be eligible for publishing. CosntantScriptManager is used to generate JavaScript objects of the published classes and enums. For enum types, the JavaScript object includes the constant values as well as an Array of names, values, and localized string keys to be used for drop down lists or other list type controls.

---
+++ src/Abp.Web/Web/Constants/IConstantScriptManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abp.Web.Constants
+{
+    /// <summary>
+    /// Define interface to get constants and enumerations injected into a script
+    /// </summary>
+    public interface IConstantScriptManager
+    {
+        /// <summary>
+        /// Gets Javascript that contains setting values.
+        /// </summary>
+        string GetScript();
+    }
+}
