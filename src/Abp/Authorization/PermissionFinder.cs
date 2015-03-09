c4e1b1d9d0dee43681936c2e683a73414b843ce6
hikalkan <hi_kalkan@yahoo.com>
2015-03-09 16:39:11 +0200

    Commented AuthorizationProvider

--- src/Abp/Authorization/PermissionFinder.cs
+++ src/Abp/Authorization/PermissionFinder.cs
@@ -6,16 +6,24 @@ namespace Abp.Authorization
 {
     /// <summary>
     /// This class is used to get permissions out of the system.
-    /// Normally, you should inject and use <see cref="IPermissionManager"/>.
-    /// But this class can be used in database migrations or in unit tests where Abp is not initialized.
+    /// Normally, you should inject and use <see cref="IPermissionManager"/> and use it.
+    /// This class can be used in database migrations or in unit tests where Abp is not initialized.
     /// </summary>
     public static class PermissionFinder
     {
         /// <summary>
         /// Collects and gets all permissions in given providers.
+        /// This method can be used to get permissions in database migrations or in unit tests where Abp is not initialized.
+        /// Otherwise, use <see cref="IPermissionManager.GetAllPermissions"/> method.
+        /// 
         /// </summary>
         /// <param name="providerTypes">Providers</param>
         /// <returns>List of permissions</returns>
+        /// <remarks>
+        /// This method creates instances of <see cref="providerTypes"/> by order and
+        /// calls <see cref="AuthorizationProvider.SetPermissions"/> to build permission list.
+        /// So, providers should not use dependency injection.
+        /// </remarks>
         public static IReadOnlyList<Permission> GetAllPermissions(params Type[] providerTypes)
         {
             return new InternalPermissionFinder(providerTypes).GetAllPermissions();
