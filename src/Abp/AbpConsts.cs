246c316144dcafc871c3e7efe61a96ea2d36cfdc
hikalkan <hi_kalkan@yahoo.com>
2015-03-18 19:24:21 +0200

    Increased version and updated change logs.

--- src/Abp/AbpConsts.cs
+++ src/Abp/AbpConsts.cs
@@ -8,6 +8,6 @@ namespace Abp
         /// <summary>
         /// Current version of the ABP.
         /// </summary>
-        public const string CurrentVersion = "0.5.7.1";
+        public const string CurrentVersion = "0.5.8.0";
     }
 }
