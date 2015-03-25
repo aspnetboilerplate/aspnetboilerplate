a384fa3ff1638e753816a97d0ed31233161e95df
hikalkan <hi_kalkan@yahoo.com>
2015-03-25 15:40:35 +0200

    Added IAuditInfoPopulator, not used yet #420.

--- src/Abp/Auditing/IAuditingStore.cs
+++ src/Abp/Auditing/IAuditingStore.cs
@@ -1,7 +1,22 @@
 ﻿namespace Abp.Auditing
 {
+    /// <summary>
+    /// This interface should be implemented by vendors to
+    /// make auditing working.
+    /// Default implementation is <see cref="SimpleLogAuditingStore"/>.
+    /// </summary>
     public interface IAuditingStore
     {
+        /// <summary>
+        /// Should save audits to a persistent store.
+        /// </summary>
+        /// <param name="auditInfo">Audit informations</param>
         void Save(AuditInfo auditInfo);
+    }
+
+    //TODO: Working on that
+    public interface IAuditInfoPopulator
+    {
+        void Populate(AuditInfo auditInfo);
     }
 }
--- src/Abp/Auditing/SimpleLogAuditingStore.cs
+++ src/Abp/Auditing/SimpleLogAuditingStore.cs
@@ -2,6 +2,9 @@
 
 namespace Abp.Auditing
 {
+    /// <summary>
+    /// Implements <see cref="IAuditingStore"/> to simply write audits to logs.
+    /// </summary>
     internal class SimpleLogAuditingStore : IAuditingStore
     {
         public ILogger Logger { get; set; }
