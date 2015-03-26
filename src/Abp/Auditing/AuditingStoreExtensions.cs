f1b9f56144aeffdfdf5ff38de51025247c7229c1
hikalkan <hi_kalkan@yahoo.com>
2015-03-26 21:04:59 +0200

    Added auditing store extensions.

---
+++ src/Abp/Auditing/AuditingStoreExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Abp.Threading;
+
+namespace Abp.Auditing
+{
+    /// <summary>
+    /// Extension methods for <see cref="IAuditingStore"/>.
+    /// </summary>
+    public static class AuditingStoreExtensions
+    {
+        /// <summary>
+        /// Should save audits to a persistent store.
+        /// </summary>
+        /// <param name="auditingStore">Auditing store</param>
+        /// <param name="auditInfo">Audit informations</param>
+        public static void Save(this IAuditingStore auditingStore, AuditInfo auditInfo)
+        {
+            AsyncHelper.RunSync(() => auditingStore.SaveAsync(auditInfo));
+        }
+    }
+}
