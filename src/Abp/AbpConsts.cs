811a260aa957733bca96c7d07b946b424a2c0e12
hikalkan <hi_kalkan@yahoo.com>
2015-04-02 19:54:41 +0300

    Multi-tenancy filters are disabled if no user logged in.

--- src/Abp/AbpConsts.cs
+++ src/Abp/AbpConsts.cs
@@ -8,6 +8,6 @@ namespace Abp
         /// <summary>
         /// Current version of the ABP.
         /// </summary>
-        public const string CurrentVersion = "0.5.10.1";
+        public const string CurrentVersion = "0.5.10.2";
     }
 }
