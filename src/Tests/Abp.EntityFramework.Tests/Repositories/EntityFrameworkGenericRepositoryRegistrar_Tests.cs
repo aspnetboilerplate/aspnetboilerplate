93da5c2d93d186d2075bd416dd13ea1256b02a7c
hikalkan <hi_kalkan@yahoo.com>
2015-07-15 15:02:52 +0300

    Enhanced EntityFrameworkGenericRepositoryRegistrar test to reflect inheritance.

--- src/Tests/Abp.EntityFramework.Tests/Repositories/EntityFrameworkGenericRepositoryRegistrar_Tests.cs
+++ src/Tests/Abp.EntityFramework.Tests/Repositories/EntityFrameworkGenericRepositoryRegistrar_Tests.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using Abp.Domain.Entities;
 using Abp.Domain.Repositories;
 using Abp.EntityFramework.Repositories;
@@ -11,30 +12,48 @@ namespace Abp.EntityFramework.Tests.Repositories
 {
     public class EntityFrameworkGenericRepositoryRegistrar_Tests : TestBaseWithLocalIocManager
     {
+        public EntityFrameworkGenericRepositoryRegistrar_Tests()
+        {
+            var fakeMainDbContextProvider = NSubstitute.Substitute.For<IDbContextProvider<MyMainDbContext>>();
+            var fakeModuleDbContextProvider = NSubstitute.Substitute.For<IDbContextProvider<MyModuleDbContext>>();
+
+            LocalIocManager.IocContainer.Register(
+                Component.For<IDbContextProvider<MyMainDbContext>>().UsingFactoryMethod(() => fakeMainDbContextProvider),
+                Component.For<IDbContextProvider<MyModuleDbContext>>().UsingFactoryMethod(() => fakeModuleDbContextProvider)
+                );
+
+            EntityFrameworkGenericRepositoryRegistrar.RegisterForDbContext(typeof(MyMainDbContext), LocalIocManager);
+            EntityFrameworkGenericRepositoryRegistrar.RegisterForDbContext(typeof(MyModuleDbContext), LocalIocManager);
+        }
+
         [Fact]
         public void Should_Resolve_Generic_Repositories()
         {
-            var fakeDbContextProvider = NSubstitute.Substitute.For<IDbContextProvider<MyDbContext>>();
-
-            LocalIocManager.IocContainer.Register(
-                Component.For<IDbContextProvider<MyDbContext>>().UsingFactoryMethod(() => fakeDbContextProvider)
-                );
-
-            EntityFrameworkGenericRepositoryRegistrar.RegisterForDbContext(typeof(MyDbContext), LocalIocManager);
-
             var entity1Repository = LocalIocManager.Resolve<IRepository<MyEntity1>>();
             entity1Repository.ShouldNotBe(null);
+            (entity1Repository is EfRepositoryBase<MyMainDbContext, MyEntity1>).ShouldBe(true);
 
             var entity1RepositoryWithPk = LocalIocManager.Resolve<IRepository<MyEntity1, int>>();
             entity1RepositoryWithPk.ShouldNotBe(null);
+            (entity1RepositoryWithPk is EfRepositoryBase<MyMainDbContext, MyEntity1, int>).ShouldBe(true);
 
             var entity2Repository = LocalIocManager.Resolve<IRepository<MyEntity2, long>>();
+            (entity2Repository is EfRepositoryBase<MyMainDbContext, MyEntity2, long>).ShouldBe(true);
             entity2Repository.ShouldNotBe(null);
+
+            var entity3Repository = LocalIocManager.Resolve<IRepository<MyEntity3, Guid>>();
+            (entity3Repository is EfRepositoryBase<MyModuleDbContext, MyEntity3, Guid>).ShouldBe(true);
+            entity3Repository.ShouldNotBe(null);
         }
 
-        public class MyDbContext : MyBaseDbContext
+        public class MyMainDbContext : MyBaseDbContext
         {
             public DbSet<MyEntity2> MyEntities2 { get; set; }
+        }
+
+        public class MyModuleDbContext : MyBaseDbContext
+        {
+            public DbSet<MyEntity3> MyEntities3 { get; set; }
         }
 
         public abstract class MyBaseDbContext : AbpDbContext
@@ -48,6 +67,11 @@ namespace Abp.EntityFramework.Tests.Repositories
         }
 
         public class MyEntity2 : Entity<long>
+        {
+
+        }
+
+        public class MyEntity3 : Entity<Guid>
         {
 
         }
