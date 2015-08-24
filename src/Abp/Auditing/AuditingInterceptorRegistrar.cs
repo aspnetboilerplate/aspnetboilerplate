0a32b360b0e006bf9c053aa1a81f1132587895d8
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2015-08-24 11:57:48 +0300

    Fix for previous minor refactoring.

--- src/Abp/Auditing/AuditingInterceptorRegistrar.cs
+++ src/Abp/Auditing/AuditingInterceptorRegistrar.cs
@@ -8,13 +8,13 @@ namespace Abp.Auditing
 {
     internal class AuditingInterceptorRegistrar
     {
-        private readonly IAuditingConfiguration _auditingConfiguration;
         private readonly IIocManager _iocManager;
+        private readonly IAuditingConfiguration _auditingConfiguration;
 
         public AuditingInterceptorRegistrar(IIocManager iocManager)
         {
-            _auditingConfiguration = _iocManager.Resolve<IAuditingConfiguration>();
             _iocManager = iocManager;
+            _auditingConfiguration = _iocManager.Resolve<IAuditingConfiguration>();
         }
 
         public void Initialize()
