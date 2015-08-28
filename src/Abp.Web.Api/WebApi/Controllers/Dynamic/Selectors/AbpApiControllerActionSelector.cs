4ced36399de2cdfdfca9d253ec2af1a03e831d4f
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2015-08-28 17:44:39 +0300

    Minor refactoring.

--- src/Abp.Web.Api/WebApi/Controllers/Dynamic/Selectors/AbpApiControllerActionSelector.cs
+++ src/Abp.Web.Api/WebApi/Controllers/Dynamic/Selectors/AbpApiControllerActionSelector.cs
@@ -16,32 +16,34 @@ namespace Abp.WebApi.Controllers.Dynamic.Selectors
         public override HttpActionDescriptor SelectAction(HttpControllerContext controllerContext)
         {
             object controllerInfoObj;
-            if (controllerContext.ControllerDescriptor.Properties.TryGetValue("__AbpDynamicApiControllerInfo", out  controllerInfoObj))
+            if (!controllerContext.ControllerDescriptor.Properties.TryGetValue("__AbpDynamicApiControllerInfo", out controllerInfoObj))
             {
-                //Get controller information which is selected by AbpHttpControllerSelector.
-                var controllerInfo = controllerInfoObj as DynamicApiControllerInfo;
-                if (controllerInfo == null)
-                {
-                    throw new AbpException("__AbpDynamicApiControllerInfo in ControllerDescriptor.Properties is not a " + typeof(DynamicApiControllerInfo).FullName + " class.");
-                }
-
-                //Get action name
-                var serviceNameWithAction = (controllerContext.RouteData.Values["serviceNameWithAction"] as string);
-                if (serviceNameWithAction != null)
-                {
-                    var actionName = DynamicApiServiceNameHelper.GetActionNameInServiceNameWithAction(serviceNameWithAction);
-
-                    //Get action information
-                    if (!controllerInfo.Actions.ContainsKey(actionName))
-                    {
-                        throw new AbpException("There is no action " + actionName + " defined for api controller " + controllerInfo.ServiceName);
-                    }
-
-                    return new DyanamicHttpActionDescriptor(controllerContext.ControllerDescriptor, controllerInfo.Actions[actionName].Method, controllerInfo.Actions[actionName].Filters);
-                }
+                return base.SelectAction(controllerContext);
             }
-            
-            return base.SelectAction(controllerContext);
+
+            //Get controller information which is selected by AbpHttpControllerSelector.
+            var controllerInfo = controllerInfoObj as DynamicApiControllerInfo;
+            if (controllerInfo == null)
+            {
+                throw new AbpException("__AbpDynamicApiControllerInfo in ControllerDescriptor.Properties is not a " + typeof(DynamicApiControllerInfo).FullName + " class.");
+            }
+
+            //Get action name
+            var serviceNameWithAction = (controllerContext.RouteData.Values["serviceNameWithAction"] as string);
+            if (serviceNameWithAction == null)
+            {
+                return base.SelectAction(controllerContext);
+            }
+
+            var actionName = DynamicApiServiceNameHelper.GetActionNameInServiceNameWithAction(serviceNameWithAction);
+
+            //Get action information
+            if (!controllerInfo.Actions.ContainsKey(actionName))
+            {
+                throw new AbpException("There is no action " + actionName + " defined for api controller " + controllerInfo.ServiceName);
+            }
+
+            return new DyanamicHttpActionDescriptor(controllerContext.ControllerDescriptor, controllerInfo.Actions[actionName].Method, controllerInfo.Actions[actionName].Filters);
         }
     }
 }
