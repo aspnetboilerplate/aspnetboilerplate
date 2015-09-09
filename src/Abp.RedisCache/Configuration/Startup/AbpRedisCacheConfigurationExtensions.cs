80f38d64ea157075dd13ed53ea7bf33358c27788
noelguo <noelguo@yaomaiche.com>
2015-09-09 11:18:29 +0800

    增加 Redis Cache

---
+++ src/Abp.RedisCache/Configuration/Startup/AbpRedisCacheConfigurationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Abp.Configuration.Startup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abp.RedisCache.Configuration.Startup
+{
+    /// <summary>
+    /// Defines extension methods to <see cref="IModuleConfigurations"/> to allow to configure ABP MongoDb module.
+    /// </summary>
+    public static class AbpRedisCacheConfigurationExtensions
+    {
+        /// <summary>
+        /// Used to configure ABP Redis Cache module.
+        /// </summary>
+        public static IAbpRedisCacheModuleConfiguration RedisCache(this IModuleConfigurations configurations)
+        {
+            return configurations.AbpConfiguration.GetOrCreate("Modules.Abp.RedisCache", () => configurations.AbpConfiguration.IocManager.Resolve<IAbpRedisCacheModuleConfiguration>());
+        }
+    }
+}
---
+++ src/Abp.RedisCache/RedisCache/Configuration/RedisConnectionProvider.cs
@@ -0,0 +1,54 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abp.RedisCache.Configuration
+{
+    public class AbpRedisConnectionProvider : IAbpRedisConnectionProvider
+    {
+        private static ConcurrentDictionary<string, Lazy<ConnectionMultiplexer>> _connectionMultiplexers = new ConcurrentDictionary<string, Lazy<ConnectionMultiplexer>>();
+      
+
+
+        public string GetConnectionString(string service)
+        {
+           
+            var _defaultSettingsKey = service;
+
+            var connectionStringSettings = ConfigurationManager.ConnectionStrings[_defaultSettingsKey];
+
+            if (connectionStringSettings == null)
+            {
+                throw new ConfigurationErrorsException("A connection string is expected for " + service);
+            }
+
+            return connectionStringSettings.ConnectionString;
+        }
+
+        public ConnectionMultiplexer GetConnection(string connectionString)
+        {
+
+            if (String.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ArgumentNullException("connectionString");
+            }
+
+            // when using ConcurrentDictionary, multiple threads can create the value
+            // at the same time, so we need to pass a Lazy so that it's only 
+            // the object which is added that will create a ConnectionMultiplexer,
+            // even when a delegate is passed
+
+            return _connectionMultiplexers.GetOrAdd(connectionString,
+                new Lazy<ConnectionMultiplexer>(() =>
+                {
+                  
+                    return ConnectionMultiplexer.Connect(connectionString);
+                })).Value;
+        }
+    }
+}
