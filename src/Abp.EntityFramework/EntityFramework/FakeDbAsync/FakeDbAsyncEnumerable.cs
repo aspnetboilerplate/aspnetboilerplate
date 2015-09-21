48a5085f6034c3ce1f8d5cbaeb7b936e5f83a830
AndHuang <andrewhuang1211@qq.com>
2015-09-22 06:47:29 +0800

    add EfRepositoryBaseInitialCacheAllOfTEntityAndTPrimaryKey.cs
    实现领域对象在仓储层的缓存，适用于缓存数据库指定表中某租户相关的所有数据，比如某租户的所有用户数据、角色数据等。

---
+++ src/Abp.EntityFramework/EntityFramework/FakeDbAsync/FakeDbAsyncEnumerable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abp.EntityFramework.FakeDbAsync
+{
+    public class FakeDbAsyncEnumerable<T> : EnumerableQuery<T>, IDbAsyncEnumerable<T>, IQueryable<T>
+    {
+        public FakeDbAsyncEnumerable(IEnumerable<T> enumerable)
+            : base(enumerable)
+        { }
+
+        public FakeDbAsyncEnumerable(Expression expression)
+            : base(expression)
+        { }
+
+        public IDbAsyncEnumerator<T> GetAsyncEnumerator()
+        {
+            return new FakeDbAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
+        }
+
+        IDbAsyncEnumerator IDbAsyncEnumerable.GetAsyncEnumerator()
+        {
+            return GetAsyncEnumerator();
+        }
+
+        IQueryProvider IQueryable.Provider
+        {
+            get { return new FakeDbAsyncQueryProvider<T>(this); }
+        }
+    }
+}
---
+++ src/Abp/Domain/Repositories/IFromDBOfTEntity.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abp.Domain.Repositories
+{
+    public interface IFromDB<TEntity> : IFromDB<TEntity, int> where TEntity : class, IEntity<int>
+    {
+
+    }
+}
