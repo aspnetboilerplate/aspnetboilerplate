8cfbde6911cf3f7b12a0b8de464a782fd76d7e39
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2015-10-14 19:12:04 +0300

    Resolved #675: Created ClaimsAbpSession as default IAbpSession implementation.

--- src/Abp.Web/Web/AbpWebApplication.cs
+++ src/Abp.Web/Web/AbpWebApplication.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
 using System.Threading;
 using System.Web;
 using Abp.Dependency;
 using Abp.Localization;
+using Abp.MultiTenancy;
 using Abp.Reflection;
+using Abp.Runtime.Security;
 
 namespace Abp.Web
 {
@@ -33,16 +37,25 @@ namespace Abp.Web
             AbpBootstrapper.Initialize();
         }
 
+        /// <summary>
+        /// This method is called by ASP.NET system on web application shutdown.
+        /// </summary>
         protected virtual void Application_End(object sender, EventArgs e)
         {
             AbpBootstrapper.Dispose();
         }
 
+        /// <summary>
+        /// This method is called by ASP.NET system when a session starts.
+        /// </summary>
         protected virtual void Session_Start(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// This method is called by ASP.NET system when a session ends.
+        /// </summary>
         protected virtual void Session_End(object sender, EventArgs e)
         {
 
@@ -66,15 +79,60 @@ namespace Abp.Web
         /// </summary>
         protected virtual void Application_EndRequest(object sender, EventArgs e)
         {
+
         }
 
         protected virtual void Application_AuthenticateRequest(object sender, EventArgs e)
         {
+            TrySetTenantId();
         }
 
         protected virtual void Application_Error(object sender, EventArgs e)
         {
 
+        }
+
+        /// <summary>
+        /// Tries to set current tenant Id.
+        /// </summary>
+        protected virtual void TrySetTenantId()
+        {
+            var claimsPrincipal = User as ClaimsPrincipal;
+            if (claimsPrincipal == null)
+            {
+                return;
+            }
+
+            var claimsIdentity = claimsPrincipal.Identity as ClaimsIdentity;
+            if (claimsIdentity == null)
+            {
+                return;
+            }
+
+            var tenantIdClaim = claimsIdentity.Claims.FirstOrDefault(c => c.Type == AbpClaimTypes.TenantId);
+            if (tenantIdClaim != null)
+            {
+                return;
+            }
+
+            var tenantId = ResolveTenantIdOrNull();
+            if (!tenantId.HasValue)
+            {
+                return;
+            }
+
+            claimsIdentity.AddClaim(new Claim(AbpClaimTypes.TenantId, tenantId.Value.ToString(CultureInfo.InvariantCulture)));
+        }
+
+        /// <summary>
+        /// Resolves current tenant id or returns null if can not.
+        /// </summary>
+        protected virtual int? ResolveTenantIdOrNull()
+        {
+            using (var tenantIdResolver = AbpBootstrapper.IocManager.ResolveAsDisposable<ITenantIdResolver>())
+            {
+                return tenantIdResolver.Object.TenantId;
+            }
         }
     }
 }
