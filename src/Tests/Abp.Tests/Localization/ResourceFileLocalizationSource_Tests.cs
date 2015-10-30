91f2ccddaa100617e59bba3024de18884aac3108
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2015-10-31 00:52:21 +0200

    Added unit tests for ResourceFileLocalizationSource.

--- src/Tests/Abp.Tests/Localization/ResourceFileLocalizationSource_Tests.cs
+++ src/Tests/Abp.Tests/Localization/ResourceFileLocalizationSource_Tests.cs
@@ -19,21 +19,51 @@ namespace Abp.Tests.Localization
         }
 
         [Fact]
-        public void Test_All()
+        public void Test_GetString()
         {
+            //Defined in English
             _resourceFileLocalizationSource.GetString("Hello", new CultureInfo("en")).ShouldBe("Hello!");
-            _resourceFileLocalizationSource.GetString("Hello", new CultureInfo("en-GB")).ShouldBe("Hello!");
+
+            //en-US and en-GB fallbacks to en
             _resourceFileLocalizationSource.GetString("Hello", new CultureInfo("en-US")).ShouldBe("Hello!");
             _resourceFileLocalizationSource.GetString("World", new CultureInfo("en-US")).ShouldBe("World!");
+            _resourceFileLocalizationSource.GetString("Hello", new CultureInfo("en-GB")).ShouldBe("Hello!");
             
+            //Defined in Turkish
             _resourceFileLocalizationSource.GetString("Hello", new CultureInfo("tr")).ShouldBe("Merhaba!");
+
+            //tr-TR fallbacks to tr
             _resourceFileLocalizationSource.GetString("Hello", new CultureInfo("tr-TR")).ShouldBe("Merhaba!");
 
-            //Undefined for Turkish, Fallback to default language
+            //Undefined for Turkish, fallbacks to default language
             _resourceFileLocalizationSource.GetString("World", new CultureInfo("tr-TR")).ShouldBe("World!");
             
             //Undefined at all, fallback to given text
             _resourceFileLocalizationSource.GetString("Apple", new CultureInfo("en-US")).ShouldBe("[Apple]");
+        }
+
+        [Fact]
+        public void Test_GetStringOrNull()
+        {
+            //Defined in English
+            _resourceFileLocalizationSource.GetStringOrNull("Hello", new CultureInfo("en")).ShouldBe("Hello!");
+
+            //en-US and en-GB fallbacks to en
+            _resourceFileLocalizationSource.GetStringOrNull("Hello", new CultureInfo("en-US")).ShouldBe("Hello!");
+            _resourceFileLocalizationSource.GetStringOrNull("World", new CultureInfo("en-US")).ShouldBe("World!");
+            _resourceFileLocalizationSource.GetStringOrNull("Hello", new CultureInfo("en-GB")).ShouldBe("Hello!");
+
+            //Defined in Turkish
+            _resourceFileLocalizationSource.GetStringOrNull("Hello", new CultureInfo("tr")).ShouldBe("Merhaba!");
+
+            //tr-TR fallbacks to tr
+            _resourceFileLocalizationSource.GetStringOrNull("Hello", new CultureInfo("tr-TR")).ShouldBe("Merhaba!");
+
+            //Undefined for Turkish, fallbacks to default language
+            _resourceFileLocalizationSource.GetStringOrNull("World", new CultureInfo("tr-TR")).ShouldBe("World!");
+
+            //Undefined at all, returns null
+            _resourceFileLocalizationSource.GetStringOrNull("Apple", new CultureInfo("en-US")).ShouldBeNull();
         }
     }
 }
