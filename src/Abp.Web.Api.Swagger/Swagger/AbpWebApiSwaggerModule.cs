ea7adc5f9ec8177ccc680313d63da7828e3733b9
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2015-11-26 08:04:03 +0200

    Converted Abp.Web.Api.Swagger to .NET 4.5.2.

--- src/Abp.Web.Api.Swagger/Swagger/AbpWebApiSwaggerModule.cs
+++ src/Abp.Web.Api.Swagger/Swagger/AbpWebApiSwaggerModule.cs
@@ -1,35 +1,14 @@
-﻿using System.Net.Http.Formatting;
-using System.Reflection;
-using System.Web.Http;
-using System.Web.Http.Controllers;
-using System.Web.Http.Dispatcher;
-using Abp.Logging;
+﻿using System.Reflection;
 using Abp.Modules;
-using Abp.Web;
-using Abp.WebApi.Configuration;
-using Abp.WebApi.Controllers;
-using Abp.WebApi.Controllers.Dynamic;
-using Abp.WebApi.Controllers.Dynamic.Formatters;
-using Abp.WebApi.Controllers.Dynamic.Selectors;
-using Abp.WebApi.Controllers.Filters;
-using Abp.WebApi.Runtime.Caching;
-using Newtonsoft.Json.Serialization;
-using NJsonSchema;
-using Abp.WebApi.Controllers.Dynamic.Builders;
-using Abp.Application.Services;
 
 namespace Abp.WebApi
 {
-  
     [DependsOn(typeof(AbpWebApiModule))]
     public class AbpWebApiSwaggerModule : AbpModule
     {
-
-
         public override void Initialize()
         {
             IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
         }
-     
     }
 }
