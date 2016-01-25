1ee8aea31d2c629d9ab3456f3e8708667d0823c5
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-01-25 18:00:50 +0200

    Added nuget package for SignalR.

--- src/Abp.Web.SignalR/Properties/AssemblyInfo.cs
+++ src/Abp.Web.SignalR/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Abp;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Abp.Web.SignalR")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(AbpConsts.CurrentVersion)]
+[assembly: AssemblyFileVersion(AbpConsts.CurrentVersion)]
---
+++ src/Abp.Web.SignalR/Web/SignalR/AbpNotificationHub.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Dependency;
+using Abp.Notifications;
+using Microsoft.AspNet.SignalR;
+
+namespace Abp.Web.SignalR
+{
+    //public class AbpNotificationHub : Hub, ITransientDependency
+    //{
+    //    public AbpNotificationHub()
+    //    {
+            
+    //    }
+
+    //    public override Task OnConnected()
+    //    {
+    //        return base.OnConnected();
+    //    }
+
+    //    public override Task OnDisconnected(bool stopCalled)
+    //    {
+    //        return base.OnDisconnected(stopCalled);
+    //    }
+
+    //    public override Task OnReconnected()
+    //    {
+    //        return base.OnReconnected();
+    //    }
+    //}
+
+    //public class SignalRRealTimeNotifier : IRealTimeNotifier
+    //{
+    //    public Task SendNotificationAsync(NotificationInfo notification, List<UserNotificationInfo> userNotifications)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+}
