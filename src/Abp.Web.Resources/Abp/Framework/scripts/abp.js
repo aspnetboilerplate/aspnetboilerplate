8500a1c9d219d5ea0a17cd82d1add4ac47694396
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-02-09 11:16:30 +0200

    Resolved #885: add off method to javascript eventbus.

--- src/Abp.Web.Resources/Abp/Framework/scripts/abp.js
+++ src/Abp.Web.Resources/Abp/Framework/scripts/abp.js
@@ -349,6 +349,27 @@
             _callbacks[eventName].push(callback);
         };
 
+        var off = function (eventName, callback) {
+            var callbacks = _callbacks[eventName];
+            if (!callbacks) {
+                return;
+            }
+
+            var index = -1;
+            for (var i = 0; i < callbacks.length; i++) {
+                if (callbacks[i] === callback) {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index < 0) {
+                return;
+            }
+
+            _callbacks[eventName].splice(index, 1);
+        };
+
         var trigger = function(eventName) {
             var callbacks = _callbacks[eventName];
             if (!callbacks || !callbacks.length) {
@@ -365,6 +386,7 @@
 
         return {
             on: on,
+            off: off,
             trigger: trigger
         };
     })();
