9270e2754c84e70272583a570b28e8e75c128a4d
ismcagdas <ismailcagdas@yahoo.com>
2016-02-12 17:41:42 +0200

    resolved #862

--- src/Abp/Application/Services/Dto/NameValueDto.cs
+++ src/Abp/Application/Services/Dto/NameValueDto.cs
@@ -6,7 +6,7 @@ namespace Abp.Application.Services.Dto
     /// Can be used to send/receive Name/Value (or Key/Value) pairs.
     /// </summary>
     [Serializable]
-    public class NameValueDto : NameValue, IDto
+    public class NameValueDto : NameValueDto<string>
     {
         /// <summary>
         /// Creates a new <see cref="NameValueDto"/>.
@@ -30,6 +30,40 @@ namespace Abp.Application.Services.Dto
         /// </summary>
         /// <param name="nameValue">A <see cref="NameValue"/> object to get it's name and value</param>
         public NameValueDto(NameValue nameValue)
+            : this(nameValue.Name, nameValue.Value)
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// Can be used to send/receive Name/Value (or Key/Value) pairs.
+    /// </summary>
+    [Serializable]
+    public class NameValueDto<T> : NameValue<T>, IDto
+    {
+        /// <summary>
+        /// Creates a new <see cref="NameValueDto"/>.
+        /// </summary>
+        public NameValueDto()
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="NameValueDto"/>.
+        /// </summary>
+        public NameValueDto(string name, T value)
+            : base(name, value)
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="NameValueDto"/>.
+        /// </summary>
+        /// <param name="nameValue">A <see cref="NameValue"/> object to get it's name and value</param>
+        public NameValueDto(NameValue<T> nameValue)
             : this(nameValue.Name, nameValue.Value)
         {
 
