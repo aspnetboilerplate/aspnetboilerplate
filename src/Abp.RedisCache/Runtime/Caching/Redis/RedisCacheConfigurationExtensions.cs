1cf48513d844186d415ad663d3490bff2b222437
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-03-09 17:16:20 +0200

    #922: Added Configuration.Caching.UseRedis() extension method.

--- src/Abp.RedisCache/Runtime/Caching/Redis/RedisCacheConfigurationExtensions.cs
+++ src/Abp.RedisCache/Runtime/Caching/Redis/RedisCacheConfigurationExtensions.cs
@@ -1,7 +1,20 @@
-﻿namespace Abp.Runtime.Caching.Redis
+﻿using Abp.Dependency;
+using Abp.Runtime.Caching.Configuration;
+
+namespace Abp.Runtime.Caching.Redis
 {
+    /// <summary>
+    /// Extension methods for <see cref="ICachingConfiguration"/>.
+    /// </summary>
     public static class RedisCacheConfigurationExtensions
     {
-
+        /// <summary>
+        /// Configures caching to use Redis as cache server.
+        /// </summary>
+        /// <param name="cachingConfiguration">The caching configuration.</param>
+        public static void UseRedis(this ICachingConfiguration cachingConfiguration)
+        {
+            cachingConfiguration.AbpConfiguration.IocManager.RegisterIfNot<ICacheManager, AbpRedisCacheManager>();
+        }
     }
 }
