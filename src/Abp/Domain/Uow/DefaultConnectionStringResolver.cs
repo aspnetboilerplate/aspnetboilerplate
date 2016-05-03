fbbd73eaaaa29b1cb9cda561c602a67c2839f4a5
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-05-03 11:20:36 +0300

    Refactored and commented.

--- src/Abp/Domain/Uow/DefaultConnectionStringResolver.cs
+++ src/Abp/Domain/Uow/DefaultConnectionStringResolver.cs
@@ -5,10 +5,19 @@ using Abp.MultiTenancy;
 
 namespace Abp.Domain.Uow
 {
+    /// <summary>
+    /// Default implementation of <see cref="IConnectionStringResolver"/>.
+    /// Get connection string from <see cref="IAbpStartupConfiguration"/>,
+    /// or "Default" connection string in config file,
+    /// or single connection string in config file.
+    /// </summary>
     public class DefaultConnectionStringResolver : IConnectionStringResolver, ITransientDependency
     {
         private readonly IAbpStartupConfiguration _configuration;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DefaultConnectionStringResolver"/> class.
+        /// </summary>
         public DefaultConnectionStringResolver(IAbpStartupConfiguration configuration)
         {
             _configuration = configuration;
@@ -32,7 +41,7 @@ namespace Abp.Domain.Uow
                 return ConfigurationManager.ConnectionStrings[0].ConnectionString;
             }
 
-            return null;
+            throw new AbpException("Could not find a connection string definition for the application. Set IAbpStartupConfiguration.DefaultNameOrConnectionString or add a 'Default' connection string to application .config file.");
         }
     }
 }
--- src/Abp/Domain/Uow/IConnectionStringResolver.cs
+++ src/Abp/Domain/Uow/IConnectionStringResolver.cs
@@ -2,8 +2,15 @@ using Abp.MultiTenancy;
 
 namespace Abp.Domain.Uow
 {
+    /// <summary>
+    /// Used to get connection string when a database connection is needed.
+    /// </summary>
     public interface IConnectionStringResolver
     {
+        /// <summary>
+        /// Gets a connection string name (in config file) or a valid connection string.
+        /// </summary>
+        /// <param name="multiTenancySide">The multi tenancy side.</param>
         string GetNameOrConnectionString(MultiTenancySides? multiTenancySide = null);
     }
 }
