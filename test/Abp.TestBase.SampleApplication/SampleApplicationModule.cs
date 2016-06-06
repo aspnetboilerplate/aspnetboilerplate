5654ffaa102c7b07b930c25d949aa444d4d69cb2
Alexander Sobolev <4nonym0us@xakep.ru>
2016-06-06 14:35:38 +0300

    Add more unit-tests for Abp.EF.GraphDiff package.

--- test/Abp.TestBase.SampleApplication/SampleApplicationModule.cs
+++ test/Abp.TestBase.SampleApplication/SampleApplicationModule.cs
@@ -1,12 +1,18 @@
-﻿using System.Reflection;
+﻿using System.Collections.Generic;
+using System.Reflection;
 using Abp.AutoMapper;
 using Abp.EntityFramework;
+using Abp.GraphDiff;
+using Abp.GraphDiff.Mapping;
 using Abp.Modules;
 using Abp.TestBase.SampleApplication.ContacLists;
+using Abp.GraphDiff.Configuration;
+using Abp.TestBase.SampleApplication.People;
+using RefactorThis.GraphDiff;
 
 namespace Abp.TestBase.SampleApplication
 {
-    [DependsOn(typeof(AbpEntityFrameworkModule), typeof(AbpAutoMapperModule))]
+    [DependsOn(typeof(AbpEntityFrameworkModule), typeof(AbpAutoMapperModule), typeof(AbpEntityFrameworkGraphDiffModule))]
     public class SampleApplicationModule : AbpModule
     {
         public override void PreInitialize()
@@ -17,6 +23,12 @@ namespace Abp.TestBase.SampleApplication
         public override void Initialize()
         {
             IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+
+            Configuration.Modules.AbpEfGraphDiff().EntityMappings = new List<EntityMapping>
+            {
+                MappingExpressionBuilder.For<ContactList>(config => config.AssociatedCollection(entity => entity.People)),
+                MappingExpressionBuilder.For<Person>(config => config.AssociatedEntity(entity => entity.ContactList))
+            };
         }
     }
 }
