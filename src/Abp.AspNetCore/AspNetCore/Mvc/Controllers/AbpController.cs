edfd21ba3ed244a9d5a6cc1139cb51233387610d
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-06-08 15:00:45 +0300

    Migrated some old MVC code to AspNetCore.

---
+++ src/Abp.AspNetCore/AspNetCore/Mvc/Controllers/AbpController.cs
@@ -0,0 +1,535 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Application.Features;
+using Abp.AspNetCore.Mvc.Controllers.Results;
+using Abp.Auditing;
+using Abp.Authorization;
+using Abp.Collections.Extensions;
+using Abp.Configuration;
+using Abp.Domain.Uow;
+using Abp.Events.Bus;
+using Abp.Events.Bus.Exceptions;
+using Abp.Localization;
+using Abp.Localization.Sources;
+using Abp.Logging;
+using Abp.Reflection;
+using Abp.Runtime.Session;
+using Abp.Timing;
+using Abp.Web.Models;
+using Abp.Web.Mvc.Models;
+using Castle.Core.Logging;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace Abp.AspNetCore.Mvc.Controllers
+{
+    /// <summary>
+    /// Base class for all MVC Controllers in Abp system.
+    /// </summary>
+    public abstract class AbpController : Controller
+    {
+        /// <summary>
+        /// Gets current session information.
+        /// </summary>
+        public IAbpSession AbpSession { get; set; }
+
+        /// <summary>
+        /// Gets the event bus.
+        /// </summary>
+        public IEventBus EventBus { get; set; }
+
+        /// <summary>
+        /// Reference to the permission manager.
+        /// </summary>
+        public IPermissionManager PermissionManager { get; set; }
+
+        /// <summary>
+        /// Reference to the setting manager.
+        /// </summary>
+        public ISettingManager SettingManager { get; set; }
+
+        /// <summary>
+        /// Reference to the permission checker.
+        /// </summary>
+        public IPermissionChecker PermissionChecker { protected get; set; }
+
+        /// <summary>
+        /// Reference to the feature manager.
+        /// </summary>
+        public IFeatureManager FeatureManager { protected get; set; }
+
+        /// <summary>
+        /// Reference to the permission checker.
+        /// </summary>
+        public IFeatureChecker FeatureChecker { protected get; set; }
+
+        /// <summary>
+        /// Reference to the localization manager.
+        /// </summary>
+        public ILocalizationManager LocalizationManager { protected get; set; }
+
+        /// <summary>
+        /// Gets/sets name of the localization source that is used in this application service.
+        /// It must be set in order to use <see cref="L(string)"/> and <see cref="L(string,CultureInfo)"/> methods.
+        /// </summary>
+        protected string LocalizationSourceName { get; set; }
+
+        /// <summary>
+        /// Gets localization source.
+        /// It's valid if <see cref="LocalizationSourceName"/> is set.
+        /// </summary>
+        protected ILocalizationSource LocalizationSource
+        {
+            get
+            {
+                if (LocalizationSourceName == null)
+                {
+                    throw new AbpException("Must set LocalizationSourceName before, in order to get LocalizationSource");
+                }
+
+                if (_localizationSource == null || _localizationSource.Name != LocalizationSourceName)
+                {
+                    _localizationSource = LocalizationManager.GetSource(LocalizationSourceName);
+                }
+
+                return _localizationSource;
+            }
+        }
+        private ILocalizationSource _localizationSource;
+
+        /// <summary>
+        /// Reference to the logger to write logs.
+        /// </summary>
+        public ILogger Logger { get; set; }
+
+        /// <summary>
+        /// Gets current session information.
+        /// </summary>
+        [Obsolete("Use AbpSession property instead. CurrentSession will be removed in future releases.")]
+        protected IAbpSession CurrentSession { get { return AbpSession; } }
+
+        /// <summary>
+        /// Reference to <see cref="IUnitOfWorkManager"/>.
+        /// </summary>
+        public IUnitOfWorkManager UnitOfWorkManager
+        {
+            get
+            {
+                if (_unitOfWorkManager == null)
+                {
+                    throw new AbpException("Must set UnitOfWorkManager before use it.");
+                }
+
+                return _unitOfWorkManager;
+            }
+            set { _unitOfWorkManager = value; }
+        }
+        private IUnitOfWorkManager _unitOfWorkManager;
+
+        /// <summary>
+        /// Gets current unit of work.
+        /// </summary>
+        protected IActiveUnitOfWork CurrentUnitOfWork { get { return UnitOfWorkManager.Current; } }
+
+        public IAuditingConfiguration AuditingConfiguration { get; set; }
+
+        public IAuditInfoProvider AuditInfoProvider { get; set; }
+
+        public IAuditingStore AuditingStore { get; set; }
+
+        /// <summary>
+        /// This object is used to measure an action execute duration.
+        /// </summary>
+        private Stopwatch _actionStopwatch;
+
+        private AuditInfo _auditInfo;
+
+        /// <summary>
+        /// MethodInfo for currently executing action.
+        /// </summary>
+        private MethodInfo _currentMethodInfo;
+
+        /// <summary>
+        /// WrapResultAttribute for currently executing action.
+        /// </summary>
+        private WrapResultAttribute _wrapResultAttribute;
+
+        /// <summary>
+        /// Ignored types for serialization on audit logging.
+        /// </summary>
+        protected static List<Type> IgnoredTypesForSerializationOnAuditLogging { get; private set; }
+
+        //static AbpController()
+        //{
+        //    IgnoredTypesForSerializationOnAuditLogging = new List<Type>
+        //    {
+        //        typeof (HttpPostedFileBase),
+        //        typeof (IEnumerable<HttpPostedFileBase>)
+        //    };
+        //}
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        protected AbpController()
+        {
+            AbpSession = NullAbpSession.Instance;
+            Logger = NullLogger.Instance;
+            LocalizationManager = NullLocalizationManager.Instance;
+            PermissionChecker = NullPermissionChecker.Instance;
+            AuditingStore = SimpleLogAuditingStore.Instance;
+            EventBus = NullEventBus.Instance;
+        }
+
+        /// <summary>
+        /// Gets localized string for given key name and current language.
+        /// </summary>
+        /// <param name="name">Key name</param>
+        /// <returns>Localized string</returns>
+        protected virtual string L(string name)
+        {
+            return LocalizationSource.GetString(name);
+        }
+
+        /// <summary>
+        /// Gets localized string for given key name and current language with formatting strings.
+        /// </summary>
+        /// <param name="name">Key name</param>
+        /// <param name="args">Format arguments</param>
+        /// <returns>Localized string</returns>
+        protected string L(string name, params object[] args)
+        {
+            return LocalizationSource.GetString(name, args);
+        }
+
+        /// <summary>
+        /// Gets localized string for given key name and specified culture information.
+        /// </summary>
+        /// <param name="name">Key name</param>
+        /// <param name="culture">culture information</param>
+        /// <returns>Localized string</returns>
+        protected virtual string L(string name, CultureInfo culture)
+        {
+            return LocalizationSource.GetString(name, culture);
+        }
+
+        /// <summary>
+        /// Gets localized string for given key name and current language with formatting strings.
+        /// </summary>
+        /// <param name="name">Key name</param>
+        /// <param name="culture">culture information</param>
+        /// <param name="args">Format arguments</param>
+        /// <returns>Localized string</returns>
+        protected string L(string name, CultureInfo culture, params object[] args)
+        {
+            return LocalizationSource.GetString(name, culture, args);
+        }
+
+        /// <summary>
+        /// Checks if current user is granted for a permission.
+        /// </summary>
+        /// <param name="permissionName">Name of the permission</param>
+        protected Task<bool> IsGrantedAsync(string permissionName)
+        {
+            return PermissionChecker.IsGrantedAsync(permissionName);
+        }
+
+        /// <summary>
+        /// Checks if current user is granted for a permission.
+        /// </summary>
+        /// <param name="permissionName">Name of the permission</param>
+        protected bool IsGranted(string permissionName)
+        {
+            return PermissionChecker.IsGranted(permissionName);
+        }
+
+
+        /// <summary>
+        /// Checks if given feature is enabled for current tenant.
+        /// </summary>
+        /// <param name="featureName">Name of the feature</param>
+        /// <returns></returns>
+        protected virtual Task<bool> IsEnabledAsync(string featureName)
+        {
+            return FeatureChecker.IsEnabledAsync(featureName);
+        }
+
+        /// <summary>
+        /// Checks if given feature is enabled for current tenant.
+        /// </summary>
+        /// <param name="featureName">Name of the feature</param>
+        /// <returns></returns>
+        protected virtual bool IsEnabled(string featureName)
+        {
+            return FeatureChecker.IsEnabled(featureName);
+        }
+
+        ///// <summary>
+        ///// Json the specified data, contentType, contentEncoding and behavior.
+        ///// </summary>
+        ///// <param name="data">Data.</param>
+        ///// <param name="contentType">Content type.</param>
+        ///// <param name="contentEncoding">Content encoding.</param>
+        ///// <param name="behavior">Behavior.</param>
+        //protected override JsonResult Json(object data, string contentType, Encoding contentEncoding, JsonRequestBehavior behavior)
+        //{
+        //    if (_wrapResultAttribute != null && !_wrapResultAttribute.WrapOnSuccess)
+        //    {
+        //        return base.Json(data, contentType, contentEncoding, behavior);
+        //    }
+
+        //    if (data == null)
+        //    {
+        //        data = new AjaxResponse();
+        //    }
+        //    else if (!ReflectionHelper.IsAssignableToGenericType(data.GetType(), typeof(AjaxResponse<>)))
+        //    {
+        //        data = new AjaxResponse(data);
+        //    }
+
+        //    return new AbpJsonResult(data)
+        //    {
+        //        Data = data,
+        //        ContentType = contentType,
+        //        ContentEncoding = contentEncoding,
+        //        JsonRequestBehavior = behavior
+        //    };
+        //}
+
+        //#region OnActionExecuting / OnActionExecuted
+
+        //public override void OnActionExecuting(ActionExecutingContext filterContext)
+        //{
+        //    SetCurrentMethodInfoAndWrapResultAttribute(filterContext);
+        //    HandleAuditingBeforeAction(filterContext);
+
+        //    base.OnActionExecuting(filterContext);
+        //}
+
+        //public override void OnActionExecuted(ActionExecutedContext filterContext)
+        //{
+        //    base.OnActionExecuted(filterContext);
+
+        //    HandleAuditingAfterAction(filterContext);
+        //}
+
+        //private void SetCurrentMethodInfoAndWrapResultAttribute(ActionExecutingContext filterContext)
+        //{
+        //    _currentMethodInfo = ActionDescriptorHelper.GetMethodInfo(filterContext.ActionDescriptor);
+        //    _wrapResultAttribute =
+        //        ReflectionHelper.GetSingleAttributeOfMemberOrDeclaringTypeOrNull<WrapResultAttribute>(_currentMethodInfo) ??
+        //        WrapResultAttribute.Default;
+        //}
+
+        //#endregion
+
+        //#region Exception handling
+
+        //protected override void OnException(ExceptionContext context)
+        //{
+        //    if (context == null)
+        //    {
+        //        throw new ArgumentNullException("context");
+        //    }
+
+        //    //If exception handled before, do nothing.
+        //    //If this is child action, exception should be handled by main action.
+        //    if (context.ExceptionHandled || context.IsChildAction)
+        //    {
+        //        base.OnException(context);
+        //        return;
+        //    }
+
+        //    //Log exception
+        //    if (_wrapResultAttribute.LogError)
+        //    {
+        //        LogHelper.LogException(Logger, context.Exception);
+        //    }
+
+        //    // If custom errors are disabled, we need to let the normal ASP.NET exception handler
+        //    // execute so that the user can see useful debugging information.
+        //    if (!context.HttpContext.IsCustomErrorEnabled)
+        //    {
+        //        base.OnException(context);
+        //        return;
+        //    }
+
+        //    // If this is not an HTTP 500 (for example, if somebody throws an HTTP 404 from an action method),
+        //    // ignore it.
+        //    if (new HttpException(null, context.Exception).GetHttpCode() != 500)
+        //    {
+        //        base.OnException(context);
+        //        return;
+        //    }
+
+        //    //Check WrapResultAttribute
+        //    if (!_wrapResultAttribute.WrapOnError)
+        //    {
+        //        base.OnException(context);
+        //        return;
+        //    }
+
+        //    //We handled the exception!
+        //    context.ExceptionHandled = true;
+
+        //    //Return a special error response to the client.
+        //    context.HttpContext.Response.Clear();
+        //    context.Result = IsJsonResult()
+        //        ? GenerateJsonExceptionResult(context)
+        //        : GenerateNonJsonExceptionResult(context);
+
+        //    // Certain versions of IIS will sometimes use their own error page when
+        //    // they detect a server error. Setting this property indicates that we
+        //    // want it to try to render ASP.NET MVC's error page instead.
+        //    context.HttpContext.Response.TrySkipIisCustomErrors = true;
+
+        //    //Trigger an event, so we can register it.
+        //    EventBus.Trigger(this, new AbpHandledExceptionData(context.Exception));
+        //}
+
+        //protected virtual bool IsJsonResult()
+        //{
+        //    return typeof(JsonResult).IsAssignableFrom(_currentMethodInfo.ReturnType) ||
+        //           typeof(Task<JsonResult>).IsAssignableFrom(_currentMethodInfo.ReturnType);
+        //}
+
+        //protected virtual ActionResult GenerateJsonExceptionResult(ExceptionContext context)
+        //{
+        //    context.HttpContext.Items.Add("IgnoreJsonRequestBehaviorDenyGet", "true");
+        //    context.HttpContext.Response.StatusCode = 200; //TODO: Consider to return 500
+        //    return new AbpJsonResult(
+        //        new MvcAjaxResponse(
+        //            ErrorInfoBuilder.Instance.BuildForException(context.Exception),
+        //            context.Exception is AbpAuthorizationException
+        //            )
+        //        );
+        //}
+
+        //protected virtual ActionResult GenerateNonJsonExceptionResult(ExceptionContext context)
+        //{
+        //    context.HttpContext.Response.StatusCode = 500;
+        //    return new ViewResult
+        //    {
+        //        ViewName = "Error",
+        //        MasterName = string.Empty,
+        //        ViewData = new ViewDataDictionary<ErrorViewModel>(new ErrorViewModel(context.Exception)),
+        //        TempData = context.Controller.TempData
+        //    };
+        //}
+
+        //#endregion
+
+        //#region Auditing
+
+        //private void HandleAuditingBeforeAction(ActionExecutingContext filterContext)
+        //{
+        //    if (!ShouldSaveAudit(filterContext))
+        //    {
+        //        _auditInfo = null;
+        //        return;
+        //    }
+
+        //    _actionStopwatch = Stopwatch.StartNew();
+        //    _auditInfo = new AuditInfo
+        //    {
+        //        TenantId = AbpSession.TenantId,
+        //        UserId = AbpSession.UserId,
+        //        ImpersonatorUserId = AbpSession.ImpersonatorUserId,
+        //        ImpersonatorTenantId = AbpSession.ImpersonatorTenantId,
+        //        ServiceName = _currentMethodInfo.DeclaringType != null
+        //                        ? _currentMethodInfo.DeclaringType.FullName
+        //                        : filterContext.ActionDescriptor.ControllerDescriptor.ControllerName,
+        //        MethodName = _currentMethodInfo.Name,
+        //        Parameters = ConvertArgumentsToJson(filterContext.ActionParameters),
+        //        ExecutionTime = Clock.Now
+        //    };
+        //}
+
+        //private void HandleAuditingAfterAction(ActionExecutedContext filterContext)
+        //{
+        //    if (_auditInfo == null || _actionStopwatch == null)
+        //    {
+        //        return;
+        //    }
+
+        //    _actionStopwatch.Stop();
+
+        //    _auditInfo.ExecutionDuration = Convert.ToInt32(_actionStopwatch.Elapsed.TotalMilliseconds);
+        //    _auditInfo.Exception = filterContext.Exception;
+
+        //    if (AuditInfoProvider != null)
+        //    {
+        //        AuditInfoProvider.Fill(_auditInfo);
+        //    }
+
+        //    AuditingStore.Save(_auditInfo);
+        //}
+
+        //private bool ShouldSaveAudit(ActionExecutingContext filterContext)
+        //{
+        //    if (AuditingConfiguration == null)
+        //    {
+        //        return false;
+        //    }
+
+        //    if (!AuditingConfiguration.MvcControllers.IsEnabled)
+        //    {
+        //        return false;
+        //    }
+
+        //    if (filterContext.IsChildAction && !AuditingConfiguration.MvcControllers.IsEnabledForChildActions)
+        //    {
+        //        return false;
+        //    }
+
+        //    return AuditingHelper.ShouldSaveAudit(
+        //        _currentMethodInfo,
+        //        AuditingConfiguration,
+        //        AbpSession,
+        //        true
+        //        );
+        //}
+
+        //private string ConvertArgumentsToJson(IDictionary<string, object> arguments)
+        //{
+        //    try
+        //    {
+        //        if (arguments.IsNullOrEmpty())
+        //        {
+        //            return "{}";
+        //        }
+
+        //        var dictionary = new Dictionary<string, object>();
+
+        //        foreach (var argument in arguments)
+        //        {
+        //            if (argument.Value != null && IgnoredTypesForSerializationOnAuditLogging.Any(t => t.IsInstanceOfType(argument.Value)))
+        //            {
+        //                dictionary[argument.Key] = null;
+        //            }
+        //            else
+        //            {
+        //                dictionary[argument.Key] = argument.Value;
+        //            }
+        //        }
+
+        //        return AuditingHelper.Serialize(dictionary);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Logger.Warn("Could not serialize arguments for method: " + _auditInfo.ServiceName + "." + _auditInfo.MethodName);
+        //        Logger.Warn(ex.ToString(), ex);
+        //        return "{}";
+        //    }
+        //}
+
+        //#endregion
+    }
+}
