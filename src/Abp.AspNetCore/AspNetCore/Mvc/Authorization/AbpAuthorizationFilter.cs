30fd8346d7ae41f5b077cbe3a8cc66f272b80dbc
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-06-24 01:37:21 +0300

    Added feature checking to AspNet Core auth filter.

--- src/Abp.AspNetCore/AspNetCore/Mvc/Authorization/AbpAuthorizationFilter.cs
+++ src/Abp.AspNetCore/AspNetCore/Mvc/Authorization/AbpAuthorizationFilter.cs
@@ -1,5 +1,7 @@
 ﻿using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using Abp.Application.Features;
 using Abp.AspNetCore.Mvc.Extensions;
 using Abp.Authorization;
 using Abp.Dependency;
@@ -19,9 +21,39 @@ namespace Abp.AspNetCore.Mvc.Authorization
 
         public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
         {
+            var methodInfo = context.ActionDescriptor.GetMethodInfo();
+            await CheckFeatures(methodInfo);
+            await CheckPermissions(methodInfo);
+        }
+
+        //TODO: This methods can be moved to another class to be shared.
+
+        private async Task CheckFeatures(MethodInfo methodInfo)
+        {
+            var featureAttributes =
+                ReflectionHelper.GetAttributesOfMemberAndDeclaringType<RequiresFeatureAttribute>(
+                    methodInfo
+                    );
+
+            if (featureAttributes.Count <= 0)
+            {
+                return;
+            }
+
+            using (var featureChecker = _iocResolver.ResolveAsDisposable<IFeatureChecker>())
+            {
+                foreach (var featureAttribute in featureAttributes)
+                {
+                    await featureChecker.Object.CheckEnabledAsync(featureAttribute.RequiresAll, featureAttribute.Features);
+                }
+            }
+        }
+
+        private async Task CheckPermissions(MethodInfo methodInfo)
+        {
             var authorizeAttributes =
                 ReflectionHelper.GetAttributesOfMemberAndDeclaringType<AbpMvcAuthorizeAttribute>(
-                    context.ActionDescriptor.GetMethodInfo()
+                    methodInfo
                 );
 
             if (!authorizeAttributes.Any())
