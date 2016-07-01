939fe410b24e2082cbc30f07d88462228e12c520
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-07-01 15:11:56 +0300

    Added unit test for #1163.

--- test/Abp.TestBase.SampleApplication/ContacLists/ContactListAppService.cs
+++ test/Abp.TestBase.SampleApplication/ContacLists/ContactListAppService.cs
@@ -1,13 +1,28 @@
-﻿using Abp.Application.Features;
+﻿using System.Collections.Generic;
+using Abp.Application.Features;
+using Abp.AutoMapper;
+using Abp.Domain.Repositories;
 
 namespace Abp.TestBase.SampleApplication.ContacLists
 {
     public class ContactListAppService : IContactListAppService
     {
+        private readonly IRepository<ContactList> _contactListRepository;
+
+        public ContactListAppService(IRepository<ContactList> contactListRepository)
+        {
+            _contactListRepository = contactListRepository;
+        }
+
         [RequiresFeature(SampleFeatureProvider.Names.Contacts)]
         public void Test()
         {
             //This method is called only if SampleFeatureProvider.Names.Contacts feature is enabled
+        }
+
+        public List<ContactListDto> GetContactLists()
+        {
+            return _contactListRepository.GetAllList().MapTo<List<ContactListDto>>();
         }
     }
 }
