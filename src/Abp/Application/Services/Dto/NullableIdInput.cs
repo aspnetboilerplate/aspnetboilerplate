cbc100d2e4dad8fb1927fa71a8b3182f77d30481
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-07-20 15:13:33 +0300

    Resolved #1222: Deprecated IInputDto, IOutputDto, IDoubleWayDto and IDto.

--- src/Abp/Application/Services/Dto/NullableIdInput.cs
+++ src/Abp/Application/Services/Dto/NullableIdInput.cs
@@ -1,11 +1,11 @@
 namespace Abp.Application.Services.Dto
 {
     /// <summary>
-    /// This <see cref="IInputDto"/> can be directly used (or inherited)
+    /// This DTO can be directly used (or inherited)
     /// to pass an nullable Id value to an application service method.
     /// </summary>
     /// <typeparam name="TId">Type of the Id</typeparam>
-    public class NullableIdInput<TId> : IInputDto
+    public class NullableIdInput<TId>
         where TId : struct
     {
         public TId? Id { get; set; }
