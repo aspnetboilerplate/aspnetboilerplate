b39157d04b845804687542e89a0b506847f73537
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-08-18 18:21:10 +0300

    Resolved #1294: Allow to set options on UseRedis method.

--- src/Abp.RedisCache/Runtime/Caching/Redis/RedisCacheConfigurationExtensions.cs
+++ src/Abp.RedisCache/Runtime/Caching/Redis/RedisCacheConfigurationExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Abp.Dependency;
+﻿using System;
+using Abp.Dependency;
 using Abp.Runtime.Caching.Configuration;
 
 namespace Abp.Runtime.Caching.Redis
@@ -12,9 +13,14 @@ namespace Abp.Runtime.Caching.Redis
         /// Configures caching to use Redis as cache server.
         /// </summary>
         /// <param name="cachingConfiguration">The caching configuration.</param>
-        public static void UseRedis(this ICachingConfiguration cachingConfiguration)
+        /// <param name="optionsAction">Ac action to get/set options</param>
+        public static void UseRedis(this ICachingConfiguration cachingConfiguration, Action<AbpRedisCacheOptions> optionsAction)
         {
-            cachingConfiguration.AbpConfiguration.IocManager.RegisterIfNot<ICacheManager, AbpRedisCacheManager>();
+            var iocManager = cachingConfiguration.AbpConfiguration.IocManager;
+
+            iocManager.RegisterIfNot<ICacheManager, AbpRedisCacheManager>();
+
+            optionsAction(iocManager.Resolve<AbpRedisCacheOptions>());
         }
     }
 }
