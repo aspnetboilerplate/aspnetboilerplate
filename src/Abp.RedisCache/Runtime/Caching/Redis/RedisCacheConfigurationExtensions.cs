1598f6c6578da0ae72926db84554e54ffa906ea2
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-08-20 12:40:18 +0300

    Add overload for UseRedis that does not need to option configuration action.

--- src/Abp.RedisCache/Runtime/Caching/Redis/RedisCacheConfigurationExtensions.cs
+++ src/Abp.RedisCache/Runtime/Caching/Redis/RedisCacheConfigurationExtensions.cs
@@ -13,6 +13,15 @@ namespace Abp.Runtime.Caching.Redis
         /// Configures caching to use Redis as cache server.
         /// </summary>
         /// <param name="cachingConfiguration">The caching configuration.</param>
+        public static void UseRedis(this ICachingConfiguration cachingConfiguration)
+        {
+            cachingConfiguration.UseRedis(options => { });
+        }
+
+        /// <summary>
+        /// Configures caching to use Redis as cache server.
+        /// </summary>
+        /// <param name="cachingConfiguration">The caching configuration.</param>
         /// <param name="optionsAction">Ac action to get/set options</param>
         public static void UseRedis(this ICachingConfiguration cachingConfiguration, Action<AbpRedisCacheOptions> optionsAction)
         {
