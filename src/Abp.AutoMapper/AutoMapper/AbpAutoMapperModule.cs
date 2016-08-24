da0dd5a6715286c98bceb779ccf4cf4c557d7830
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-08-24 15:00:55 +0300

    Remove unncessary resolve.

--- src/Abp.AutoMapper/AutoMapper/AbpAutoMapperModule.cs
+++ src/Abp.AutoMapper/AutoMapper/AbpAutoMapperModule.cs
@@ -81,11 +81,10 @@ namespace Abp.AutoMapper
 
         private void CreateCoreMappings(IMapperConfigurationExpression configuration)
         {
-            var localizationManager = IocManager.Resolve<ILocalizationManager>();
             var localizationContext = IocManager.Resolve<ILocalizationContext>();
 
             configuration.CreateMap<ILocalizableString, string>().ConvertUsing(ls => ls?.Localize(localizationContext));
-            configuration.CreateMap<LocalizableString, string>().ConvertUsing(ls => ls == null ? null : localizationManager.GetString(ls));
+            configuration.CreateMap<LocalizableString, string>().ConvertUsing(ls => ls == null ? null : localizationContext.LocalizationManager.GetString(ls));
         }
     }
 }
