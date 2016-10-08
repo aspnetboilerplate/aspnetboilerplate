840522c60556c7d4bb2d3bcc5980df071c27a768
Soar360 <Soar360@live.com>
2016-10-08 14:19:04 +0800

    add JobBase to Abp.Quartz

---
+++ src/Abp.Quartz/Quartz/JobBase.cs
@@ -0,0 +1,133 @@
+﻿using System.Globalization;
+using Abp.Configuration;
+using Abp.Domain.Uow;
+using Abp.Localization;
+using Abp.Localization.Sources;
+using Abp.ObjectMapping;
+using Castle.Core.Logging;
+using Quartz;
+
+namespace Abp.Quartz.Quartz
+{
+    public abstract class JobBase : IJob
+    {
+        private ILocalizationSource _localizationSource;
+        private IUnitOfWorkManager _unitOfWorkManager;
+
+        protected JobBase()
+        {
+            Logger = NullLogger.Instance;
+            LocalizationManager = NullLocalizationManager.Instance;
+        }
+
+        public ILogger Logger { protected get; set; }
+
+        /// <summary>
+        ///     Reference to the setting manager.
+        /// </summary>
+        public ISettingManager SettingManager { get; set; }
+
+        /// <summary>
+        ///     Reference to <see cref="IUnitOfWorkManager" />.
+        /// </summary>
+        public IUnitOfWorkManager UnitOfWorkManager
+        {
+            get
+            {
+                if (_unitOfWorkManager == null)
+                    throw new AbpException("Must set UnitOfWorkManager before use it.");
+
+                return _unitOfWorkManager;
+            }
+            set { _unitOfWorkManager = value; }
+        }
+
+        /// <summary>
+        ///     Gets current unit of work.
+        /// </summary>
+        protected IActiveUnitOfWork CurrentUnitOfWork
+        {
+            get { return UnitOfWorkManager.Current; }
+        }
+
+        /// <summary>
+        ///     Reference to the localization manager.
+        /// </summary>
+        public ILocalizationManager LocalizationManager { get; set; }
+
+        /// <summary>
+        ///     Gets/sets name of the localization source that is used in this application service.
+        ///     It must be set in order to use <see cref="L(string)" /> and <see cref="L(string,CultureInfo)" /> methods.
+        /// </summary>
+        protected string LocalizationSourceName { get; set; }
+
+        /// <summary>
+        ///     Gets localization source.
+        ///     It's valid if <see cref="LocalizationSourceName" /> is set.
+        /// </summary>
+        protected ILocalizationSource LocalizationSource
+        {
+            get
+            {
+                if (LocalizationSourceName == null)
+                    throw new AbpException("Must set LocalizationSourceName before, in order to get LocalizationSource");
+
+                if ((_localizationSource == null) || (_localizationSource.Name != LocalizationSourceName))
+                    _localizationSource = LocalizationManager.GetSource(LocalizationSourceName);
+
+                return _localizationSource;
+            }
+        }
+
+        /// <summary>
+        ///     Reference to the object to object mapper.
+        /// </summary>
+        public IObjectMapper ObjectMapper { get; set; }
+
+        public abstract void Execute(IJobExecutionContext context);
+
+        /// <summary>
+        ///     Gets localized string for given key name and current language.
+        /// </summary>
+        /// <param name="name">Key name</param>
+        /// <returns>Localized string</returns>
+        protected virtual string L(string name)
+        {
+            return LocalizationSource.GetString(name);
+        }
+
+        /// <summary>
+        ///     Gets localized string for given key name and current language with formatting strings.
+        /// </summary>
+        /// <param name="name">Key name</param>
+        /// <param name="args">Format arguments</param>
+        /// <returns>Localized string</returns>
+        protected string L(string name, params object[] args)
+        {
+            return LocalizationSource.GetString(name, args);
+        }
+
+        /// <summary>
+        ///     Gets localized string for given key name and specified culture information.
+        /// </summary>
+        /// <param name="name">Key name</param>
+        /// <param name="culture">culture information</param>
+        /// <returns>Localized string</returns>
+        protected virtual string L(string name, CultureInfo culture)
+        {
+            return LocalizationSource.GetString(name, culture);
+        }
+
+        /// <summary>
+        ///     Gets localized string for given key name and current language with formatting strings.
+        /// </summary>
+        /// <param name="name">Key name</param>
+        /// <param name="culture">culture information</param>
+        /// <param name="args">Format arguments</param>
+        /// <returns>Localized string</returns>
+        protected string L(string name, CultureInfo culture, params object[] args)
+        {
+            return LocalizationSource.GetString(name, culture, args);
+        }
+    }
+}
