09ffd4c3b272e685bc3e70bccd5ca0efe413998c
natiki <donovan@natiki.com.au>
2016-10-20 23:02:11 +1100

    Addition of IRedisCacheSerializer and implementation of ProtoBug serializer.

---
+++ test/Abp.RedisCache.Tests/ProtoBufRedisCacheSerializer_Test.cs
@@ -0,0 +1,39 @@
+﻿using Abp.Runtime.Caching.Redis;
+using Abp.Tests;
+using ProtoBuf;
+using Shouldly;
+using Xunit;
+
+namespace Abp.RedisCache.Tests
+{
+    [ProtoContract]
+    public class ClassToSerialize
+    {
+        [ProtoMember(2)]
+        public int Age { get; set; }
+
+        [ProtoMember(1)]
+        public string Name { get; set; }
+    }
+
+    public class ProtoBufRedisCacheSerializer_Test : TestBaseWithLocalIocManager
+    {
+        [Fact]
+        public void Simple_Serialize_Deserialize_Test()
+        {
+            //Arrange
+            ProtoBufRedisCacheSerializer protoBufSerializer = new ProtoBufRedisCacheSerializer();
+            ClassToSerialize objectToSerialize = new ClassToSerialize {Age = 10, Name = "John"};
+
+            //Act
+            string classSerializedString = protoBufSerializer.Serialize(objectToSerialize, typeof(ClassToSerialize));
+            object classUnSerialized = protoBufSerializer.Deserialize(classSerializedString);
+
+            //Assert
+            classUnSerialized.ShouldBeOfType<ClassToSerialize>();
+            ClassToSerialize classUnSerializedTyped = classUnSerialized as ClassToSerialize;
+            classUnSerializedTyped.Age.ShouldBe(10);
+            classUnSerializedTyped.Name.ShouldBe("John");
+        }
+    }
+}
