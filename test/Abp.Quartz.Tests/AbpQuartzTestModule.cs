3953bfaa8ff6684aa037d1ace3a89d6162f7408d
oguzhan.soykan <oguzhan.soykan@trendyol.com>
2016-10-24 12:46:44 +0300

    Abp.Quartz unit tests for #1290

---
+++ test/Abp.Quartz.Tests/AbpQuartzTestModule.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+using Abp.Modules;
+using Abp.Quartz.Quartz;
+using Abp.Quartz.Quartz.Configuration;
+
+namespace Abp.Quartz.Tests
+{
+    [DependsOn(typeof(AbpQuartzModule))]
+    public class AbpQuartzTestModule : AbpModule
+    {
+        public override void PreInitialize()
+        {
+            Configuration.BackgroundJobs.IsJobExecutionEnabled = true;
+            Configuration.BackgroundJobs.UseQuartz();
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+        }
+    }
+}
