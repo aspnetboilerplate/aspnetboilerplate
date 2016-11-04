b30a7ef09b08a68657ea07747545338d2be65cf2
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-11-04 11:08:00 +0300

    Move PlugInSourceExtensions to it's own file.

--- src/Abp/PlugIns/IPlugInSource.cs
+++ src/Abp/PlugIns/IPlugInSource.cs
@@ -1,24 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using Abp.Modules;
 
 namespace Abp.PlugIns
 {
     public interface IPlugInSource
     {
         List<Type> GetModules();
-    }
-
-    public static class PlugInSourceExtensions
-    {
-        public static List<Type> GetModulesWithAllDependencies(this IPlugInSource plugInSource)
-        {
-            return plugInSource
-                .GetModules()
-                .SelectMany(AbpModule.FindDependedModuleTypesRecursivelyIncludingGivenModule)
-                .Distinct()
-                .ToList();
-        }
     }
 }
