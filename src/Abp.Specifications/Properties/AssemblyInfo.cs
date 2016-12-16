394668f4681bd73134b95b68e89adcc91c774672
赵忍德 <351157970@qq.com>
2016-12-16 19:54:32 +0800

    修改版本号和程序集信息

--- src/Abp.Specifications/Properties/AssemblyInfo.cs
+++ src/Abp.Specifications/Properties/AssemblyInfo.cs
@@ -1,15 +1,18 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Abp.Specifications")]
-[assembly: AssemblyTrademark("")]
 
+[assembly: AssemblyTitle("Abp.Specification")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Volosoft")]
+[assembly: AssemblyProduct("Abp.Specification")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
