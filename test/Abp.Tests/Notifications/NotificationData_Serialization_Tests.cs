726dc610f5c90114ff2bd3c6f712ea0558f84300
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-12-20 14:47:34 +0300

    Make Notification Data backward compatible.

---
+++ test/Abp.Tests/Notifications/NotificationData_Serialization_Tests.cs
@@ -0,0 +1,42 @@
+﻿using Abp.Json;
+using Abp.Localization;
+using Abp.Notifications;
+using Newtonsoft.Json;
+using Shouldly;
+using Xunit;
+
+namespace Abp.Tests.Notifications
+{
+    public class NotificationData_Serialization_Tests
+    {
+        [Fact]
+        public void Should_Deserialize_And_Serialize_MessageNotificationData()
+        {
+            var data = JsonConvert
+                .DeserializeObject(
+                    new MessageNotificationData("Hello World!").ToJsonString(),
+                    typeof(MessageNotificationData)
+                ) as MessageNotificationData;
+
+            Assert.NotNull(data);
+            data.Message.ShouldBe("Hello World!");
+        }
+
+        [Fact]
+        public void Should_Deserialize_And_Serialize_LocalizableMessageNotificationData()
+        {
+            var serialized = new LocalizableMessageNotificationData(new LocalizableString("Hello", "MySource")).ToJsonString();
+
+            var data = JsonConvert
+                .DeserializeObject(
+                    serialized,
+                    typeof(LocalizableMessageNotificationData)
+                ) as LocalizableMessageNotificationData;
+
+            Assert.NotNull(data);
+            Assert.NotNull(data.Message);
+            data.Message.Name.ShouldBe("Hello");
+            data.Message.SourceName.ShouldBe("MySource");
+        }
+    }
+}
