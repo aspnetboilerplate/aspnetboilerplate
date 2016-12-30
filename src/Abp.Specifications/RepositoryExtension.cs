3929876e26bb24e8fd7195002fa05c1c8512f220
赵忍德 <351157970@qq.com>
2016-12-30 11:30:32 +0800

    remove  AbpError 、 AbpGuard 、StringExtensions File change Task.Run  to  Task.FromResult

--- src/Abp.Specifications/RepositoryExtension.cs
+++ src/Abp.Specifications/RepositoryExtension.cs
@@ -6,7 +6,6 @@
 
     using Abp.Domain.Entities;
     using Abp.Domain.Repositories;
-    using Abp.Linq.Extensions;
     using Abp.Specifications;
 
     /// <summary>
@@ -34,54 +33,144 @@
             this IRepository<T, TKey> rep,
             ISpecification<T> specification) where T : Entity<TKey>
         {
-            AbpGuard.NotNull(specification, "specification");
-
+            CheckSpecification(specification);
             var query = rep.GetAll();
-            return await Task.Run(() => query.WhereIf(specification != null, specification.GetExpression()).Any());
+            return await Task.FromResult(query.Where(specification.GetExpression()).Any());
         }
-        
+
+        /// <summary>
+        /// The find.
+        /// </summary>
+        /// <param name="rep">
+        /// The rep.
+        /// </param>
+        /// <param name="specification">
+        /// The specification.
+        /// </param>
+        /// <typeparam name="T">
+        /// </typeparam>
+        /// <typeparam name="TKey">
+        /// </typeparam>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        public async static Task<T> Find<T,TKey>
+            (
+            this IRepository<T, TKey> rep,
+            ISpecification<T> specification) where T : Entity<TKey>
+        {
+            CheckSpecification(specification);
+            return await rep.FirstOrDefaultAsync(specification.GetExpression());
+        }
+
+        /// <summary>
+        /// The find all.
+        /// </summary>
+        /// <param name="rep">
+        /// The rep.
+        /// </param>
+        /// <param name="specification">
+        /// The specification.
+        /// </param>
+        /// <typeparam name="T">
+        /// </typeparam>
+        /// <typeparam name="TKey">
+        /// </typeparam>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
         public static async Task<IQueryable<T>> FindAll<T, TKey>(
 this IRepository<T, TKey> rep,
 ISpecification<T> specification)
 where T : Entity<TKey>
         {
-            AbpGuard.NotNull(specification, "specification");
-            var query = rep.GetAll();
-
-            var result = query.Where(specification.GetExpression());
-
-            return await Task.Run(() => result);
+            CheckSpecification(specification);
+            return await Task.FromResult(rep.GetAll().Where(specification.GetExpression()));
         }
 
+        /// <summary>
+        /// The find all.
+        /// </summary>
+        /// <param name="rep">
+        /// The rep.
+        /// </param>
+        /// <param name="orderBy">
+        /// The order by.
+        /// </param>
+        /// <typeparam name="T">
+        /// </typeparam>
+        /// <typeparam name="TKey">
+        /// </typeparam>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
         public static async Task<IQueryable<T>> FindAll<T, TKey>(
     this IRepository<T, TKey> rep,
     string orderBy)
     where T : Entity<TKey>
         {
             var query = rep.GetAll();
-            
-            var result = query
-                .OrderBy(orderBy);
-
-            return await Task.Run(() => result);
+            return await Task.FromResult(query
+                .OrderBy(orderBy));
         }
 
+        /// <summary>
+        /// The find all.
+        /// </summary>
+        /// <param name="rep">
+        /// The rep.
+        /// </param>
+        /// <param name="specification">
+        /// The specification.
+        /// </param>
+        /// <param name="orderBy">
+        /// The order by.
+        /// </param>
+        /// <typeparam name="T">
+        /// </typeparam>
+        /// <typeparam name="TKey">
+        /// </typeparam>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
         public static async Task<IQueryable<T>> FindAll<T, TKey>(
         this IRepository<T, TKey> rep,
         ISpecification<T> specification,
         string orderBy)
         where T : Entity<TKey>
         {
-            AbpGuard.NotNull(specification, "specification");
+            CheckSpecification(specification);
             var query = rep.GetAll();
-            query = query.WhereIf(specification != null, specification.GetExpression());
-
-            var result = query
-                .OrderBy(orderBy);
-
-            return await Task.Run(() => result);
+            
+            return await Task.FromResult(query
+                .OrderBy(orderBy));
         }
 
+        /// <summary>
+        /// The find all.
+        /// </summary>
+        /// <param name="rep">
+        /// The rep.
+        /// </param>
+        /// <param name="specification">
+        /// The specification.
+        /// </param>
+        /// <param name="skip">
+        /// The skip.
+        /// </param>
+        /// <param name="take">
+        /// The take.
+        /// </param>
+        /// <param name="orderBy">
+        /// The order by.
+        /// </param>
+        /// <typeparam name="T">
+        /// </typeparam>
+        /// <typeparam name="TKey">
+        /// </typeparam>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
         public static async Task<IQueryable<T>> FindAll<T, TKey>(
             this IRepository<T, TKey> rep,
             ISpecification<T> specification,
@@ -90,31 +179,57 @@ where T : Entity<TKey>
             string orderBy)
             where T : Entity<TKey>
         {
-            AbpGuard.NotNull(specification, "specification");
+            CheckSpecification(specification);
             var query = rep.GetAll();
-            query = query.WhereIf(specification != null, specification.GetExpression());
-
-            var result = query
+            
+            return await Task.FromResult(query
                 .OrderBy(orderBy)
                 .Skip(skip)
-                .Take(take);
-
-            return await Task.Run(() =>result);
+                .Take(take));
         }
-        
+
+        /// <summary>
+        /// The count.
+        /// </summary>
+        /// <param name="rep">
+        /// The rep.
+        /// </param>
+        /// <param name="specification">
+        /// The specification.
+        /// </param>
+        /// <typeparam name="T">
+        /// </typeparam>
+        /// <typeparam name="TKey">
+        /// </typeparam>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
         public static async Task<int> Count<T, TKey>(
             this IRepository<T, TKey> rep, 
             ISpecification<T> specification)
             where T : Entity<TKey>
         {
-            AbpGuard.NotNull(specification, "specification");
-            var query = rep.GetAll();
-            return await Task.Run(() =>
-            query.WhereIf(specification != null,
-            specification.GetExpression()).Count());
+            CheckSpecification(specification);
+            return await rep.CountAsync(specification.GetExpression());
         }
 
-
-
+        /// <summary>
+        /// The checkspecification.
+        /// </summary>
+        /// <param name="specification">
+        /// The specification.
+        /// </param>
+        /// <typeparam name="T">
+        /// Entity
+        /// </typeparam>
+        /// <exception cref="AbpException">
+        /// </exception>
+        private static void CheckSpecification<T>(ISpecification<T> specification)
+        {
+            if (specification == null)
+            {
+                throw new AbpException("specification is null");
+            }
+        }
     }
 }
