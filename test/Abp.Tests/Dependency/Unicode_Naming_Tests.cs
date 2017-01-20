ceea981c89eed75e43226413b18b74ce4c9a5e80
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-01-20 17:10:09 +0300

    Added unit tests to show unicode naming support of castle.

---
+++ test/Abp.Tests/Dependency/Unicode_Naming_Tests.cs
@@ -0,0 +1,50 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Shouldly;
+using Xunit;
+
+namespace Abp.Tests.Dependency
+{
+    public class Unicode_Naming_Tests
+    {
+        [Fact]
+        public void Castle_Should_Support_Unicode_Class_Names()
+        {
+            var container = new WindsorContainer();
+
+            container.Register(
+                Component.For<Iお知らせAppService>().ImplementedBy<お知らせAppService>().LifestyleTransient()
+            );
+
+            container.Resolve<Iお知らせAppService>().ShouldBeOfType<お知らせAppService>();
+        }
+
+        //[Fact] This test is failing because Castle Windsor does not support it
+        public void Castle_Should_Register_Unicode_Names_In_Conventions()
+        {
+            var container = new WindsorContainer();
+
+            container.Register(
+                Classes
+                    .FromThisAssembly()
+                    .Where(c => c == typeof(お知らせAppService))
+                    .WithServiceDefaultInterfaces()
+                    .WithServiceSelf()
+                    .LifestyleTransient()
+            );
+
+            container.Resolve<お知らせAppService>().ShouldBeOfType<お知らせAppService>();
+            container.Resolve<Iお知らせAppService>().ShouldBeOfType<お知らせAppService>();
+        }
+
+        public interface Iお知らせAppService
+        {
+            
+        }
+
+        public class お知らせAppService : Iお知らせAppService
+        {
+
+        }
+    }
+}
