099379988acd859132cd20694e9460578dbf4be6
LewYue <lewyue@outlook.com>
2017-01-23 13:46:06 +0800

    Add tests for specification about Operater and EntitySpecification

--- test/Abp.Tests/Specifications/Specification_Tests.cs
+++ test/Abp.Tests/Specifications/Specification_Tests.cs
@@ -5,6 +5,9 @@ using System.Linq.Expressions;
 using Abp.Specifications;
 using Shouldly;
 using Xunit;
+using Abp.Domain.Specifications;
+using Abp.Domain.Entities;
+using Abp.Extensions;
 
 namespace Abp.Tests.Specifications
 {
@@ -16,16 +19,16 @@ namespace Abp.Tests.Specifications
         {
             _customers = new List<Customer>
             {
-                new Customer("John", 17, 47000, "England"),
-                new Customer("Tuana", 2, 500, "Turkey"),
-                new Customer("Martin", 43, 16000, "USA"),
-                new Customer("Lee", 32, 24502, "China"),
-                new Customer("Douglas", 42, 42000, "England"),
-                new Customer("Abelard", 14, 2332, "German"),
-                new Customer("Neo", 16, 120000, "USA"),
-                new Customer("Daan", 39, 6000, "Netherlands"),
-                new Customer("Alessandro", 22, 8271, "Italy"),
-                new Customer("Noah", 33, 82192, "Belgium")
+                new Customer(1,"John", 17, 47000, "England"),
+                new Customer(2,"Tuana", 2, 500, "Turkey"),
+                new Customer(3,"Martin", 43, 16000, "USA"),
+                new Customer(4,"Lee", 32, 24502, "China"),
+                new Customer(5,"Douglas", 42, 42000, "England"),
+                new Customer(6,"Abelard", 14, 2332, "German"),
+                new Customer(7,"Neo", 16, 120000, "USA"),
+                new Customer(8,"Daan", 39, 6000, "Netherlands"),
+                new Customer(9,"Alessandro", 22, 8271, "Italy"),
+                new Customer(10,"Noah", 33, 82192, "Belgium")
             }.AsQueryable();
         }
 
@@ -60,12 +63,12 @@ namespace Abp.Tests.Specifications
         public void Should_Support_Native_Expressions_And_Combinations()
         {
             _customers
-                .Where(new ExpressionSpecification<Customer>(c => c.Age >= 18).ToExpression())
+                .Where(new DirectSpecification<Customer>(c => c.Age >= 18).ToExpression())
                 .Count()
                 .ShouldBe(6);
 
             _customers
-                .Where(new EuropeanCustomerSpecification().And(new ExpressionSpecification<Customer>(c => c.Age >= 18)).ToExpression())
+                .Where(new EuropeanCustomerSpecification().And(new DirectSpecification<Customer>(c => c.Age >= 18)).ToExpression())
                 .Count()
                 .ShouldBe(4);
         }
@@ -97,10 +100,10 @@ namespace Abp.Tests.Specifications
         [Fact]
         public void IsSatisfiedBy_Tests()
         {
-            new PremiumCustomerSpecification().IsSatisfiedBy(new Customer("David", 49, 55000, "USA")).ShouldBeTrue();
+            new PremiumCustomerSpecification().IsSatisfiedBy(new Customer(1, "David", 49, 55000, "USA")).ShouldBeTrue();
 
-            new PremiumCustomerSpecification().IsSatisfiedBy(new Customer("David", 49, 200, "USA")).ShouldBeFalse();
-            new PremiumCustomerSpecification().IsSatisfiedBy(new Customer("David", 12, 55000, "USA")).ShouldBeFalse();
+            new PremiumCustomerSpecification().IsSatisfiedBy(new Customer(2, "David", 49, 200, "USA")).ShouldBeFalse();
+            new PremiumCustomerSpecification().IsSatisfiedBy(new Customer(3, "David", 12, 55000, "USA")).ShouldBeFalse();
         }
 
         [Fact]
@@ -117,75 +120,190 @@ namespace Abp.Tests.Specifications
                .ShouldBe(2);
 
             _customers
-                .Where(new Age18PlusCustomerSpecification().AndNot(new EuropeanCustomerSpecification()).ToExpression())
+                .Where(new Age18PlusCustomerSpecification() & !(new EuropeanCustomerSpecification()))
                 .Count()
                 .ShouldBe(2);
         }
 
-        private class Customer
+        [Fact]
+        public void EntitySpecification_Tests()
         {
-            public string Name { get; private set; }
+            var spec = CustomerSpecification.Any();
+            _customers.Where(spec).Count().ShouldBe(10);
 
-            public byte Age { get; private set; }
+            spec = CustomerSpecification.IsKey(2);
+            _customers.Where(spec).Count().ShouldBe(1);
 
-            public long Balance { get; private set; }
+            spec = CustomerSpecification.InKeys(new List<int>() { 1, 3, 5 });
+            _customers.Where(spec).Count().ShouldBe(3);
 
-            public string Location { get; private set; }
+            spec = CustomerSpecification.FromEngland()
+                & CustomerSpecification.Grown();
+            _customers.Where(spec).Count().ShouldBe(1);
 
-            public Customer(string name, byte age, long balance, string location)
-            {
-                Name = name;
-                Age = age;
-                Balance = balance;
-                Location = location;
-            }
+            spec = CustomerSpecification.GrownBigCustomer();
+            _customers.Where(spec).Count().ShouldBe(4);
         }
 
-        private class EuropeanCustomerSpecification : Specification<Customer>
+        [Fact]
+        public void EntitySpecification_Operater_Tests()
         {
-            public override Expression<Func<Customer, bool>> ToExpression()
-            {
-                return c => c.Location == "England" ||
-                            c.Location == "Turkey" ||
-                            c.Location == "German" ||
-                            c.Location == "Netherlands" ||
-                            c.Location == "Italy" ||
-                            c.Location == "Belgium";
-            }
+
+            var spec = CustomerSpecification.FromEngland()
+                 & CustomerSpecification.Grown();
+            _customers.Where(spec).Count().ShouldBe(1);
+
+            spec = CustomerSpecification.FromEngland()
+                | CustomerSpecification.FromUSA();
+            _customers.Where(spec).Count().ShouldBe(4);
+
+            spec = !CustomerSpecification.FromEngland()
+                & CustomerSpecification.Grown();
+            _customers.Where(spec).Count().ShouldBe(5);
         }
 
-        private class Age18PlusCustomerSpecification : Specification<Customer>
+        [Fact]
+        public void EntitySpecification_Condition_Tests()
         {
-            public override Expression<Func<Customer, bool>> ToExpression()
-            {
-                return c => c.Age >= 18;
-            }
+            //Define Condition,we alawys use Nullable<T> to ensure this condition need to be set in search
+            string nameStartWith = "A";
+            int? ageMaxThan = null;
+            int? balanceMaxThan = 5000;
+
+            //Construct Spec
+            var spec = CustomerSpecification.Any()
+                .AndIf(!nameStartWith.IsNullOrEmpty(), CustomerSpecification.NameStartWith(nameStartWith))
+                .AndIf(ageMaxThan.HasValue, () => { return CustomerSpecification.AgeMaxThan(ageMaxThan.Value); })
+                .AndIf(balanceMaxThan.HasValue, () => { return CustomerSpecification.BalanceMaxThan(balanceMaxThan.Value); });
+
+            _customers.Where(spec).Count().ShouldBe(1);
+
+
+            nameStartWith = "";
+            ageMaxThan = 20;
+            balanceMaxThan = 5000;
+
+            spec = CustomerSpecification.Any()
+                .AndIf(!nameStartWith.IsNullOrEmpty(), CustomerSpecification.NameStartWith(nameStartWith))
+                .AndIf(ageMaxThan.HasValue, () => { return CustomerSpecification.AgeMaxThan(ageMaxThan.Value); })
+                .AndIf(balanceMaxThan.HasValue, () => { return CustomerSpecification.BalanceMaxThan(balanceMaxThan.Value); });
+
+            _customers.Where(spec).Count().ShouldBe(6);
+        }
+    }
+
+    public class Customer : Entity<int>
+    {
+
+        public string Name { get; private set; }
+
+        public byte Age { get; private set; }
+
+        public long Balance { get; private set; }
+
+        public string Location { get; private set; }
+
+        public Customer(int id, string name, byte age, long balance, string location)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Balance = balance;
+            Location = location;
+        }
+    }
+
+    public class EuropeanCustomerSpecification : Specification<Customer>
+    {
+        public override Expression<Func<Customer, bool>> SatisfiedBy()
+        {
+            return c => c.Location == "England" ||
+                        c.Location == "Turkey" ||
+                        c.Location == "German" ||
+                        c.Location == "Netherlands" ||
+                        c.Location == "Italy" ||
+                        c.Location == "Belgium";
+        }
+    }
+
+    public class Age18PlusCustomerSpecification : Specification<Customer>
+    {
+        public override Expression<Func<Customer, bool>> SatisfiedBy()
+        {
+            return c => c.Age >= 18;
+        }
+    }
+
+    public class BalanceCustomerSpecification : Specification<Customer>
+    {
+        public int MinBalance { get; }
+
+        public int MaxBalance { get; }
+
+        public BalanceCustomerSpecification(int minBalance, int maxBalance)
+        {
+            MinBalance = minBalance;
+            MaxBalance = maxBalance;
         }
 
-        private class BalanceCustomerSpecification : Specification<Customer>
+        public override Expression<Func<Customer, bool>> SatisfiedBy()
         {
-            public int MinBalance { get; }
+            return c => c.Balance >= MinBalance && c.Balance <= MaxBalance;
+        }
+    }
 
-            public int MaxBalance { get; }
+    public class PremiumCustomerSpecification : Specification<Customer>
+    {
+        private Specification<Customer> _RightSideSpecification = null;
+        private Specification<Customer> _LeftSideSpecification = null;
 
-            public BalanceCustomerSpecification(int minBalance, int maxBalance)
-            {
-                MinBalance = minBalance;
-                MaxBalance = maxBalance;
-            }
-
-            public override Expression<Func<Customer, bool>> ToExpression()
-            {
-                return c => c.Balance >= MinBalance && c.Balance <= MaxBalance;
-            }
+        public PremiumCustomerSpecification()
+        {
+            _RightSideSpecification = new Age18PlusCustomerSpecification();
+            _LeftSideSpecification = new BalanceCustomerSpecification(20000, int.MaxValue);
         }
 
-        private class PremiumCustomerSpecification : AndSpecification<Customer>
+        public override Expression<Func<Customer, bool>> SatisfiedBy()
         {
-            public PremiumCustomerSpecification()
-                : base(new Age18PlusCustomerSpecification(), new BalanceCustomerSpecification(20000, int.MaxValue))
-            {
-            }
+            return (_RightSideSpecification & _LeftSideSpecification).SatisfiedBy();
+        }
+    }
+
+    public class CustomerSpecification : EntitySpecification<Customer, int>
+    {
+        public static Specification<Customer> AgeMaxThan(int age)
+        {
+            return new DirectSpecification<Customer>(c => c.Age >= age);
+        }
+
+        public static Specification<Customer> Grown()
+        {
+            return new DirectSpecification<Customer>(c => c.Age >= 18);
+        }
+
+        public static Specification<Customer> FromEngland()
+        {
+            return new DirectSpecification<Customer>(c => c.Location == "England");
+        }
+
+        public static Specification<Customer> FromUSA()
+        {
+            return new DirectSpecification<Customer>(c => c.Location == "USA");
+        }
+
+        public static Specification<Customer> NameStartWith(string nameIndex)
+        {
+            return new DirectSpecification<Customer>(c => c.Name.StartsWith(nameIndex));
+        }
+
+        public static Specification<Customer> GrownBigCustomer()
+        {
+            return new DirectSpecification<Customer>(c => c.Age >= 18 && c.Balance >= 10000);
+        }
+
+        public static Specification<Customer> BalanceMaxThan(int balance)
+        {
+            return new DirectSpecification<Customer>(c => c.Balance >= balance);
         }
     }
 }
