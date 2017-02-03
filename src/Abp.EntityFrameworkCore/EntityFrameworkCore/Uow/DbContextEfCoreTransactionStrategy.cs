43a1627ee453c2b4bff9b736dd16ad18d82894d1
Diogo Damiani <diogodamiani@gmail.com>
2017-02-03 14:49:24 -0200

    Add EfCoreTransactionStrategy

---
+++ src/Abp.EntityFrameworkCore/EntityFrameworkCore/Uow/DbContextEfCoreTransactionStrategy.cs
@@ -0,0 +1,33 @@
+﻿using Abp.Domain.Uow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Abp.Dependency;
+using Microsoft.EntityFrameworkCore;
+
+namespace Abp.EntityFrameworkCore.Uow
+{
+  public class DbContextEfCoreTransactionStrategy : IEfCoreTransactionStrategy
+  {
+    public void Commit()
+    {
+      throw new NotImplementedException();
+    }
+
+    public DbContext CreateDbContext<TDbContext>(string connectionString, IDbContextResolver dbContextResolver) where TDbContext : DbContext
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Dispose(IIocResolver iocResolver)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void InitOptions(UnitOfWorkOptions options)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
