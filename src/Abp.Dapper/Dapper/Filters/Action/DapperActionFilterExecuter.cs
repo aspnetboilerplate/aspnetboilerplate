652debc7a59ae0dd4df6b101920971918b3a2b68
osoykan <oguzhansoykan@gmail.com>
2017-03-22 00:47:15 +0300

    DapperActionFilter ioc resolver refactor

--- src/Abp.Dapper/Dapper/Filters/Action/DapperActionFilterExecuter.cs
+++ src/Abp.Dapper/Dapper/Filters/Action/DapperActionFilterExecuter.cs
@@ -1,36 +1,30 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-
-using Abp.Dependency;
+﻿using Abp.Dependency;
 using Abp.Domain.Entities;
 
 namespace Abp.Dapper.Filters.Action
 {
     public class DapperActionFilterExecuter : IDapperActionFilterExecuter, ITransientDependency
     {
-        private readonly IEnumerable<IDapperActionFilter> _actionFilters;
+        private readonly IIocResolver _iocResolver;
 
         public DapperActionFilterExecuter(IIocResolver iocResolver)
         {
-            _actionFilters = iocResolver.ResolveAll<IDapperActionFilter>();
+            _iocResolver = iocResolver;
         }
 
         public void ExecuteCreationAuditFilter<TEntity, TPrimaryKey>(TEntity entity) where TEntity : class, IEntity<TPrimaryKey>
         {
-            IDapperActionFilter filter = _actionFilters.First(x => x.GetType() == typeof(CreationAuditDapperActionFilter));
-            filter.ExecuteFilter<TEntity, TPrimaryKey>(entity);
+            _iocResolver.Resolve<CreationAuditDapperActionFilter>().ExecuteFilter<TEntity, TPrimaryKey>(entity);
         }
 
         public void ExecuteModificationAuditFilter<TEntity, TPrimaryKey>(TEntity entity) where TEntity : class, IEntity<TPrimaryKey>
         {
-            IDapperActionFilter filter = _actionFilters.First(x => x.GetType() == typeof(ModificationAuditDapperActionFilter));
-            filter.ExecuteFilter<TEntity, TPrimaryKey>(entity);
+            _iocResolver.Resolve<ModificationAuditDapperActionFilter>().ExecuteFilter<TEntity, TPrimaryKey>(entity);
         }
 
         public void ExecuteDeletionAuditFilter<TEntity, TPrimaryKey>(TEntity entity) where TEntity : class, IEntity<TPrimaryKey>
         {
-            IDapperActionFilter filter = _actionFilters.First(x => x.GetType() == typeof(DeletionAuditDapperActionFilter));
-            filter.ExecuteFilter<TEntity, TPrimaryKey>(entity);
+            _iocResolver.Resolve<DeletionAuditDapperActionFilter>().ExecuteFilter<TEntity, TPrimaryKey>(entity);
         }
     }
 }
