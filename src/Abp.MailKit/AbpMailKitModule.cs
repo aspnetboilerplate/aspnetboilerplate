22597675f90875c9053de813cd273132fae33e9f
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2017-04-12 15:37:05 +0300

    Deleted test configuration

--- src/Abp.MailKit/AbpMailKitModule.cs
+++ src/Abp.MailKit/AbpMailKitModule.cs
@@ -14,11 +14,6 @@ namespace Abp.MailKit
             IocManager.Register<IAbpMailKitConfiguration, AbpMailKitConfiguration>(DependencyLifeStyle.Singleton);
 
             Configuration.ReplaceService<IEmailSender, MailKitEmailSender>(DependencyLifeStyle.Transient);
-
-            Configuration.Modules.AbpMailKit().SmtpClientConfigurer = client =>
-            {
-                client.ServerCertificateValidationCallback = (sender, certificate, chain, errors) => true;
-            };
         }
 
         public override void Initialize()
