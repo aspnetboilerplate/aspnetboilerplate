6a4bf4470aed3e83cce49582fccff27f57b9334c
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-04-14 15:20:47 +0300

    Should specifically call UseHangfire in order to replace IBackgroundJobManager.

--- src/Abp.HangFire.AspNetCore/Hangfire/AbpHangfireAspNetCoreModule.cs
+++ src/Abp.HangFire.AspNetCore/Hangfire/AbpHangfireAspNetCoreModule.cs
@@ -1,19 +1,11 @@
-﻿using Abp.BackgroundJobs;
-using Abp.Modules;
+﻿using Abp.Modules;
 using Abp.Reflection.Extensions;
-using Abp.Configuration.Startup;
-using Abp.Dependency;
 
 namespace Abp.Hangfire
 {
     [DependsOn(typeof(AbpKernelModule))]
     public class AbpHangfireAspNetCoreModule : AbpModule
     {
-        public override void PreInitialize()
-        {
-            Configuration.ReplaceService<IBackgroundJobManager, HangfireBackgroundJobManager>(DependencyLifeStyle.Singleton);
-        }
-
         public override void Initialize()
         {
             IocManager.RegisterAssemblyByConvention(typeof(AbpHangfireAspNetCoreModule).GetAssembly());
