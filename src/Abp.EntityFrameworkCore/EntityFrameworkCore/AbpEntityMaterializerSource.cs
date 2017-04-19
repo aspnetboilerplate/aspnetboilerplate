f187b8e4b118dabcfaa06ce01b0b67cf390f18c7
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-04-19 17:46:09 +0300

    #2048 Handle nullable DateTime on DateTime.Kind normalization

--- src/Abp.EntityFrameworkCore/EntityFrameworkCore/AbpEntityMaterializerSource.cs
+++ src/Abp.EntityFrameworkCore/EntityFrameworkCore/AbpEntityMaterializerSource.cs
@@ -9,19 +9,43 @@ namespace Abp.EntityFrameworkCore
 {
     public class AbpEntityMaterializerSource : EntityMaterializerSource
     {
-        private static readonly MethodInfo NormalizeMethod = typeof(Clock).GetTypeInfo().GetMethod(nameof(Clock.Normalize));
+        private static readonly MethodInfo NormalizeDateTimeMethod = typeof(AbpEntityMaterializerSource).GetTypeInfo().GetMethod(nameof(NormalizeDateTime), BindingFlags.Static | BindingFlags.NonPublic);
+        private static readonly MethodInfo NormalizeNullableDateTimeMethod = typeof(AbpEntityMaterializerSource).GetTypeInfo().GetMethod(nameof(NormalizeNullableDateTime), BindingFlags.Static | BindingFlags.NonPublic);
 
         public override Expression CreateReadValueExpression(Expression valueBuffer, Type type, int index, IProperty property = null)
         {
             if (type == typeof(DateTime))
             {
                 return Expression.Call(
-                    NormalizeMethod,
+                    NormalizeDateTimeMethod,
+                    base.CreateReadValueExpression(valueBuffer, type, index, property)
+                );
+            }
+
+            if (type == typeof(DateTime?))
+            {
+                return Expression.Call(
+                    NormalizeNullableDateTimeMethod,
                     base.CreateReadValueExpression(valueBuffer, type, index, property)
                 );
             }
 
             return base.CreateReadValueExpression(valueBuffer, type, index, property);
+        }
+
+        private static DateTime NormalizeDateTime(DateTime value)
+        {
+            return Clock.Normalize(value);
+        }
+
+        private static DateTime? NormalizeNullableDateTime(DateTime? value)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+
+            return Clock.Normalize(value.Value);
         }
     }
 }
