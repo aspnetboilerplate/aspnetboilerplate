2f79821b3c09132394ec01f1673b7fad8e6893af
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-05-18 09:38:07 +0300

    Fixed #2131: Creation and Modification audit values should not be set for cross database users.

--- src/Abp/MultiTenancy/MultiTenancySideAttribute.cs
+++ src/Abp/MultiTenancy/MultiTenancySideAttribute.cs
@@ -5,6 +5,7 @@ namespace Abp.MultiTenancy
     /// <summary>
     /// Used to declare multi tenancy side of an object.
     /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Method | AttributeTargets.Interface)]
     public class MultiTenancySideAttribute : Attribute
     {
         /// <summary>
