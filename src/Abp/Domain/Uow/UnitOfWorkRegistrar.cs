ee956995b63236f86f0fdabdb046482a17967a31
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-05-19 10:54:40 +0300

    Fixed #2143: AspNet Core - Unit of work types are not registered for types registered before AddAbp (in v2.1.0-beta2)

--- src/Abp/Domain/Uow/UnitOfWorkRegistrar.cs
+++ src/Abp/Domain/Uow/UnitOfWorkRegistrar.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using System.Reflection;
 using Abp.Dependency;
 using Castle.Core;
+using Castle.MicroKernel;
 
 namespace Abp.Domain.Uow
 {
@@ -18,27 +19,46 @@ namespace Abp.Domain.Uow
         {
             iocManager.IocContainer.Kernel.ComponentRegistered += (key, handler) =>
             {
-                if (!iocManager.IsRegistered<IUnitOfWorkDefaultOptions>())
-                {
-                    return;
-                }
+                var implementationType = handler.ComponentModel.Implementation.GetTypeInfo();
 
-                var uowOptions = iocManager.Resolve<IUnitOfWorkDefaultOptions>();
-
-                if (uowOptions.IsConventionalUowClass(handler.ComponentModel.Implementation))
-                {
-                    handler.ComponentModel.Interceptors.Add(new InterceptorReference(typeof(UnitOfWorkInterceptor)));
-                }
-                else if (UnitOfWorkHelper.HasUnitOfWorkAttribute(handler.ComponentModel.Implementation.GetTypeInfo()) &&
-                        handler.ComponentModel
-                               .Implementation
-                               .GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
-                               .Any(UnitOfWorkHelper.HasUnitOfWorkAttribute))
-                {
-                    //Intercept all methods of classes those have at least one method that has UnitOfWork attribute or class marked as UnitOfWork.
-                    handler.ComponentModel.Interceptors.Add(new InterceptorReference(typeof(UnitOfWorkInterceptor)));
-                }
+                HandleTypesWithUnitOfWorkAttribute(implementationType, handler);
+                HandleConventionalUnitOfWorkTypes(iocManager, implementationType, handler);
             };
+        }
+
+        private static void HandleTypesWithUnitOfWorkAttribute(TypeInfo implementationType, IHandler handler)
+        {
+            if (IsUnitOfWorkType(implementationType) || AnyMethodHasUnitOfWork(implementationType))
+            {
+                handler.ComponentModel.Interceptors.Add(new InterceptorReference(typeof(UnitOfWorkInterceptor)));
+            }
+        }
+
+        private static void HandleConventionalUnitOfWorkTypes(IIocManager iocManager, TypeInfo implementationType, IHandler handler)
+        {
+            if (!iocManager.IsRegistered<IUnitOfWorkDefaultOptions>())
+            {
+                return;
+            }
+
+            var uowOptions = iocManager.Resolve<IUnitOfWorkDefaultOptions>();
+
+            if (uowOptions.IsConventionalUowClass(implementationType.AsType()))
+            {
+                handler.ComponentModel.Interceptors.Add(new InterceptorReference(typeof(UnitOfWorkInterceptor)));
+            }
+        }
+
+        private static bool IsUnitOfWorkType(TypeInfo implementationType)
+        {
+            return UnitOfWorkHelper.HasUnitOfWorkAttribute(implementationType);
+        }
+
+        private static bool AnyMethodHasUnitOfWork(TypeInfo implementationType)
+        {
+            return implementationType
+                .GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                .Any(UnitOfWorkHelper.HasUnitOfWorkAttribute);
         }
     }
 }
