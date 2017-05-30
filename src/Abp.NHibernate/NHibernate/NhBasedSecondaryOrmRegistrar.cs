b5c0399290e37394eed00fec6f36e2094d04c5d5
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-05-30 06:19:00 +0300

    Use AutoRepositoryTypesAttribute.WithDefaultRepositoryInterfaces for NhBasedSecondaryOrmRegistrar.

--- src/Abp.NHibernate/NHibernate/NhBasedSecondaryOrmRegistrar.cs
+++ src/Abp.NHibernate/NHibernate/NhBasedSecondaryOrmRegistrar.cs
@@ -1,6 +1,7 @@
 ﻿using Abp.Dependency;
 using Abp.Domain.Repositories;
 using Abp.Orm;
+using Castle.MicroKernel.Registration;
 
 namespace Abp.NHibernate
 {
@@ -10,8 +11,18 @@ namespace Abp.NHibernate
 
         public void RegisterRepositories(IIocManager iocManager, AutoRepositoryTypesAttribute defaultRepositoryTypes)
         {
-            iocManager.Register(defaultRepositoryTypes.RepositoryInterface, defaultRepositoryTypes.RepositoryImplementation);
-            iocManager.Register(defaultRepositoryTypes.RepositoryInterfaceWithPrimaryKey, defaultRepositoryTypes.RepositoryImplementationWithPrimaryKey);
+            if (defaultRepositoryTypes.WithDefaultRepositoryInterfaces)
+            {
+                iocManager.IocContainer.Register(
+                    Component.For(typeof(IRepository<>),defaultRepositoryTypes.RepositoryInterface).ImplementedBy(defaultRepositoryTypes.RepositoryImplementation).LifestyleTransient(),
+                    Component.For(typeof(IRepository<,>),defaultRepositoryTypes.RepositoryInterfaceWithPrimaryKey).ImplementedBy(defaultRepositoryTypes.RepositoryImplementationWithPrimaryKey).LifestyleTransient()
+                );
+            }
+            else
+            {
+                iocManager.Register(defaultRepositoryTypes.RepositoryInterface, defaultRepositoryTypes.RepositoryImplementation, DependencyLifeStyle.Transient);
+                iocManager.Register(defaultRepositoryTypes.RepositoryInterfaceWithPrimaryKey, defaultRepositoryTypes.RepositoryImplementationWithPrimaryKey, DependencyLifeStyle.Transient);
+            }
         }
     }
 }
