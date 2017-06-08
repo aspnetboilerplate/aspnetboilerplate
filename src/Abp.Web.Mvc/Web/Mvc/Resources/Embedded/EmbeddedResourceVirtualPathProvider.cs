1b75d3a05cee534683b1a7f00ac4510988301932
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-06-08 06:01:58 +0300

    Fixed #2185: AspNet MVC 5.x embedded views path problem when hosting under IIS virtual directory.

--- src/Abp.Web.Mvc/Web/Mvc/Resources/Embedded/EmbeddedResourceVirtualPathProvider.cs
+++ src/Abp.Web.Mvc/Web/Mvc/Resources/Embedded/EmbeddedResourceVirtualPathProvider.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Web;
 using System.Web.Caching;
 using System.Web.Hosting;
 using Abp.Dependency;
@@ -56,7 +57,7 @@ namespace Abp.Web.Mvc.Resources.Embedded
 
         private EmbeddedResourceItem GetResource(string virtualPath)
         {
-            return _embeddedResourceManager.GetResource(virtualPath.RemovePreFix("~"));
+            return _embeddedResourceManager.GetResource(VirtualPathUtility.ToAppRelative(virtualPath).RemovePreFix("~"));
         }
     }
 }
