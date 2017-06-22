dddafc1fa0cb10a2356023fe72dd71427f0c275c
ShiningRush <277040271@qq.com>
2017-06-22 16:29:50 +0800

    fixd datetimebinder bug that value could be null or a value which can not be convert to datetime, it will cause a invalidoperation exception which could not be builded to ErrorInfo correctly by ErrorInfoBuilder.

--- src/Abp.Web.Api/WebApi/Controllers/Dynamic/Binders/AbpApiDateTimeBinder.cs
+++ src/Abp.Web.Api/WebApi/Controllers/Dynamic/Binders/AbpApiDateTimeBinder.cs
@@ -14,8 +14,14 @@ namespace Abp.WebApi.Controllers.Dynamic.Binders
         public bool BindModel(HttpActionContext actionContext, ModelBindingContext bindingContext)
         {
             var value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
-            var date = value?.ConvertTo(typeof(DateTime?), CultureInfo.CurrentCulture) as DateTime?;
-            if (date != null)
+            DateTime? date = null;
+            try
+            {
+                date = value?.ConvertTo(typeof(DateTime?), CultureInfo.CurrentCulture) as DateTime?;
+            }
+            catch { }
+
+            if (date.HasValue)
             {
                 bindingContext.Model = Clock.Normalize(date.Value);
             }
