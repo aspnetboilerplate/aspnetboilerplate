2fdaeacb8828b1ebb602cbe0dc0a38c70b9226da
alirıza adıyahşi <alirizaadiyahsi@gmail.com>
2017-07-12 16:57:40 +0300

    resolved #2304 EntityNotFoundException handling in DefaultErrorInfoConverter

---
+++ test/Abp.Web.Common.Tests/Web/DefaultErrorInfoConverter.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Abp.Domain.Entities;
+using Abp.TestBase;
+using Abp.Web.Configuration;
+using Abp.Web.Models;
+using Shouldly;
+using Xunit;
+
+namespace Abp.Web.Common.Tests.Web
+{
+    public class DefaultErrorInfoConverter_Tests : AbpIntegratedTestBase<AbpWebCommonTestModule>
+    {
+        private readonly DefaultErrorInfoConverter _defaultErrorInfoConverter;
+
+        public DefaultErrorInfoConverter_Tests()
+        {
+            _defaultErrorInfoConverter = Resolve<DefaultErrorInfoConverter>();
+        }
+
+        [Fact]
+        public async Task DefaultErrorInfoConverter_Should_Work_For_EntityNotFoundException_Overload_Methods()
+        {
+            var message = "Test message";
+            var errorInfo = _defaultErrorInfoConverter.Convert(new EntityNotFoundException(message));
+
+            Assert.Equal(errorInfo.Message, message);
+
+            var exception = new EntityNotFoundException();
+            errorInfo = _defaultErrorInfoConverter.Convert(exception);
+
+            Assert.Equal(errorInfo.Message, exception.Message);
+        }
+    }
+}
