ed32b2a9acb8e794ade1c773941d65ed471bc22a
alirıza adıyahşi <alirizaadiyahsi@gmail.com>
2017-07-18 09:40:27 +0300

    Resolved #2311 Load web common's localization file fail!

--- test/Abp.Tests/Localization/XmlEmbeddedFileLocalizationDictionaryProvider_Tests.cs
+++ test/Abp.Tests/Localization/XmlEmbeddedFileLocalizationDictionaryProvider_Tests.cs
@@ -1,7 +1,7 @@
 ﻿using System.Linq;
-using System.Reflection;
 using Abp.Localization.Dictionaries.Xml;
 using Abp.Reflection.Extensions;
+using Abp.Web;
 using Shouldly;
 using Xunit;
 
@@ -9,33 +9,55 @@ namespace Abp.Tests.Localization
 {
     public class XmlEmbeddedFileLocalizationDictionaryProvider_Tests
     {
-        private readonly XmlEmbeddedFileLocalizationDictionaryProvider _dictionaryProvider;
+        private readonly XmlEmbeddedFileLocalizationDictionaryProvider _dictionaryProviderTest;
+        private readonly XmlEmbeddedFileLocalizationDictionaryProvider _dictionaryProviderProd;
 
         public XmlEmbeddedFileLocalizationDictionaryProvider_Tests()
         {
-            _dictionaryProvider = new XmlEmbeddedFileLocalizationDictionaryProvider(
+            _dictionaryProviderTest = new XmlEmbeddedFileLocalizationDictionaryProvider(
                 typeof(XmlEmbeddedFileLocalizationDictionaryProvider_Tests).GetAssembly(),
                 "Abp.Tests.Localization.TestXmlFiles"
                 );
 
-            _dictionaryProvider.Initialize("Test");
+            _dictionaryProviderTest.Initialize("Test");
+
+            _dictionaryProviderProd = new XmlEmbeddedFileLocalizationDictionaryProvider(
+                typeof(AbpWebCommonModule).GetAssembly(),
+                "Abp.Web.Localization.AbpWebXmlSource"
+            );
+
+            _dictionaryProviderProd.Initialize("AbpWeb");
         }
 
         [Fact]
-        public void Should_Get_Dictionaries()
+        public void Should_Get_Dictionaries_Test()
         {
-            var dictionaries = _dictionaryProvider.Dictionaries.Values.ToList();
+            var dictionaries = _dictionaryProviderTest.Dictionaries.Values.ToList();
             
             dictionaries.Count.ShouldBe(2);
 
             var enDict = dictionaries.FirstOrDefault(d => d.CultureInfo.Name == "en");
             enDict.ShouldNotBe(null);
-            enDict.ShouldBe(_dictionaryProvider.DefaultDictionary);
+            enDict.ShouldBe(_dictionaryProviderTest.DefaultDictionary);
             enDict["hello"].ShouldBe("Hello");
             
             var trDict = dictionaries.FirstOrDefault(d => d.CultureInfo.Name == "tr");
             trDict.ShouldNotBe(null);
             trDict["hello"].ShouldBe("Merhaba");
+        }
+
+        [Fact]
+        public void Should_Get_Dictionaries_Prod()
+        {
+            var dictionaries = _dictionaryProviderProd.Dictionaries.Values.ToList();
+
+            dictionaries.Count().ShouldBeGreaterThan(1);
+
+            var enDict = dictionaries.FirstOrDefault(d => d.CultureInfo.Name == "en");
+            enDict.ShouldNotBe(null);
+
+            var trDict = dictionaries.FirstOrDefault(d => d.CultureInfo.Name == "tr");
+            trDict.ShouldNotBe(null);
         }
     }
 }
