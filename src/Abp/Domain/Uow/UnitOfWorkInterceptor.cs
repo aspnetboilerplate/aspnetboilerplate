7e40c702c35c48f97a29516d0d575893e6cf2f8b
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-07-26 11:17:10 +0300

    Fixed #2354: Unit of work interception does not work for interface proxies.

--- src/Abp/Domain/Uow/UnitOfWorkInterceptor.cs
+++ src/Abp/Domain/Uow/UnitOfWorkInterceptor.cs
@@ -25,7 +25,7 @@ namespace Abp.Domain.Uow
         /// <param name="invocation">Method invocation arguments</param>
         public void Intercept(IInvocation invocation)
         {
-            var unitOfWorkAttr = _unitOfWorkOptions.GetUnitOfWorkAttributeOrNull(invocation.GetConcreteMethod());
+            var unitOfWorkAttr = _unitOfWorkOptions.GetUnitOfWorkAttributeOrNull(invocation.GetConcreteMethodInvocationTarget());
             if (unitOfWorkAttr == null || unitOfWorkAttr.IsDisabled)
             {
                 //No need to a uow
---
+++ test/Abp.TestBase.SampleApplication.Tests/Domain/Uow/UnitOfWork_Attribute_Tests.cs
@@ -0,0 +1,31 @@
+﻿using Abp.Domain.Services;
+using Abp.Domain.Uow;
+using Shouldly;
+using Xunit;
+
+namespace Abp.TestBase.SampleApplication.Tests.Domain.Uow
+{
+    public class UnitOfWork_Attribute_Tests: SampleApplicationTestBase
+    {
+        [Fact]
+        public void UnitOfWork_Attribute_Should_Work_Without_Virtual_When_Resolved_By_Interface()
+        {
+            var service = Resolve<IMyTestDomainService>();
+            service.DoIt();
+        }
+    }
+
+    public interface IMyTestDomainService : IDomainService
+    {
+        void DoIt();
+    }
+
+    public class MyTestDomainService : DomainService, IMyTestDomainService
+    {
+        [UnitOfWork]
+        public void DoIt()
+        {
+            CurrentUnitOfWork.ShouldNotBeNull();
+        }
+    }
+}
