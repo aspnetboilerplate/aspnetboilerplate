6f487399b5cb75059b6b97abb36dc89275a7b912
lafei <0511-tf@163.com>
2017-10-11 15:56:36 +0800

    add Abp.QuartzCore

---
+++ src/Abp.QuartzCore/Quartz/AbpQuartzJobFactory.cs
@@ -0,0 +1,27 @@
+﻿using Abp.Dependency;
+using Abp.Extensions;
+using Quartz;
+using Quartz.Spi;
+
+namespace Abp.Quartz.Quartz
+{
+    public class AbpQuartzJobFactory : IJobFactory
+    {
+        private readonly IIocResolver _iocResolver;
+
+        public AbpQuartzJobFactory(IIocResolver iocResolver)
+        {
+            _iocResolver = iocResolver;
+        }
+
+        public IJob NewJob(TriggerFiredBundle bundle, IScheduler scheduler)
+        {
+            return _iocResolver.Resolve(bundle.JobDetail.JobType).As<IJob>();
+        }
+
+        public void ReturnJob(IJob job)
+        {
+            _iocResolver.Release(job);
+        }
+    }
+}
