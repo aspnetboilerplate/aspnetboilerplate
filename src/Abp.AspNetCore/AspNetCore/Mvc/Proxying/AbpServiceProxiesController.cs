62f9232b415980a826788a7937510c2a3ccc38d4
Alper Ebicoglu <alperonline@hotmail.com>
2017-12-24 00:45:24 +0300

    Added javascript content-type to the response of GetAll action

--- src/Abp.AspNetCore/AspNetCore/Mvc/Proxying/AbpServiceProxiesController.cs
+++ src/Abp.AspNetCore/AspNetCore/Mvc/Proxying/AbpServiceProxiesController.cs
@@ -17,10 +17,10 @@ namespace Abp.AspNetCore.Mvc.Proxying
             _proxyScriptManager = proxyScriptManager;
         }
 
-        [Produces("text/javascript", "text/plain")]
-        public string GetAll(ApiProxyGenerationModel model)
+        public ContentResult GetAll(ApiProxyGenerationModel model)
         {
-            return _proxyScriptManager.GetScript(model.CreateOptions());
+            var script = _proxyScriptManager.GetScript(model.CreateOptions());
+            return Content(script, "application/x-javascript");
         }
     }
 }
