67cbf14e099ec2095359d7c07fe61b828df4a6ef
Aaron Chong <aaronjhchong@hotmail.com>
2018-01-04 17:14:29 +0800

    Implement IEntityChangeSetReasonProvider in Abp.AspNetCore

---
+++ src/Abp.AspNetCore/AspNetCore/EntityHistory/HttpRequestEntityChangeSetReasonProvider.cs
@@ -0,0 +1,29 @@
+﻿using Abp.Dependency;
+using Abp.EntityHistory;
+using Abp.Runtime;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+
+namespace Abp.AspNetCore.EntityHistory
+{
+    /// <summary>
+    /// Implements <see cref="IEntityChangeSetReasonProvider"/> to get reason from HTTP request.
+    /// </summary>
+    public class HttpRequestEntityChangeSetReasonProvider : EntityChangeSetReasonProviderBase, ISingletonDependency
+    {
+        [CanBeNull]
+        public override string Reason => HttpContextAccessor.HttpContext?.Request.GetDisplayUrl();
+
+        protected IHttpContextAccessor HttpContextAccessor { get; }
+
+        public HttpRequestEntityChangeSetReasonProvider(
+            IHttpContextAccessor httpContextAccessor,
+
+            IAmbientScopeProvider<ReasonOverride> reasonOverrideScopeProvider
+            ) : base(reasonOverrideScopeProvider)
+        {
+            HttpContextAccessor = httpContextAccessor;
+        }
+    }
+}
