925b53301f492f49f93ff13ca1753055a6352f30
Berin Iwlew <berin@bodyworkbuddy.com>
2018-02-01 08:02:45 -0500

    Features/FeatureManager.cs documentation changes

--- src/Abp/Application/Features/FeatureManager.cs
+++ src/Abp/Application/Features/FeatureManager.cs
@@ -13,12 +13,20 @@ namespace Abp.Application.Features
         private readonly IIocManager _iocManager;
         private readonly IFeatureConfiguration _featureConfiguration;
 
+        /// <summary>
+        /// Creates a new <see cref="FeatureManager"/> object
+        /// </summary>
+        /// <param name="iocManager">IOC Manager</param>
+        /// <param name="featureConfiguration">Feature configuration</param>
         public FeatureManager(IIocManager iocManager, IFeatureConfiguration featureConfiguration)
         {
             _iocManager = iocManager;
             _featureConfiguration = featureConfiguration;
         }
 
+        /// <summary>
+        /// Initializes this <see cref="FeatureManager"/>
+        /// </summary>
         public void Initialize()
         {
             foreach (var providerType in _featureConfiguration.Providers)
@@ -32,6 +40,10 @@ namespace Abp.Application.Features
             Features.AddAllFeatures();
         }
 
+        /// <summary>
+        /// Gets a feature by its given name
+        /// </summary>
+        /// <param name="name">Name of the feature</param>
         public Feature Get(string name)
         {
             var feature = GetOrNull(name);
@@ -43,11 +55,19 @@ namespace Abp.Application.Features
             return feature;
         }
 
+        /// <summary>
+        /// Gets all the features
+        /// </summary>
         public IReadOnlyList<Feature> GetAll()
         {
             return Features.Values.ToImmutableList();
         }
 
+        /// <summary>
+        /// ??? Not sure if this is the best description
+        /// Creates a disposable <see cref="FeatureProvider"/> from the given provider type
+        /// </summary>
+        /// <param name="providerType">Provider type</param>
         private IDisposableDependencyObjectWrapper<FeatureProvider> CreateProvider(Type providerType)
         {
             return _iocManager.ResolveAsDisposable<FeatureProvider>(providerType);
