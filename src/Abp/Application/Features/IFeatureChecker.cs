ff56c1eb99d67e3d6c656f7421457d3f8a987417
Berin Iwlew <berin@bodyworkbuddy.com>
2018-02-17 07:49:20 -0500

    Features/IFeatureChecker.cs documentation changes

--- src/Abp/Application/Features/IFeatureChecker.cs
+++ src/Abp/Application/Features/IFeatureChecker.cs
@@ -4,21 +4,21 @@ using Abp.Runtime.Session;
 namespace Abp.Application.Features
 {
     /// <summary>
-    /// This interface should be used to get value of
+    /// This interface should be implemented to get the value of features
     /// </summary>
     public interface IFeatureChecker
     {
         /// <summary>
-        /// Gets value of a feature by it's name.
+        /// Gets the value of a feature by its name.
         /// This is a shortcut for <see cref="GetValueAsync(int, string)"/> that uses <see cref="IAbpSession.TenantId"/> as tenantId.
-        /// So, this method should be used only if TenantId can be obtained from the session.
+        /// Note: This method should only be used if a TenantId can be obtained from the session.
         /// </summary>
         /// <param name="name">Unique feature name</param>
         /// <returns>Feature's current value</returns>
         Task<string> GetValueAsync(string name);
 
         /// <summary>
-        /// Gets value of a feature for a tenant by the feature name.
+        /// Gets the value of a feature for a tenant by the feature's name.
         /// </summary>
         /// <param name="tenantId">Tenant's Id</param>
         /// <param name="name">Unique feature name</param>
