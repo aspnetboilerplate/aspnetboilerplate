9fe99fb78b70b46ddc1b486d8353b0303cf51081
Ryan Chang <quan0203@gmail.com>
2018-03-16 01:18:23 +0800

    added MultiTenancySide attribute test

--- test/Abp.AspNetCore.Tests/Tests/MultiTenancy_Tests.cs
+++ test/Abp.AspNetCore.Tests/Tests/MultiTenancy_Tests.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Abp.AspNetCore.App.Controllers;
+using Abp.Authorization;
 using Abp.Configuration.Startup;
+using Abp.Dependency;
 using Abp.MultiTenancy;
 using Abp.Web.Models;
 using Abp.Web.MultiTenancy;
@@ -14,11 +16,15 @@ namespace Abp.AspNetCore.Tests
     public class MultiTenancy_Tests : AppTestBase
     {
         private readonly IWebMultiTenancyConfiguration _multiTenancyConfiguration;
+        private readonly MyMultiTenancyClass _multiTenancyClass;
+        private readonly MyTenantClass _tenantClass;
 
         public MultiTenancy_Tests()
         {
             IocManager.Resolve<IMultiTenancyConfig>().IsEnabled = true;
             _multiTenancyConfiguration = Resolve<IWebMultiTenancyConfiguration>();
+            _multiTenancyClass = Resolve<MyMultiTenancyClass>();
+            _tenantClass = Resolve<MyTenantClass>();
         }
 
         [Fact]
@@ -88,6 +94,116 @@ namespace Abp.AspNetCore.Tests
 
             //Assert
             response.Result.ShouldBe(1);
+        }
+
+        [Fact]
+        public void Should_Not_Intercept_Class_For_Authenticated_User()
+        {
+            using (AbpSession.Use(null, 1))
+            {
+                //Allow authenticated host user to call tenant method
+                _tenantClass.TenantMethod();
+            }
+
+            using (AbpSession.Use(1, 1))
+            {
+                //Allow authenticated tenant user to call host method
+                _tenantClass.HostMethod();
+            }
+        }
+
+        [Fact]
+        public void Should_Not_Intercept_Method_For_Authenticated_User()
+        {
+            using (AbpSession.Use(null, 1))
+            {
+                //Allow authenticated host user to call tenant method
+                _multiTenancyClass.TenantMethod();
+            }
+
+            using (AbpSession.Use(1, 1))
+            {
+                //Allow authenticated tenant user to call host method
+                _multiTenancyClass.HostMethod();
+            }
+        }
+
+        [Fact]
+        public void Should_Intercept_Class_For_Anonymous_Host_User()
+        {
+            using (AbpSession.Use(null, null))
+            {
+                Should.Throw<AbpAuthorizationException>(() => _tenantClass.TenantMethod()).Message.ShouldBe("Anonymous host user must not call tenant method.");
+            }
+        }
+
+        [Fact]
+        public void Should_Intercept_Method_For_Anonymous_Host_User()
+        {
+            using (AbpSession.Use(null, null))
+            {
+                Should.Throw<AbpAuthorizationException>(() => _multiTenancyClass.TenantMethod()).Message.ShouldBe("Anonymous host user must not call tenant method.");
+            }
+        }
+
+        [Fact]
+        public void Should_Intercept_Class_For_Anonymous_Tenant_User()
+        {
+            using (AbpSession.Use(1, null))
+            {
+                Should.Throw<AbpAuthorizationException>(() => _tenantClass.HostMethod()).Message.ShouldBe("Anonymous tenant user must not call host method.");
+            }
+        }
+
+        [Fact]
+        public void Should_Intercept_Method_For_Anonymous_Tenant_User()
+        {
+            using (AbpSession.Use(1, null))
+            {
+                Should.Throw<AbpAuthorizationException>(() => _multiTenancyClass.HostMethod()).Message.ShouldBe("Anonymous tenant user must not call host method.");
+            }
+        }
+    }
+
+    public class MyMultiTenancyClass : ITransientDependency
+    {
+        public MyMultiTenancyClass()
+        {
+        }
+
+        [MultiTenancySide(MultiTenancySides.Host)]
+        public virtual void HostMethod()
+        {
+        }
+
+        [MultiTenancySide(MultiTenancySides.Tenant)]
+        public virtual void TenantMethod()
+        {
+        }
+
+        public virtual void AnotherMethod()
+        {
+        }
+    }
+
+    [MultiTenancySide(MultiTenancySides.Tenant)]
+    public class MyTenantClass : ITransientDependency
+    {
+        public MyTenantClass()
+        {
+        }
+
+        [MultiTenancySide(MultiTenancySides.Host)]
+        public virtual void HostMethod()
+        {
+        }
+
+        public virtual void TenantMethod()
+        {
+        }
+
+        public virtual void AnotherMethod()
+        {
         }
     }
 }
