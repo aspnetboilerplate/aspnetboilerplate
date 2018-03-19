c90cac7add90a6b8c6dbc4d4e0eb9292b157f594
刘杨 <yang.liu636@gmail.com>
2018-03-19 21:12:54 +0800

    add test

---
+++ test/Abp.Quartz.Tests/AbpQuartzConfigurationTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Abp.Dependency;
+using Abp.Quartz.Configuration;
+using Abp.TestBase;
+using Quartz;
+using Shouldly;
+using Xunit;
+
+namespace Abp.Quartz.Tests
+{
+    public class AbpQuartzConfigurationTests
+    {
+        [Fact]
+        public void TestGetScheduler()
+        {
+            var config = new AbpQuartzConfiguration();
+            var t1 = Task.Run(() => config.Scheduler);
+            var t2 = Task.Run(() => config.Scheduler);
+            Task.WaitAll(t1, t2);
+            Assert.True(true);
+        }
+    }
+}
