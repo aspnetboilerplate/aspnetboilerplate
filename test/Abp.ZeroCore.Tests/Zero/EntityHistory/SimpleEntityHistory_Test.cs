646b8c2713defa21c3fec84c0519eca140f0960a
Aaron Chong <aaronjhchong@hotmail.com>
2018-04-28 11:30:31 +0800

    Add test case for shadow property

--- test/Abp.ZeroCore.Tests/Zero/EntityHistory/SimpleEntityHistory_Test.cs
+++ test/Abp.ZeroCore.Tests/Zero/EntityHistory/SimpleEntityHistory_Test.cs
@@ -24,6 +24,7 @@ namespace Abp.Zero.EntityHistory
     {
         private readonly IRepository<Blog> _blogRepository;
         private readonly IRepository<Post, Guid> _postRepository;
+        private readonly IRepository<Comment> _commentRepository;
 
         private IEntityHistoryStore _entityHistoryStore;
 
@@ -31,6 +32,7 @@ namespace Abp.Zero.EntityHistory
         {
             _blogRepository = Resolve<IRepository<Blog>>();
             _postRepository = Resolve<IRepository<Post, Guid>>();
+            _commentRepository = Resolve<IRepository<Comment>>();
 
             Resolve<IEntityHistoryConfiguration>().IsEnabledForAnonymousUsers = true;
         }
@@ -167,6 +169,30 @@ namespace Abp.Zero.EntityHistory
                      s.EntityChanges[0].EntityId == post1Id.ToJsonString(false, false) &&
                      s.EntityChanges[0].EntityTypeFullName == typeof(Post).FullName &&
                      s.EntityChanges[0].PropertyChanges.Count == 1 // Post.BlogId
+            ));
+        }
+
+        [Fact]
+        public void Should_Write_History_For_Tracked_Property_Foreign_Key_Shadow()
+        {
+            /* Comment has Audited attribute. */
+
+            using (var uow = Resolve<IUnitOfWorkManager>().Begin())
+            {
+                var comment1 = _commentRepository.Single(b => b.Content == "test-comment-1-content");
+                var post2 = _postRepository.Single(b => b.Body == "test-post-2-body");
+
+                // Change foreign key by assigning navigation property
+                comment1.Post = post2;
+                _commentRepository.Update(comment1);
+
+                uow.Complete();
+            }
+
+            _entityHistoryStore.Received().SaveAsync(Arg.Is<EntityChangeSet>(
+                s => s.EntityChanges.Count == 1 &&
+                     s.EntityChanges[0].PropertyChanges.Count == 1 &&
+                     s.EntityChanges[0].PropertyChanges.First().PropertyName == "PostId"
             ));
         }
 
