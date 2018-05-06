fdd5504c7e2291ebcd3ed2d851200ad1ad9a164f
tangdf <tangdf@users.noreply.github.com>
2018-05-06 16:59:34 +0800

    Replace reflection to call API.

--- src/Abp/Reflection/ProxyHelper.cs
+++ src/Abp/Reflection/ProxyHelper.cs
@@ -10,21 +10,7 @@ namespace Abp.Reflection
         /// </summary>
         public static object UnProxy(object obj)
         {
-            if (obj.GetType().Namespace != "Castle.Proxies")
-            {
-                return obj;
-            }
-
-            var targetField = obj.GetType()
-                .GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
-                .FirstOrDefault(f => f.Name == "__target");
-
-            if (targetField == null)
-            {
-                return obj;
-            }
-
-            return targetField.GetValue(obj);
+            return Castle.DynamicProxy.ProxyUtil.GetUnproxiedInstance(obj);
         }
     }
 }
