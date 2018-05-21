cf14dcac17fcc13674ac887bc9c5c817ed76ea82
alirizaadiyahsi <alirizaadiyahsi@outlook.com>
2018-05-21 12:15:14 +0300

    #3371 - Added test case to reproduce issue

--- test/Abp.Zero.SampleApp/Authorization/AppAuthorizationProvider.cs
+++ test/Abp.Zero.SampleApp/Authorization/AppAuthorizationProvider.cs
@@ -1,6 +1,7 @@
 ﻿using Abp.Application.Features;
 using Abp.Authorization;
 using Abp.Localization;
+using Abp.MultiTenancy;
 using Abp.Zero.SampleApp.Features;
 
 namespace Abp.Zero.SampleApp.Authorization
@@ -13,6 +14,7 @@ namespace Abp.Zero.SampleApp.Authorization
             context.CreatePermission("Permission2", new FixedLocalizableString("Permission2"));
             context.CreatePermission("Permission3", new FixedLocalizableString("Permission3"));
             context.CreatePermission("Permission4", new FixedLocalizableString("Permission4"));
+            context.CreatePermission("Permission5", new FixedLocalizableString("Permission5"), multiTenancySides: MultiTenancySides.Host);
             context.CreatePermission("PermissionWithFeatureDependency", new FixedLocalizableString("PermissionWithFeatureDependency"), featureDependency:new SimpleFeatureDependency(AppFeatureProvider.MyBoolFeature));
         }
     }
