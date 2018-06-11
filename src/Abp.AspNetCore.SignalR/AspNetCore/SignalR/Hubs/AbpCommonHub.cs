bfbba79718a8df3bfa4e8df3be1529a70cc21fa0
Halil ibrahim Kalkan <hi_kalkan@yahoo.com>
2018-06-11 14:22:25 +0300

    Created OnlineClientHubBase.

--- src/Abp.AspNetCore.SignalR/AspNetCore/SignalR/Hubs/AbpCommonHub.cs
+++ src/Abp.AspNetCore.SignalR/AspNetCore/SignalR/Hubs/AbpCommonHub.cs
@@ -1,89 +1,18 @@
-﻿using System;
-using System.Threading.Tasks;
-using Abp.Auditing;
-using Abp.Dependency;
+﻿using Abp.Auditing;
 using Abp.RealTime;
-using Abp.Runtime.Session;
-using Castle.Core.Logging;
 
 namespace Abp.AspNetCore.SignalR.Hubs
 {
-    /// <summary>
-    /// Common Hub of ABP.
-    /// </summary>
-    public class AbpCommonHub : AbpHubBase, ITransientDependency
+    public class AbpCommonHub : OnlineClientHubBase
     {
-        private readonly IOnlineClientManager _onlineClientManager;
-        private readonly IClientInfoProvider _clientInfoProvider;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="AbpCommonHub"/> class.
-        /// </summary>
-        public AbpCommonHub(
-            IOnlineClientManager onlineClientManager,
-            IClientInfoProvider clientInfoProvider)
+        public AbpCommonHub(IOnlineClientManager onlineClientManager, IClientInfoProvider clientInfoProvider) 
+            : base(onlineClientManager, clientInfoProvider)
         {
-            _onlineClientManager = onlineClientManager;
-            _clientInfoProvider = clientInfoProvider;
-
-            Logger = NullLogger.Instance;
-            AbpSession = NullAbpSession.Instance;
         }
 
         public void Register()
         {
             Logger.Debug("A client is registered: " + Context.ConnectionId);
-        }
-
-        public override async Task OnConnectedAsync()
-        {
-            await base.OnConnectedAsync();
-
-            var client = CreateClientForCurrentConnection();
-
-            Logger.Debug("A client is connected: " + client);
-
-            _onlineClientManager.Add(client);
-        }
-
-        public override async Task OnDisconnectedAsync(Exception exception)
-        {
-            await base.OnDisconnectedAsync(exception);
-
-            Logger.Debug("A client is disconnected: " + Context.ConnectionId);
-
-            try
-            {
-                _onlineClientManager.Remove(Context.ConnectionId);
-            }
-            catch (Exception ex)
-            {
-                Logger.Warn(ex.ToString(), ex);
-            }
-        }
-
-        private IOnlineClient CreateClientForCurrentConnection()
-        {
-            return new OnlineClient(
-                Context.ConnectionId,
-                GetIpAddressOfClient(),
-                AbpSession.TenantId,
-                AbpSession.UserId
-            );
-        }
-
-        protected virtual string GetIpAddressOfClient()
-        {
-            try
-            {
-                return _clientInfoProvider.ClientIpAddress;
-            }
-            catch (Exception ex)
-            {
-                Logger.Error("Can not find IP address of the client! connectionId: " + Context.ConnectionId);
-                Logger.Error(ex.Message, ex);
-                return "";
-            }
         }
     }
 }
