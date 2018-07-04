9550fdfcfd4a319a18d693ecc5069cb67ccba6ee
alirizaadiyahsi <alirizaadiyahsi@outlook.com>
2018-07-04 14:19:37 +0300

    Removed multi tenancy side parameter from permission provider

--- test/Abp.Zero.SampleApp/Authorization/AppAuthorizationProvider.cs
+++ test/Abp.Zero.SampleApp/Authorization/AppAuthorizationProvider.cs
@@ -10,15 +10,15 @@ namespace Abp.Zero.SampleApp.Authorization
     {
         public override void SetPermissions(IPermissionDefinitionContext context)
         {
-            var permission1 =context.CreatePermission("Permission1", new FixedLocalizableString("Permission1"),multiTenancySides:MultiTenancySides.Tenant);
+            var permission1 =context.CreatePermission("Permission1", new FixedLocalizableString("Permission1"));
             context.CreatePermission("Permission2", new FixedLocalizableString("Permission2"));
             context.CreatePermission("Permission3", new FixedLocalizableString("Permission3"));
             context.CreatePermission("Permission4", new FixedLocalizableString("Permission4"));
             context.CreatePermission("Permission5", new FixedLocalizableString("Permission5"), multiTenancySides: MultiTenancySides.Host);
             context.CreatePermission("PermissionWithFeatureDependency", new FixedLocalizableString("PermissionWithFeatureDependency"), featureDependency:new SimpleFeatureDependency(AppFeatureProvider.MyBoolFeature));
 
-            var firstLevelChilPermission1 = permission1.CreateChildPermission("FirstLevelChilPermission1", new FixedLocalizableString("FirstLevelChilPermission1"),multiTenancySides:MultiTenancySides.Tenant);
-            firstLevelChilPermission1.CreateChildPermission("SecondLevelChildPermission1", new FixedLocalizableString("SecondLevelChildPermission1"),multiTenancySides:MultiTenancySides.Tenant);
+            var firstLevelChilPermission1 = permission1.CreateChildPermission("FirstLevelChilPermission1", new FixedLocalizableString("FirstLevelChilPermission1"));
+            firstLevelChilPermission1.CreateChildPermission("SecondLevelChildPermission1", new FixedLocalizableString("SecondLevelChildPermission1"));
         }
     }
 }
