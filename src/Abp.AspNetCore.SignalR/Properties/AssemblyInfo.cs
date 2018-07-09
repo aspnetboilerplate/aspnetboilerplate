1da294ddc74db7a9da25a8e7097b6dd3e88ae481
Aaron Chong <aaronjhchong@hotmail.com>
2018-07-09 23:53:10 +0800

    Change namespace from Abp.Web.SignalR to Abp.AspNetCore.SignalR

--- src/Abp.AspNetCore.SignalR/Properties/AssemblyInfo.cs
+++ src/Abp.AspNetCore.SignalR/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Abp.Web.SignalR.AspNetCore")]
+[assembly: AssemblyTitle("Abp.AspNetCore.SignalR")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Volosoft")]
-[assembly: AssemblyProduct("Abp.Web.SignalR.AspNetCore")]
+[assembly: AssemblyProduct("Abp.AspNetCore.SignalR")]
 [assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
