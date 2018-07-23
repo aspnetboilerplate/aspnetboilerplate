00f2d372c71086cfb18bb46c19dbb3fa93e9bcc5
İsmail ÇAGDAŞ <ismailcagdas@gmail.com>
2018-07-23 17:11:16 +0300

    Added test controller for FluentValidation into MVC 5.x sample project

--- test/aspnet-mvc-demo/AbpAspNetMvcDemo/Controllers/TestFluentValidationController.cs
+++ test/aspnet-mvc-demo/AbpAspNetMvcDemo/Controllers/TestFluentValidationController.cs
@@ -3,28 +3,6 @@ using FluentValidation;
 
 namespace AbpAspNetMvcDemo.Controllers
 {
-    public class HomeController : DemoControllerBase
-    {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-    }
-
     public class TestFluentValidationController : DemoControllerBase
     {
         public ContentResult GetJsonValue(MyCustomArgument arg1)
