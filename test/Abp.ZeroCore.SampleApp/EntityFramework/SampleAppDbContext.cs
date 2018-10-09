36cda9ef87531f93c90dd097d9e1597dc498e411
İsmail ÇAGDAŞ <ismailcagdas@gmail.com>
2018-10-09 10:46:40 +0300

    #3855: Added unit tests

--- test/Abp.ZeroCore.SampleApp/EntityFramework/SampleAppDbContext.cs
+++ test/Abp.ZeroCore.SampleApp/EntityFramework/SampleAppDbContext.cs
@@ -27,6 +27,8 @@ namespace Abp.ZeroCore.SampleApp.EntityFramework
 
         public DbSet<Author> Authors { get; set; }
 
+        public DbSet<Store> Stores { get; set; }
+
         public SampleAppDbContext(DbContextOptions<SampleAppDbContext> options) 
             : base(options)
         {
@@ -40,6 +42,8 @@ namespace Abp.ZeroCore.SampleApp.EntityFramework
 
             modelBuilder.Entity<Book>().ToTable("Books");
             modelBuilder.Entity<Book>().Property(e => e.Id).ValueGeneratedNever();
+
+            modelBuilder.Entity<Store>().Property(e => e.Id).HasColumnName("StoreId");
         }
     }
 }
