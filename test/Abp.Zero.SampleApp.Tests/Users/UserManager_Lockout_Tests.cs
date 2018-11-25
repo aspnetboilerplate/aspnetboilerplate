e5410cf739048c3747c90821589b2406db8773eb
Aaron Chong <aaronjhchong@hotmail.com>
2018-11-25 22:26:39 +0800

    Use stricter value for IsLockoutEnabled if set

--- test/Abp.Zero.SampleApp.Tests/Users/UserManager_Lockout_Tests.cs
+++ test/Abp.Zero.SampleApp.Tests/Users/UserManager_Lockout_Tests.cs
@@ -87,17 +87,7 @@ namespace Abp.Zero.SampleApp.Tests.Users
         {
             // Arrange
 
-            LocalIocManager.Using<ISettingManager>(async settingManager =>
-            {
-                if (AbpSession.TenantId is int tenantId)
-                {
-                    await settingManager.ChangeSettingForTenantAsync(tenantId, AbpZeroSettingNames.UserManagement.UserLockOut.IsEnabled, isLockoutEnabledByDefault.ToString());
-                }
-                else
-                {
-                    await settingManager.ChangeSettingForApplicationAsync(AbpZeroSettingNames.UserManagement.UserLockOut.IsEnabled, isLockoutEnabledByDefault.ToString());
-                }
-            });
+            ChangeLockoutEnabledSetting(isLockoutEnabledByDefault);
 
             // Act
 
@@ -119,5 +109,57 @@ namespace Abp.Zero.SampleApp.Tests.Users
 
             (await _userManager.GetLockoutEnabledAsync(user.Id)).ShouldBe(isLockoutEnabledByDefault);
         }
+
+        [Theory]
+        [InlineData(true, true, true)]
+        [InlineData(true, false, true)]
+        [InlineData(false, true, true)]
+        [InlineData(false, false, false)]
+        public async Task Test_IsLockoutEnabled_On_User_Creation_Should_Use_Stricter_Value_If_Set(bool isLockoutEnabledByDefault, bool isLockoutEnabled, bool isLockoutEnabledExpected)
+        {
+            // Arrange
+
+            ChangeLockoutEnabledSetting(isLockoutEnabledByDefault);
+
+            // Act
+
+            var user = new User
+            {
+                TenantId = AbpSession.TenantId,
+                UserName = "user1",
+                Name = "John",
+                Surname = "Doe",
+                EmailAddress = "user1@aspnetboilerplate.com",
+                IsEmailConfirmed = true,
+                Password = "AM4OLBpptxBYmM79lGOX9egzZk3vIQU3d/gFCJzaBjAPXzYIK3tQ2N7X4fcrHtElTw==", //123qwe
+                IsLockoutEnabled = isLockoutEnabled
+            };
+
+            await WithUnitOfWorkAsync(async () => await _userManager.CreateAsync(user));
+
+            // Assert
+
+            (await _userManager.GetLockoutEnabledAsync(user.Id)).ShouldBe(isLockoutEnabledExpected);
+        }
+
+        #region Helpers
+
+        private void ChangeLockoutEnabledSetting(bool isLockoutEnabledByDefault)
+        {
+
+            LocalIocManager.Using<ISettingManager>(settingManager =>
+            {
+                if (AbpSession.TenantId is int tenantId)
+                {
+                    settingManager.ChangeSettingForTenant(tenantId, AbpZeroSettingNames.UserManagement.UserLockOut.IsEnabled, isLockoutEnabledByDefault.ToString());
+                }
+                else
+                {
+                    settingManager.ChangeSettingForApplication(AbpZeroSettingNames.UserManagement.UserLockOut.IsEnabled, isLockoutEnabledByDefault.ToString());
+                }
+            });
+        }
+
+        #endregion
     }
 }
