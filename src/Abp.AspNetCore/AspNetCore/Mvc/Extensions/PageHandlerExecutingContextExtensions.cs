98db5e1451962020ff4a562e2b9a7e8aea5c00b2
İsmail ÇAGDAŞ <ismailcagdas@gmail.com>
2019-01-24 11:55:17 +0300

    #2464: Created AbpAuditPageFilter

---
+++ src/Abp.AspNetCore/AspNetCore/Mvc/Extensions/PageHandlerExecutingContextExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Abp.Reflection;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Abp.AspNetCore.Mvc.Extensions
+{
+    public static class PageHandlerExecutingContextExtensions
+    {
+        public static Dictionary<string, object> GetBoundPropertiesAsDictionary(this PageHandlerExecutingContext context)
+        {
+            if (!context.ActionDescriptor.BoundProperties.Any())
+            {
+                return new Dictionary<string, object>();
+            }
+
+            var result = new Dictionary<string, object>();
+
+            foreach (var boundProperty in context.ActionDescriptor.BoundProperties)
+            {
+                var value = ReflectionHelper.GetValueByPath(
+                    context.HandlerInstance,
+                    context.HandlerInstance.GetType(),
+                    boundProperty.Name);
+
+                result.Add(boundProperty.Name, value);
+            }
+
+            return result;
+        }
+    }
+}
