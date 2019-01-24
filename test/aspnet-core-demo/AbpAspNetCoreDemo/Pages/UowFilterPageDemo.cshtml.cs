dd637370a4c59ded7e7d138cb9b6225b8888a79a
İsmail ÇAGDAŞ <ismailcagdas@gmail.com>
2019-01-24 10:28:33 +0300

    #2464: Added razor page uow filter tests and ASP.NET Core integration tests

---
+++ test/aspnet-core-demo/AbpAspNetCoreDemo/Pages/UowFilterPageDemo.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Abp.Domain.Uow;
+using Abp.UI;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbpAspNetCoreDemo.Pages
+{
+    public class UowFilterPageDemo : PageModel
+    {
+        private readonly IUnitOfWorkManager _unitOfWorkManager;
+
+        public UowFilterPageDemo(IUnitOfWorkManager unitOfWorkManager)
+        {
+            _unitOfWorkManager = unitOfWorkManager;
+        }
+
+        public void OnGet()
+        {
+            if (_unitOfWorkManager.Current == null)
+            {
+                throw new UserFriendlyException("Current UnitOfWork is null");
+            }
+        }
+
+        [UnitOfWork(IsDisabled = true)]
+        public void OnPost()
+        {
+            if (_unitOfWorkManager.Current == null)
+            {
+                throw new UserFriendlyException("Current UnitOfWork is null");
+            }
+        }
+    }
+}
