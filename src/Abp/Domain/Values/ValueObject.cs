5813006dccf428bf6f781f830a3cdb5c0c196284
alirizaadiyahsi <alirizaadiyahsi@gmail.com>
2019-02-21 16:28:38 +0300

    Added generic ValueObject.cs

--- src/Abp/Domain/Values/ValueObject.cs
+++ src/Abp/Domain/Values/ValueObject.cs
@@ -1,10 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using Abp.Reflection;
 
 namespace Abp.Domain.Values
 {
     //Inspired from https://docs.microsoft.com/en-us/dotnet/standard/microservices-architecture/microservice-ddd-cqrs-patterns/implement-value-objects
-
+    /// <summary>
+    /// Base class for value objects.
+    /// </summary>
     public abstract class ValueObject
     {
         protected static bool EqualOperator(ValueObject left, ValueObject right)
@@ -47,6 +52,7 @@ namespace Abp.Domain.Values
                     return false;
                 }
             }
+
             return !thisValues.MoveNext() && !otherValues.MoveNext();
         }
 
@@ -75,6 +81,101 @@ namespace Abp.Domain.Values
         public static bool operator !=(ValueObject a, ValueObject b)
         {
             return !(a == b);
+        }
+    }
+
+    //Inspired from https://blogs.msdn.microsoft.com/cesardelatorre/2011/06/06/implementing-a-value-object-base-class-supertype-patternddd-patterns-related/
+    /// <summary>
+    /// Base class for value objects.
+    /// Recommended to use non-generic ValueObject class instead.
+    /// </summary>
+    /// <typeparam name="TValueObject">The type of the value object.</typeparam>
+    public abstract class ValueObject<TValueObject> : IEquatable<TValueObject>
+        where TValueObject : ValueObject<TValueObject>
+    {
+        public bool Equals(TValueObject other)
+        {
+            if ((object)other == null)
+            {
+                return false;
+            }
+
+            var publicProperties = GetPropertiesForCompare();
+            if (!publicProperties.Any())
+            {
+                return true;
+            }
+
+            return publicProperties.All(property => Equals(property.GetValue(this, null), property.GetValue(other, null)));
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            var item = obj as ValueObject<TValueObject>;
+            return (object)item != null && Equals((TValueObject)item);
+        }
+
+        public override int GetHashCode()
+        {
+            const int index = 1;
+            const int initialHasCode = 31;
+
+            var publicProperties = GetPropertiesForCompare();
+
+            if (!publicProperties.Any())
+            {
+                return initialHasCode;
+            }
+
+            var hashCode = initialHasCode;
+            var changeMultiplier = false;
+
+            foreach (var property in publicProperties)
+            {
+                var value = property.GetValue(this, null);
+
+                if (value == null)
+                {
+                    //support {"a",null,null,"a"} != {null,"a","a",null}
+                    hashCode = hashCode ^ (index * 13);
+                    continue;
+                }
+
+                hashCode = hashCode * (changeMultiplier ? 59 : 114) + value.GetHashCode();
+                changeMultiplier = !changeMultiplier;
+            }
+
+            return hashCode;
+        }
+
+        public static bool operator ==(ValueObject<TValueObject> x, ValueObject<TValueObject> y)
+        {
+            if (ReferenceEquals(x, y))
+            {
+                return true;
+            }
+
+            if (((object)x == null) || ((object)y == null))
+            {
+                return false;
+            }
+
+            return x.Equals(y);
+        }
+
+        public static bool operator !=(ValueObject<TValueObject> x, ValueObject<TValueObject> y)
+        {
+            return !(x == y);
+        }
+
+        private PropertyInfo[] GetPropertiesForCompare()
+        {
+            return GetType().GetTypeInfo().GetProperties().Where(t => ReflectionHelper.GetSingleAttributeOrDefault<IgnoreOnCompareAttribute>(t) == null).ToArray();
         }
     }
 }
