bbba9ba4110ff93cc0d34c0b38fe9b4561fa4086
Aaron Chong <aaronjhchong@hotmail.com>
2019-03-09 00:30:33 +0800

    Use NSubstitute for test case

--- test/Abp.ZeroCore.Tests/Zero/AbpZeroTestModule.cs
+++ test/Abp.ZeroCore.Tests/Zero/AbpZeroTestModule.cs
@@ -24,8 +24,6 @@ namespace Abp.Zero
             Configuration.Modules.Zero().LanguageManagement.EnableDbLocalization();
             Configuration.UnitOfWork.IsTransactional = false;
             Configuration.Notifications.Distributers.Add<FakeNotificationDistributer>();
-            Configuration.Notifications.Notifiers.Add<FakeRealTimeNotifier1>();
-            Configuration.Notifications.Notifiers.Add<FakeRealTimeNotifier2>();
         }
 
         public override void Initialize()
@@ -35,18 +33,6 @@ namespace Abp.Zero
             IocManager.IocContainer.Register(
                 Component
                     .For<FakeNotificationDistributer>()
-                    .LifestyleSingleton()
-            );
-
-            IocManager.IocContainer.Register(
-                Component
-                    .For<FakeRealTimeNotifier1>()
-                    .LifestyleSingleton()
-            );
-
-            IocManager.IocContainer.Register(
-                Component
-                    .For<FakeRealTimeNotifier2>()
                     .LifestyleSingleton()
             );
 
