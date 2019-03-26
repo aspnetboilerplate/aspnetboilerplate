48c2dc88d39720c83c11537c12b82e6afef1fe94
İsmail ÇAGDAŞ <ismailcagdas@gmail.com>
2019-03-26 09:53:00 +0300

    Added OData support to AbpAspNetCoreDemo project and used Sqlite instead of SqlServer

---
+++ test/aspnet-core-demo/AbpAspNetCoreDemo.Tests/Tests/AbpODataEntityControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Abp.Auditing;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NSubstitute;
+using Xunit;
+
+namespace AbpAspNetCoreDemo.IntegrationTests.Tests
+{
+    public class AbpODataEntityControllerTests
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+        
+        private IAuditingStore _auditingStore;
+
+        public AbpODataEntityControllerTests()
+        {
+            _factory = new WebApplicationFactory<Startup>();
+        }
+
+//        [Fact]
+//        public async Task RazorPage_RazorAuditPageFilter_Get_Test()
+//        {
+//            // Arrange
+//            var client = _factory.CreateClient();
+
+//            // Act
+//            var response = await client.GetAsync("/" + pageName);
+
+//            // Assert
+//            response.EnsureSuccessStatusCode();
+
+//#pragma warning disable 4014
+
+//            if (shouldWriteAuditLog)
+//            {
+//                _auditingStore.Received().SaveAsync(Arg.Is<AuditInfo>(a => a.ServiceName.Contains(pageName)));
+//            }
+//            else
+//            {
+//                _auditingStore.DidNotReceive().SaveAsync(Arg.Any<AuditInfo>());
+//            }
+
+//#pragma warning restore 4014
+//        }
+    }
+}
