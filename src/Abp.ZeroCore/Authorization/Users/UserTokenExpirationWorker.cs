e6c994b80535c972df2fc46ab71af0198c5091a8
Ryan Chang <ryancyq@gmail.com>
2019-04-11 10:02:40 +0800

    add user token expiration worker

---
+++ src/Abp.ZeroCore/Authorization/Users/UserTokenExpirationWorker.cs
@@ -0,0 +1,31 @@
+﻿using Abp.Dependency;
+using Abp.Domain.Repositories;
+using Abp.Threading.BackgroundWorkers;
+using Abp.Threading.Timers;
+using Abp.Timing;
+
+namespace Abp.Authorization.Users
+{
+    public class UserTokenExpirationWorker : PeriodicBackgroundWorkerBase, ISingletonDependency
+    {
+        private const int IntervalInMilliseconds = 1 * 60 * 60 * 1000; // 1 hour
+
+        private readonly IRepository<UserToken, long> _userTokenRepository;
+
+        public UserTokenExpirationWorker(
+            AbpTimer timer,
+            IRepository<UserToken, long> userTokenRepository)
+            : base(timer)
+        {
+            _userTokenRepository = userTokenRepository;
+
+            Timer.Period = IntervalInMilliseconds;
+        }
+
+        protected override void DoWork()
+        {
+            var utcNow = Clock.Now.ToUniversalTime();
+            _userTokenRepository.Delete(t => t.ExpireDate <= utcNow);
+        }
+    }
+}
