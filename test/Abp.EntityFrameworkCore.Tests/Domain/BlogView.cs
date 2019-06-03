eaf899785753a0deb597b2f1c930de74e8ca0f55
maliming <malimings@gmail.com>
2019-06-03 13:44:56 +0800

    DbQuery unit test.

---
+++ test/Abp.EntityFrameworkCore.Tests/Domain/BlogView.cs
@@ -0,0 +1,11 @@
+﻿namespace Abp.EntityFrameworkCore.Tests.Domain
+{
+    public class BlogView
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+    }
+}
---
+++ test/Abp.EntityFrameworkCore.Tests/Tests/DbQuery_Tests.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Abp.Domain.Repositories;
+using Abp.Domain.Uow;
+using Abp.EntityFrameworkCore.Repositories;
+using Abp.EntityFrameworkCore.Tests.Domain;
+using Abp.EntityFrameworkCore.Tests.Ef;
+using Shouldly;
+using Xunit;
+
+namespace Abp.EntityFrameworkCore.Tests.Tests
+{
+    public class DbQuery_Tests : EntityFrameworkCoreModuleTestBase
+    {
+        private readonly IRepository<Blog> _blogRepository;
+
+        public DbQuery_Tests()
+        {
+            _blogRepository = Resolve<IRepository<Blog>>();
+        }
+
+        [Fact]
+        public async Task DbQuery_Test()
+        {
+            using (var uow = Resolve<IUnitOfWorkManager>().Begin())
+            {
+                var blogViews = (_blogRepository.GetDbContext() as BloggingDbContext)?.BlogView.ToList();
+
+                blogViews.ShouldNotBeNull();
+                blogViews.ShouldContain(x => x.Name == "test-blog-1" && x.Url == "http://testblog1.myblogs.com");
+
+                await uow.CompleteAsync();
+            }
+        }
+    }
+}
