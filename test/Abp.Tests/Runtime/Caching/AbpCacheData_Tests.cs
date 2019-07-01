fc212bde0b7a1803e9ec32c7b0de6704ac8d4cf4
Musa Demir <musa.demir@volosoft.com>
2019-07-01 14:38:20 +0300

    redis cache unity test error fix

--- test/Abp.Tests/Runtime/Caching/AbpCacheData_Tests.cs
+++ test/Abp.Tests/Runtime/Caching/AbpCacheData_Tests.cs
@@ -21,7 +21,7 @@ namespace Abp.Tests.Runtime.Caching
             };
 
             var result = AbpCacheData.Serialize(source);
-            result.Type.ShouldBe("System.Collections.Generic.List`1[[System.String]]");
+            result.Type.ShouldBe("System.Collections.Generic.List`1[[System.String, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]], mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
             result.Payload.ShouldBe("[\"Stranger Things\",\"The OA\",\"Lost in Space\"]");
         }
 
@@ -35,7 +35,7 @@ namespace Abp.Tests.Runtime.Caching
             };
 
             var result = AbpCacheData.Serialize(source);
-            result.Type.ShouldBe("Abp.Tests.Runtime.Caching.AbpCacheData_Tests+MyTestClass, Abp.Tests");
+            result.Type.ShouldBe("Abp.Tests.Runtime.Caching.AbpCacheData_Tests+MyTestClass, Abp.Tests, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
             result.Payload.ShouldBe("{\"Field1\":42,\"Field2\":\"Stranger Things\"}");
         }
 
