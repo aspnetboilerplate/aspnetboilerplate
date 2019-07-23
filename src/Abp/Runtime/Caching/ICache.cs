61b4d7cdc95c3c03b2e8299cd49c7b20b14d18c5
Ryan Chang <ryancyq@gmail.com>
2019-07-24 02:02:30 +0800

    extract ICacheMultipleKeysOperations

--- src/Abp/Runtime/Caching/ICache.cs
+++ src/Abp/Runtime/Caching/ICache.cs
@@ -7,81 +7,11 @@ namespace Abp.Runtime.Caching
     /// <summary>
     /// Defines a cache that can be store and get items by keys.
     /// </summary>
-    public interface ICache : IDisposable, ICacheOptions, ICacheOperations, ICacheSingleKeyOperations<string, object>
+    public interface ICache : IDisposable, ICacheOptions, ICacheOperations, ICacheSingleKeyOperations<string, object>, ICacheMultipleKeysOperations<string, object>
     {
         /// <summary>
         /// Unique name of the cache.
         /// </summary>
         string Name { get; }
-
-        /// <summary>
-        /// Gets items from the cache.
-        /// This method hides cache provider failures (and logs them),
-        /// uses the factory method to get the object if cache provider fails.
-        /// </summary>
-        /// <param name="keys">Keys</param>
-        /// <param name="factory">Factory method to create cache item if not exists</param>
-        /// <returns>Cached item</returns>
-        object[] Get(string[] keys, Func<string, object> factory);
-
-        /// <summary>
-        /// Gets items from the cache.
-        /// This method hides cache provider failures (and logs them),
-        /// uses the factory method to get the object if cache provider fails.
-        /// </summary>
-        /// <param name="keys">Keys</param>
-        /// <param name="factory">Factory method to create cache item if not exists</param>
-        /// <returns>Cached items</returns>
-        Task<object[]> GetAsync(string[] keys, Func<string, Task<object>> factory);
-
-        /// <summary>
-        /// Gets items from the cache. For every key that is not found, a null value is returned.
-        /// </summary>
-        /// <param name="keys">Keys</param>
-        /// <returns>Cached items</returns>
-        object[] GetOrDefault(string[] keys);
-
-        /// <summary>
-        /// Gets items from the cache. For every key that is not found, a null value is returned.
-        /// </summary>
-        /// <param name="keys">Keys</param>
-        /// <returns>Cached items</returns>
-        Task<object[]> GetOrDefaultAsync(string[] keys);
-
-        /// <summary>
-        /// Saves/Overrides items in the cache by the pairs.
-        /// Use one of the expire times at most (<paramref name="slidingExpireTime"/> or <paramref name="absoluteExpireTime"/>).
-        /// If none of them is specified, then
-        /// <see cref="DefaultAbsoluteExpireTime"/> will be used if it's not null. Othewise, <see cref="DefaultSlidingExpireTime"/>
-        /// will be used.
-        /// </summary>
-        /// <param name="pairs">Pairs</param>
-        /// <param name="slidingExpireTime">Sliding expire time</param>
-        /// <param name="absoluteExpireTime">Absolute expire time</param>
-        void Set(KeyValuePair<string, object>[] pairs, TimeSpan? slidingExpireTime = null, TimeSpan? absoluteExpireTime = null);
-
-        /// <summary>
-        /// Saves/Overrides items in the cache by the pairs.
-        /// Use one of the expire times at most (<paramref name="slidingExpireTime"/> or <paramref name="absoluteExpireTime"/>).
-        /// If none of them is specified, then
-        /// <see cref="DefaultAbsoluteExpireTime"/> will be used if it's not null. Othewise, <see cref="DefaultSlidingExpireTime"/>
-        /// will be used.
-        /// </summary>
-        /// <param name="pairs">Pairs</param>
-        /// <param name="slidingExpireTime">Sliding expire time</param>
-        /// <param name="absoluteExpireTime">Absolute expire time</param>
-        Task SetAsync(KeyValuePair<string, object>[] pairs, TimeSpan? slidingExpireTime = null, TimeSpan? absoluteExpireTime = null);
-
-        /// <summary>
-        /// Removes cache items by their keys.
-        /// </summary>
-        /// <param name="keys">Keys</param>
-        void Remove(string[] keys);
-
-        /// <summary>
-        /// Removes cache items by their keys.
-        /// </summary>
-        /// <param name="keys">Keys</param>
-        Task RemoveAsync(string[] keys);
     }
 }
--- src/Abp/Runtime/Caching/ITypedCache.cs
+++ src/Abp/Runtime/Caching/ITypedCache.cs
@@ -11,7 +11,7 @@ namespace Abp.Runtime.Caching
     /// </summary>
     /// <typeparam name="TKey">Key type for cache items</typeparam>
     /// <typeparam name="TValue">Value type for cache items</typeparam>
-    public interface ITypedCache<TKey, TValue> : IDisposable, ICacheOptions, ICacheOperations, ICacheSingleKeyOperations<TKey, TValue>
+    public interface ITypedCache<TKey, TValue> : IDisposable, ICacheOptions, ICacheOperations, ICacheSingleKeyOperations<TKey, TValue>, ICacheMultipleKeysOperations<TKey, TValue>
     {
         /// <summary>
         /// Unique name of the cache.
@@ -22,63 +22,5 @@ namespace Abp.Runtime.Caching
         /// Gets the internal cache.
         /// </summary>
         ICache InternalCache { get; }
-
-        /// <summary>
-        /// Gets items from the cache.
-        /// </summary>
-        /// <param name="keys">Keys</param>
-        /// <param name="factory">Factory method to create cache item if not exists</param>
-        /// <returns>Cached items</returns>
-        TValue[] Get(TKey[] keys, Func<TKey, TValue> factory);
-
-        /// <summary>
-        /// Gets items from the cache.
-        /// </summary>
-        /// <param name="keys">Keys</param>
-        /// <param name="factory">Factory method to create cache item if not exists</param>
-        /// <returns>Cached item</returns>
-        Task<TValue[]> GetAsync(TKey[] keys, Func<TKey, Task<TValue>> factory);
-
-        /// <summary>
-        /// Gets items from the cache. For every key that is not found, a null value is returned.
-        /// </summary>
-        /// <param name="keys">Keys</param>
-        /// <returns>Cached items</returns>
-        TValue[] GetOrDefault(TKey[] keys);
-
-        /// <summary>
-        /// Gets items from the cache. For every key that is not found, a null value is returned.
-        /// </summary>
-        /// <param name="keys">Keys</param>
-        /// <returns>Cached items</returns>
-        Task<TValue[]> GetOrDefaultAsync(TKey[] keys);
-
-        /// <summary>
-        /// Saves/Overrides items in the cache by the pairs.
-        /// </summary>
-        /// <param name="pairs">Pairs</param>
-        /// <param name="slidingExpireTime">Sliding expire time</param>
-        /// <param name="absoluteExpireTime">Absolute expire time</param>
-        void Set(KeyValuePair<TKey, TValue>[] pairs, TimeSpan? slidingExpireTime = null, TimeSpan? absoluteExpireTime = null);
-
-        /// <summary>
-        /// Saves/Overrides items in the cache by the pairs.
-        /// </summary>
-        /// <param name="pairs">Pairs</param>
-        /// <param name="slidingExpireTime">Sliding expire time</param>
-        /// <param name="absoluteExpireTime">Absolute expire time</param>
-        Task SetAsync(KeyValuePair<TKey, TValue>[] pairs, TimeSpan? slidingExpireTime = null, TimeSpan? absoluteExpireTime = null);
-
-        /// <summary>
-        /// Removes cache items by their keys.
-        /// </summary>
-        /// <param name="keys">Keys</param>
-        void Remove(TKey[] keys);
-
-        /// <summary>
-        /// Removes cache items by their keys.
-        /// </summary>
-        /// <param name="keys">Keys</param>
-        Task RemoveAsync(TKey[] keys);
     }
 }
