79d89e3876f09fda566bb819fe6c905c3aef4f82
maliming <malimings@gmail.com>
2019-10-01 18:14:28 +0800

    Refactoring the abp.message function.

--- src/Abp.Web.Resources/Abp/Framework/scripts/libs/abp.sweet-alert.js
+++ src/Abp.Web.Resources/Abp/Framework/scripts/libs/abp.sweet-alert.js
@@ -34,19 +34,17 @@
 
     /* MESSAGE **************************************************/
 
-    var showMessage = function (type, message, title, isHtml, options) {
-
-        if (!title) {
-            title = message;
-            message = undefined;
+    var showMessage = function (type, message, title, callback, options) {
+        options = options || {};
+        var messageContent = {};
+        if(title){
+            messageContent.title = title;
         }
 
-        var messageContent = {
-            title: title
-        };
-
-        if (isHtml) {
+        if (options.isHtml) {
             var el = document.createElement('div');
+            //https://github.com/t4t5/sweetalert/issues/842
+            el.style = 'position: relative;';
             el.innerHTML = message;
 
             messageContent.content = el;
@@ -63,64 +61,31 @@
         );
 
         return $.Deferred(function ($dfd) {
-            sweetAlert(opts).then(function () {
-                $dfd.resolve();
-            });
-        });
-    };
-
-    abp.message.info = function (message, title, isHtml, options) {
-        return showMessage('info', message, title, isHtml, options);
-    };
-
-    abp.message.success = function (message, title, isHtml, options) {
-        return showMessage('success', message, title, isHtml, options);
-    };
-
-    abp.message.warn = function (message, title, isHtml, options) {
-        return showMessage('warn', message, title, isHtml, options);
-    };
-
-    abp.message.error = function (message, title, isHtml, options) {
-        return showMessage('error', message, title, isHtml, options);
-    };
-
-    abp.message.confirm = function (message, titleOrCallback, callback, isHtml, options) {
-        var messageContent;
-
-        if (isHtml) {
-            var el = document.createElement('div');
-            el.innerHTML = message;
-
-            messageContent = {
-                content: el
-            }
-        } else {
-            messageContent = {
-                text: message
-            }
-        }
-
-        if ($.isFunction(titleOrCallback)) {
-            callback = titleOrCallback;
-        } else if (titleOrCallback) {
-            messageContent.title = titleOrCallback;
-        };
-
-        var opts = $.extend(
-            {},
-            abp.libs.sweetAlert.config['default'],
-            abp.libs.sweetAlert.config.confirm,
-            messageContent,
-            options
-        );
-
-        return $.Deferred(function ($dfd) {
             sweetAlert(opts).then(function (isConfirmed) {
                 callback && callback(isConfirmed);
                 $dfd.resolve(isConfirmed);
             });
         });
+    };
+
+    abp.message.info = function (message, title, options) {
+        return showMessage('info', message, title, null, options);
+    };
+
+    abp.message.success = function (message, title, options) {
+        return showMessage('success', message, title, null, options);
+    };
+
+    abp.message.warn = function (message, title, options) {
+        return showMessage('warn', message, title, null, options);
+    };
+
+    abp.message.error = function (message, title, options) {
+        return showMessage('error', message, title, null, options);
+    };
+
+    abp.message.confirm = function (message, title, callback, options) {
+        return showMessage('confirm', message, title, callback, options);
     };
 
     abp.event.on('abp.dynamicScriptsInitialized', function () {
