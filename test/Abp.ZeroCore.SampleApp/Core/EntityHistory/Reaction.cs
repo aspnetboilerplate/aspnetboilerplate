5119fb5eafa4049bcd1cdad2611aafc784f3b4ae
ryancyq <quan0203@gmail.com>
2019-10-15 02:18:16 +0800

    add efcore test case

---
+++ test/Abp.ZeroCore.SampleApp/Core/EntityHistory/Reaction.cs
@@ -0,0 +1,9 @@
+﻿using Abp.Domain.Entities.Auditing;
+
+namespace Abp.ZeroCore.SampleApp.Core.EntityHistory
+{
+    public class Reaction : FullAuditedAggregateRoot
+    {
+        public string Name { get; set; }
+    }
+}
