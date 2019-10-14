92cbe1563705b1c23d71c033395944b794230b19
ryancyq <quan0203@gmail.com>
2019-10-15 02:07:56 +0800

    add ef6 test case

---
+++ test/Abp.Zero.SampleApp/EntityHistory/Reaction.cs
@@ -0,0 +1,9 @@
+﻿using Abp.Domain.Entities.Auditing;
+
+namespace Abp.Zero.SampleApp.EntityHistory
+{
+    public class Reaction : FullAuditedAggregateRoot
+    {
+        public string Name { get; set; }
+    }
+}
