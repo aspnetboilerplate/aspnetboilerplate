d4c2c128b632f500acd896d88e63e946a7ba41c5
Musa Demir <musa.demir@volosoft.com>
2019-11-27 16:17:14 +0300

    add tests

---
+++ test/Abp.Tests/Localization/DefaultLanguageProvider_Test.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using Abp.Localization;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+using Shouldly;
+using Xunit;
+
+namespace Abp.Tests.Localization
+{
+    public class DefaultLanguageProvider_Test : TestBaseWithLocalIocManager
+    {
+        public DefaultLanguageProvider_Test()
+        {
+            LocalIocManager.Register<ILanguageProvider, DefaultLanguageProvider>();
+            var bootstrapper = AbpBootstrapper.Create<DefaultLanguageProviderLangModule>(options =>
+            {
+                options.IocManager = LocalIocManager;
+            });
+
+            bootstrapper.Initialize();
+        }
+
+        [Fact]
+        public void Should_Get_Languages()
+        {
+            var languageProvider = LocalIocManager.Resolve<ILanguageProvider>();
+
+            var allLanguages = languageProvider.GetLanguages();
+            allLanguages.Count.ShouldBe(2);
+        }
+
+        [Fact]
+        public void Should_Get_Active_Languages()
+        {
+            var languageProvider = LocalIocManager.Resolve<ILanguageProvider>();
+
+            var activeLanguages = languageProvider.GetActiveLanguages();
+            activeLanguages.Count.ShouldBe(1);
+            activeLanguages.Single().Name.ShouldBe("en");
+        }
+    }
+
+    public class DefaultLanguageProviderLangModule : AbpModule
+    {
+        public override void PreInitialize()
+        {
+            Configuration.Localization.Languages.Add(new LanguageInfo("en", "English", isDefault: true));
+            Configuration.Localization.Languages.Add(new LanguageInfo("tr", "Türkçe", isDisabled: true));
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(typeof(DefaultLanguageProviderLangModule).GetAssembly());
+        }
+    }
+}
--- test/Abp.Zero.SampleApp.Tests/Localization/ApplicationLanguageManager_Tests.cs
+++ test/Abp.Zero.SampleApp.Tests/Localization/ApplicationLanguageManager_Tests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Abp.Domain.Repositories;
 using Abp.Localization;
 using Abp.Zero.SampleApp.MultiTenancy;
 using Shouldly;
@@ -12,11 +13,13 @@ namespace Abp.Zero.SampleApp.Tests.Localization
     {
         private readonly Tenant _defaultTenant;
         private readonly IApplicationLanguageManager _languageManager;
+        private readonly IRepository<ApplicationLanguage> _applicationLanguageRepository;
 
         public ApplicationLanguageManager_Tests()
         {
             _defaultTenant = GetDefaultTenant();
             _languageManager = Resolve<IApplicationLanguageManager>();
+            _applicationLanguageRepository = Resolve<IRepository<ApplicationLanguage>>();
         }
 
         [Fact]
@@ -71,6 +74,32 @@ namespace Abp.Zero.SampleApp.Tests.Localization
             var languages = await _languageManager.GetLanguagesAsync(null);
             languages.Count.ShouldBe(2);
             languages.FirstOrDefault(l => l.Name == "tr").ShouldBeNull();
+        }
+        [Fact]
+        public async Task Should_Get_Host_Active_Languages()
+        {
+            await Should_Get_Only_Active_Languages(null);
+        }
+
+        [Fact]
+        public async Task Should_Get_Tenant_Active_Languages()
+        {
+            await Should_Get_Only_Active_Languages(_defaultTenant.Id);
+        }
+
+        private async Task Should_Get_Only_Active_Languages(int? tenantId)
+        {
+            var allLanguages = await _languageManager.GetLanguagesAsync(tenantId);
+            allLanguages.ShouldNotBeEmpty("Can not check that without any languages");
+
+            await WithUnitOfWorkAsync(async () =>
+            {
+                var language = await _applicationLanguageRepository.GetAsync(allLanguages.First().Id);
+                language.IsDisabled = true;
+            });
+
+            var activeLanguages = await _languageManager.GetActiveLanguagesAsync(tenantId);
+            activeLanguages.Count.ShouldBe(allLanguages.Count - 1);
         }
     }
 }
