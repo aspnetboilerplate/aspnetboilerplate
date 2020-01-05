0bd11733787b958264f8979816ce37827af7a20f
ryancyq <quan0203@gmail.com>
2020-01-06 02:52:31 +0800

    obsolete sync methods in async queryable executer

--- src/Abp/Linq/IAsyncQueryableExecuter.cs
+++ src/Abp/Linq/IAsyncQueryableExecuter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,14 +12,17 @@ namespace Abp.Linq
     {
         Task<int> CountAsync<T>(IQueryable<T> queryable);
 
+        [Obsolete("Use System.Linq.Queryable.Count() instead.")]
         int Count<T>(IQueryable<T> queryable);
 
         Task<List<T>> ToListAsync<T>(IQueryable<T> queryable);
 
+        [Obsolete("Use System.Linq.Queryable.ToList() instead.")]
         List<T> ToList<T>(IQueryable<T> queryable);
 
         Task<T> FirstOrDefaultAsync<T>(IQueryable<T> queryable);
 
+        [Obsolete("Use System.Linq.Queryable.FirstOrDefault() instead.")]
         T FirstOrDefault<T>(IQueryable<T> queryable);
 
         Task<bool> AnyAsync<T>(IQueryable<T> queryable);
