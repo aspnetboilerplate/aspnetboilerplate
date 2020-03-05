812efdf22f57a63ccd219c8e13de7c7032c4d9af
Ryan Chang <ryancyq@gmail.com>
2020-03-05 09:40:40 +0800

    remove obsolete methods in async queryable executer

--- src/Abp/Linq/IAsyncQueryableExecuter.cs
+++ src/Abp/Linq/IAsyncQueryableExecuter.cs
@@ -12,18 +12,9 @@ namespace Abp.Linq
     {
         Task<int> CountAsync<T>(IQueryable<T> queryable);
 
-        [Obsolete("Use System.Linq.Queryable.Count() instead.")]
-        int Count<T>(IQueryable<T> queryable);
-
         Task<List<T>> ToListAsync<T>(IQueryable<T> queryable);
 
-        [Obsolete("Use System.Linq.Queryable.ToList() instead.")]
-        List<T> ToList<T>(IQueryable<T> queryable);
-
         Task<T> FirstOrDefaultAsync<T>(IQueryable<T> queryable);
-
-        [Obsolete("Use System.Linq.Queryable.FirstOrDefault() instead.")]
-        T FirstOrDefault<T>(IQueryable<T> queryable);
 
         Task<bool> AnyAsync<T>(IQueryable<T> queryable);
     }
