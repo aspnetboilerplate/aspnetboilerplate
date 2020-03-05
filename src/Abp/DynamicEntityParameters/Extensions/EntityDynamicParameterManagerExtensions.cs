781ae0b08d6301e46509d08e4617b1d304edb4c2
Musa Demir <musa.demir@volosoft.com>
2020-03-05 17:35:18 +0300

    extend extensions

--- src/Abp/DynamicEntityParameters/Extensions/EntityDynamicParameterManagerExtensions.cs
+++ src/Abp/DynamicEntityParameters/Extensions/EntityDynamicParameterManagerExtensions.cs
@@ -6,28 +6,66 @@ namespace Abp.DynamicEntityParameters.Extensions
 {
     public static class EntityDynamicParameterManagerExtensions
     {
-        public static List<EntityDynamicParameter> GetAll<TEntity, TPrimaryKey>(this EntityDynamicParameterManager manager)
+        public static List<EntityDynamicParameter> GetAll<TEntity, TPrimaryKey>(this IEntityDynamicParameterManager manager)
             where TEntity : IEntity<TPrimaryKey>
         {
             return manager.GetAll(entityFullName: typeof(TEntity).FullName);
         }
 
-        public static List<EntityDynamicParameter> GetAll<TEntity>(this EntityDynamicParameterManager manager)
+        public static List<EntityDynamicParameter> GetAll<TEntity>(this IEntityDynamicParameterManager manager)
             where TEntity : IEntity<int>
         {
             return manager.GetAll<TEntity, int>();
         }
 
-        public static Task<List<EntityDynamicParameter>> GetAllAsync<TEntity, TPrimaryKey>(this EntityDynamicParameterManager manager)
+        public static Task<List<EntityDynamicParameter>> GetAllAsync<TEntity, TPrimaryKey>(this IEntityDynamicParameterManager manager)
             where TEntity : IEntity<TPrimaryKey>
         {
             return manager.GetAllAsync(entityFullName: typeof(TEntity).FullName);
         }
 
-        public static Task<List<EntityDynamicParameter>> GetAllAsync<TEntity>(this EntityDynamicParameterManager manager)
+        public static Task<List<EntityDynamicParameter>> GetAllAsync<TEntity>(this IEntityDynamicParameterManager manager)
             where TEntity : IEntity<int>
         {
             return manager.GetAllAsync<TEntity, int>();
+        }
+
+        public static EntityDynamicParameter Add<TEntity>(this IEntityDynamicParameterManager manager, int dynamicParameterId, int? tenantId)
+            where TEntity : IEntity<int>
+        {
+            return manager.Add<TEntity, int>(dynamicParameterId, tenantId);
+        }
+
+        public static EntityDynamicParameter Add<TEntity, TPrimaryKey>(this IEntityDynamicParameterManager manager, int dynamicParameterId, int? tenantId)
+            where TEntity : IEntity<TPrimaryKey>
+        {
+            var entity = new EntityDynamicParameter()
+            {
+                DynamicParameterId = dynamicParameterId,
+                EntityFullName = typeof(TEntity).FullName,
+                TenantId = tenantId
+            };
+            manager.Add(entity);
+            return entity;
+        }
+
+        public static Task<EntityDynamicParameter> AddAsync<TEntity>(this IEntityDynamicParameterManager manager, int dynamicParameterId, int? tenantId)
+            where TEntity : IEntity<int>
+        {
+            return manager.AddAsync<TEntity, int>(dynamicParameterId, tenantId);
+        }
+
+        public static async Task<EntityDynamicParameter> AddAsync<TEntity, TPrimaryKey>(this IEntityDynamicParameterManager manager, int dynamicParameterId, int? tenantId)
+            where TEntity : IEntity<TPrimaryKey>
+        {
+            var entity = new EntityDynamicParameter()
+            {
+                DynamicParameterId = dynamicParameterId,
+                EntityFullName = typeof(TEntity).FullName,
+                TenantId = tenantId
+            };
+            await manager.AddAsync(entity);
+            return entity;
         }
     }
 }
