fce7b39ecd88a7f8c2026c094108cf8b0b81ad40
Arkat Erol <arkaterol@outlook.com>
2020-03-05 10:03:44 +0300

    create-push.ps1 update

--- nupkg/create-push.ps1
+++ nupkg/create-push.ps1
@@ -1,9 +1,9 @@
 $prefix = '"..\tools\nuget\NuGet.exe" "push" "'
 $suffix = '" API_KEY -Source https://api.nuget.org/v3/index.json'
 
-Get-ChildItem .\*.nupkg | Select-Object { $prefix + $_.Name + $suffix }  | Out-File -width 1000 .\push.ps1 -Force
+Get-ChildItem .\*.nupkg | Select-Object { $prefix + $_.Name + $suffix }  | Out-File -width 1000 .\push.bat -Force
 
-(gc .\push.ps1 | select -Skip 3) | sc .\push.ps1
+(gc .\push.bat | select -Skip 3) | sc .\push.bat
 
 
 
