385486a2b5ba9da0ce09f6a7216148c907cb9340
ryancyq <quan0203@gmail.com>
2020-03-15 17:39:22 +0800

    simplify ConditionalValue

--- src/Abp/Data/ConditionalValue.cs
+++ src/Abp/Data/ConditionalValue.cs
@@ -8,48 +8,26 @@
     public struct ConditionalValue<TValue>
     {
         /// <summary>
-        /// Is there a value.
-        /// </summary>
-        private readonly bool hasValue;
-
-        /// <summary>
-        /// The value.
-        /// </summary>
-        private readonly TValue value;
-
-        /// <summary>
         /// Initializes a new instance of the <cref name="ConditionalValue{TValue}"/> class with the given value.
         /// </summary>
         /// <param name="hasValue">Indicates whether the value is valid.</param>
         /// <param name="value">The value.</param>
         public ConditionalValue(bool hasValue, TValue value)
         {
-            this.hasValue = hasValue;
-            this.value = value;
+            HasValue = hasValue;
+            Value = value;
         }
 
         /// <summary>
         /// Gets a value indicating whether the current <cref name="ConditionalValue{TValue}"/> object has a valid value of its underlying type.
         /// </summary>
         /// <returns><languageKeyword>true</languageKeyword>: Value is valid, <languageKeyword>false</languageKeyword> otherwise.</returns>
-        public bool HasValue
-        {
-            get
-            {
-                return this.hasValue;
-            }
-        }
+        public bool HasValue { get; }
 
         /// <summary>
         /// Gets the value of the current <cref name="ConditionalValue{TValue}"/> object if it has been assigned a valid underlying value.
         /// </summary>
         /// <returns>The value of the object. If HasValue is <languageKeyword>false</languageKeyword>, returns the default value for type of the TValue parameter.</returns>
-        public TValue Value
-        {
-            get
-            {
-                return this.value;
-            }
-        }
+        public TValue Value { get; }
     }
 }
