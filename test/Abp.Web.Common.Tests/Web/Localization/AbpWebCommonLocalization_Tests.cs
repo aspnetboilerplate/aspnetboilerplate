6994971258b8da391cb5281823f8c5b181f4cb84
maliming <malimings@gmail.com>
2020-05-15 13:43:17 +0800

    Add some unit tests.

--- test/Abp.Web.Common.Tests/Web/Localization/AbpWebCommonLocalization_Tests.cs
+++ test/Abp.Web.Common.Tests/Web/Localization/AbpWebCommonLocalization_Tests.cs
@@ -1,4 +1,5 @@
-﻿using Abp.Localization;
+﻿using System.Collections.Generic;
+using Abp.Localization;
 using Abp.TestBase;
 using Shouldly;
 using Xunit;
@@ -15,7 +16,7 @@ namespace Abp.Web.Common.Tests.Web.Localization
         }
 
         [Fact]
-        public void Should_Localize_AbpWebCommon_Texts()
+        public void Should_Localize_AbpWebCommon_Text()
         {
             using (CultureInfoHelper.Use("en"))
             {
@@ -23,6 +24,21 @@ namespace Abp.Web.Common.Tests.Web.Localization
                     .GetSource(AbpWebConsts.LocalizaionSourceName)
                     .GetString("ValidationError")
                     .ShouldBe("Your request is not valid!");
+            }
+        }
+
+        [Fact]
+        public void Should_Localize_AbpWebCommon_Texts()
+        {
+            using (CultureInfoHelper.Use("en"))
+            {
+                var texts = _localizationManager
+                    .GetSource(AbpWebConsts.LocalizaionSourceName)
+                    .GetStrings(new List<string> {"ValidationError", "InternalServerError"});
+
+                texts.ShouldContain(x => x == "Your request is not valid!");
+                texts.ShouldContain(x => x == "An internal error occurred during your request!");
+
             }
         }
     }
