4745d41aacbb5ed8fe7a097d3e2691972149dc76
maliming <6908465+maliming@users.noreply.github.com>
2020-06-11 15:11:27 +0800

    Refactor the HardDelete function to ensure that there is a unit of work in the current context.
    https://support.aspnetzero.com/QA/Questions/9166#answer-ea7d68e0-1647-ae28-164d-39f5afd73c3f

--- src/Abp/Domain/Repositories/RepositoryExtensions.cs
+++ src/Abp/Domain/Repositories/RepositoryExtensions.cs
@@ -92,76 +92,107 @@ namespace Abp.Domain.Repositories
             throw new ArgumentException($"Given {nameof(repository)} is not inherited from {typeof(AbpRepositoryBase<TEntity, TPrimaryKey>).AssemblyQualifiedName}");
         }
 
-        public static async Task HardDeleteAsync<TEntity, TPrimaryKey>(this IRepository<TEntity, TPrimaryKey> repository, TEntity entity)
-            where TEntity : class, IEntity<TPrimaryKey>, ISoftDelete
-        {
-            var repo = ProxyHelper.UnProxy(repository) as IRepository<TEntity, TPrimaryKey>;
-            if (repo == null)
-            {
-                throw new ArgumentException($"Given {nameof(repository)} is not inherited from {typeof(IRepository<TEntity, TPrimaryKey>).AssemblyQualifiedName}");
-            }
-
-            var items = ((IUnitOfWorkManagerAccessor)repo).UnitOfWorkManager.Current.Items;
-            var hardDeleteEntities = items.GetOrAdd(UnitOfWorkExtensionDataTypes.HardDelete, () => new HashSet<string>()) as HashSet<string>;
-
-            var tenantId = GetCurrentTenantIdOrNull(repo.GetIocResolver());
-            var hardDeleteKey = EntityHelper.GetHardDeleteKey(entity, tenantId);
-
-            hardDeleteEntities.Add(hardDeleteKey);
-
-            await repo.DeleteAsync(entity);
-        }
-
         public static void HardDelete<TEntity, TPrimaryKey>(this IRepository<TEntity, TPrimaryKey> repository, TEntity entity)
             where TEntity : class, IEntity<TPrimaryKey>, ISoftDelete
         {
-            var repo = ProxyHelper.UnProxy(repository) as IRepository<TEntity, TPrimaryKey>;
-            if (repo == null)
+            HardDelete(repository, null, entity);
+        }
+
+
+        public static void HardDelete<TEntity, TPrimaryKey>(this IRepository<TEntity, TPrimaryKey> repository, Expression<Func<TEntity, bool>> predicate)
+            where TEntity : class, IEntity<TPrimaryKey>, ISoftDelete
+        {
+            HardDelete(repository, predicate, null);
+        }
+
+        private static void HardDelete<TEntity, TPrimaryKey>(this IRepository<TEntity, TPrimaryKey> repository, Expression<Func<TEntity, bool>> predicate, TEntity entity)
+            where TEntity : class, IEntity<TPrimaryKey>, ISoftDelete
+        {
+            var currentUnitOfWork = GetCurrentUnitOfWorkOrThrowException(repository);
+            var hardDeleteEntities = currentUnitOfWork.Items.GetOrAdd(UnitOfWorkExtensionDataTypes.HardDelete, () => new HashSet<string>()) as HashSet<string>;
+
+            var tenantId = currentUnitOfWork.GetTenantId();
+
+            if (predicate != null)
             {
-                throw new ArgumentException($"Given {nameof(repository)} is not inherited from {typeof(IRepository<TEntity, TPrimaryKey>).AssemblyQualifiedName}");
+                foreach (var e in repository.GetAll().Where(predicate).ToList())
+                {
+                    var hardDeleteKey = EntityHelper.GetHardDeleteKey(e, tenantId);
+
+                    hardDeleteEntities.Add(hardDeleteKey);
+
+                    repository.Delete(e);
+                }
             }
 
-            var items = ((IUnitOfWorkManagerAccessor)repo).UnitOfWorkManager.Current.Items;
-            var hardDeleteEntities = items.GetOrAdd(UnitOfWorkExtensionDataTypes.HardDelete, () => new HashSet<string>()) as HashSet<string>;
+            if (entity != null)
+            {
+                var hardDeleteKey = EntityHelper.GetHardDeleteKey(entity, tenantId);
 
-            var tenantId = GetCurrentTenantIdOrNull(repo.GetIocResolver());
-            var hardDeleteKey = EntityHelper.GetHardDeleteKey(entity, tenantId);
+                hardDeleteEntities.Add(hardDeleteKey);
 
-            hardDeleteEntities.Add(hardDeleteKey);
+                repository.Delete(entity);
+            }
+        }
 
-            repo.Delete(entity);
+        public static async Task HardDeleteAsync<TEntity, TPrimaryKey>(this IRepository<TEntity, TPrimaryKey> repository, TEntity entity)
+            where TEntity : class, IEntity<TPrimaryKey>, ISoftDelete
+        {
+            await HardDeleteAsync(repository, null, entity);
         }
 
         public static async Task HardDeleteAsync<TEntity, TPrimaryKey>(this IRepository<TEntity, TPrimaryKey> repository, Expression<Func<TEntity, bool>> predicate)
             where TEntity : class, IEntity<TPrimaryKey>, ISoftDelete
         {
-            foreach (var entity in repository.GetAll().Where(predicate).ToList())
-            {
-                await repository.HardDeleteAsync(entity);
-            }
+            await HardDeleteAsync(repository, predicate, null);
         }
 
-        public static void HardDelete<TEntity, TPrimaryKey>(this IRepository<TEntity, TPrimaryKey> repository, Expression<Func<TEntity, bool>> predicate)
+        private static async Task HardDeleteAsync<TEntity, TPrimaryKey>(this IRepository<TEntity, TPrimaryKey> repository, Expression<Func<TEntity, bool>> predicate, TEntity entity)
             where TEntity : class, IEntity<TPrimaryKey>, ISoftDelete
         {
-            foreach (var entity in repository.GetAll().Where(predicate).ToList())
-            {
-                repository.HardDelete(entity);
-            }
-        }
+            var currentUnitOfWork = GetCurrentUnitOfWorkOrThrowException(repository);
+            var hardDeleteEntities = currentUnitOfWork.Items.GetOrAdd(UnitOfWorkExtensionDataTypes.HardDelete, () => new HashSet<string>()) as HashSet<string>;
 
-        private static int? GetCurrentTenantIdOrNull(IIocResolver iocResolver)
-        {
-            using (var scope = iocResolver.CreateScope())
+            var tenantId = currentUnitOfWork.GetTenantId();
+
+            if (predicate != null)
             {
-                var currentUnitOfWorkProvider = scope.Resolve<ICurrentUnitOfWorkProvider>();
-                if (currentUnitOfWorkProvider?.Current != null)
+                foreach (var e in repository.GetAll().Where(predicate).ToList())
                 {
-                    return currentUnitOfWorkProvider.Current.GetTenantId();
+                    var hardDeleteKey = EntityHelper.GetHardDeleteKey(e, tenantId);
+
+                    hardDeleteEntities.Add(hardDeleteKey);
+
+                    await repository.DeleteAsync(e);
                 }
             }
 
-            return iocResolver.Resolve<IAbpSession>().TenantId;
+            if (entity != null)
+            {
+                var hardDeleteKey = EntityHelper.GetHardDeleteKey(entity, tenantId);
+
+                hardDeleteEntities.Add(hardDeleteKey);
+
+                await repository.DeleteAsync(entity);
+            }
+        }
+
+        private static IActiveUnitOfWork GetCurrentUnitOfWorkOrThrowException<TEntity, TPrimaryKey>(IRepository<TEntity, TPrimaryKey> repository)
+            where TEntity : class, IEntity<TPrimaryKey>, ISoftDelete
+        {
+            var repo = ProxyHelper.UnProxy(repository) as IRepository<TEntity, TPrimaryKey>;
+            if (repo == null)
+            {
+                throw new ArgumentException($"Given {nameof(repository)} is not inherited from {typeof(IRepository<TEntity, TPrimaryKey>).AssemblyQualifiedName}");
+            }
+
+            var currentUnitOfWork = ((IUnitOfWorkManagerAccessor) repo).UnitOfWorkManager.Current;
+            if (currentUnitOfWork == null)
+            {
+                throw new AbpException($"There is no unit of work in the current context, The hard delete function can only be used in a unit of work.");
+            }
+
+            return currentUnitOfWork;
         }
     }
 }
