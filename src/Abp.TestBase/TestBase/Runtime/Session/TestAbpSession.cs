0ebd4402a104a9c5bc6e39514dafc165c50d93d6
Angel iT <angels2it@Angels-MacBook-Pro.local>
2020-08-11 11:37:08 +0700

    update test

--- src/Abp.TestBase/TestBase/Runtime/Session/TestAbpSession.cs
+++ src/Abp.TestBase/TestBase/Runtime/Session/TestAbpSession.cs
@@ -56,6 +56,39 @@ namespace Abp.TestBase.Runtime.Session
             }
         }
 
+        public virtual long? BranchId
+        {
+            get
+            {
+                if (!_multiTenancy.IsEnabled)
+                {
+                    return 1;
+                }
+
+                if (_sessionOverrideScopeProvider.GetValue(AbpSessionBase.SessionOverrideContextKey) != null)
+                {
+                    return _sessionOverrideScopeProvider.GetValue(AbpSessionBase.SessionOverrideContextKey).BranchId;
+                }
+
+                var resolvedValue = _branchResolver.ResolveBranchId();
+                if (resolvedValue != null)
+                {
+                    return resolvedValue;
+                }
+
+                return _branchId;
+            }
+            set
+            {
+                if (!_multiTenancy.IsEnabled && value != 1 && value != null)
+                {
+                    throw new AbpException("Can not set BranchId since multi-tenancy is not enabled. Use IMultiTenancyConfig.IsEnabled to enable it.");
+                }
+
+                _branchId = value;
+            }
+        }
+
         public virtual MultiTenancySides MultiTenancySide { get { return GetCurrentMultiTenancySide(); } }
         
         public virtual long? ImpersonatorUserId { get; set; }
@@ -65,17 +98,21 @@ namespace Abp.TestBase.Runtime.Session
         private readonly IMultiTenancyConfig _multiTenancy;
         private readonly IAmbientScopeProvider<SessionOverride> _sessionOverrideScopeProvider;
         private readonly ITenantResolver _tenantResolver;
+        private readonly IBranchResolver _branchResolver;
         private int? _tenantId;
         private long? _userId;
+        private long? _branchId;
 
         public TestAbpSession(
             IMultiTenancyConfig multiTenancy, 
             IAmbientScopeProvider<SessionOverride> sessionOverrideScopeProvider,
-            ITenantResolver tenantResolver)
+            ITenantResolver tenantResolver,
+            IBranchResolver branchResolver)
         {
             _multiTenancy = multiTenancy;
             _sessionOverrideScopeProvider = sessionOverrideScopeProvider;
             _tenantResolver = tenantResolver;
+            _branchResolver = branchResolver;
         }
 
         protected virtual MultiTenancySides GetCurrentMultiTenancySide()
@@ -85,9 +122,9 @@ namespace Abp.TestBase.Runtime.Session
                 : MultiTenancySides.Tenant;
         }
 
-        public virtual IDisposable Use(int? tenantId, long? userId)
+        public virtual IDisposable Use(int? tenantId, long? userId, long? branchId)
         {
-            return _sessionOverrideScopeProvider.BeginScope(AbpSessionBase.SessionOverrideContextKey, new SessionOverride(tenantId, userId));
+            return _sessionOverrideScopeProvider.BeginScope(AbpSessionBase.SessionOverrideContextKey, new SessionOverride(tenantId, userId, branchId));
         }
     }
 }
