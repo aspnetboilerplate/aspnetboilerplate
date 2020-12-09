1c0a5f37034d35aa0da212a6f48530b59321a37d
maliming <malimings@gmail.com>
2020-12-09 14:12:21 +0800

    Add TenantCache async tests.

--- test/Abp.Zero.SampleApp.Tests/MultiTenancy/TenantCache_Tests.cs
+++ test/Abp.Zero.SampleApp.Tests/MultiTenancy/TenantCache_Tests.cs
@@ -1,4 +1,5 @@
-﻿using Abp.Domain.Repositories;
+﻿using System.Threading.Tasks;
+using Abp.Domain.Repositories;
 using Abp.MultiTenancy;
 using Abp.Zero.SampleApp.MultiTenancy;
 using Shouldly;
@@ -26,7 +27,7 @@ namespace Abp.Zero.SampleApp.Tests.MultiTenancy
             //Assert
             tenant.TenancyName.ShouldBe(Tenant.DefaultTenantName);
         }
-        
+
         [Fact]
         public void Should_Get_Tenant_By_TenancyName()
         {
@@ -46,7 +47,7 @@ namespace Abp.Zero.SampleApp.Tests.MultiTenancy
             //Assert
             tenant.ShouldBeNull();
         }
-        
+
         [Fact]
         public void Should_Refresh_Cache_If_Tenancy_Name_Changes()
         {
@@ -75,6 +76,69 @@ namespace Abp.Zero.SampleApp.Tests.MultiTenancy
 
             //Can get with new name
             tenant = _tenantCache.GetOrNull("Default-Changed");
+            tenant.ShouldNotBeNull();
+            tenant.Id.ShouldBe(1);
+            tenant.IsActive.ShouldBeFalse();
+        }
+
+        [Fact]
+        public async Task Should_Get_Tenant_By_Id_Async()
+        {
+            //Act
+            var tenant = await _tenantCache.GetAsync(1);
+
+            //Assert
+            tenant.TenancyName.ShouldBe(Tenant.DefaultTenantName);
+        }
+
+        [Fact]
+        public async Task  Should_Get_Tenant_By_TenancyName_Async()
+        {
+            //Act
+            var tenant = await _tenantCache.GetOrNullAsync(Tenant.DefaultTenantName);
+
+            //Assert
+            tenant.Id.ShouldBe(1);
+        }
+
+        [Fact]
+        public async Task Should_Get_Null_Tenant_By_TenancyName_If_Not_Found_Async()
+        {
+            //Act
+            var tenant = await _tenantCache.GetOrNullAsync("unknown-tenancy-name");
+
+            //Assert
+            tenant.ShouldBeNull();
+        }
+
+        [Fact]
+        public async Task Should_Refresh_Cache_If_Tenancy_Name_Changes_Async()
+        {
+            //--- Get a known tenant from cache
+
+            //Act
+            var tenant = await _tenantCache.GetOrNullAsync(Tenant.DefaultTenantName);
+
+            //Assert
+            tenant.Id.ShouldBe(1);
+            tenant.IsActive.ShouldBeTrue();
+
+            //--- Change tenant name
+
+            _tenantRepository.Update(tenant.Id, t =>
+            {
+                t.TenancyName = "Default-Changed";
+                t.IsActive = false;
+            });
+
+            //--- Get again from cache
+
+            //Can not get with old name
+            tenant = await _tenantCache.GetOrNullAsync(Tenant.DefaultTenantName);
+            tenant.ShouldBeNull();
+
+            //Can get with new name
+            tenant = await _tenantCache.GetOrNullAsync("Default-Changed");
             tenant.ShouldNotBeNull();
             tenant.Id.ShouldBe(1);
             tenant.IsActive.ShouldBeFalse();
