3f3305940310abaaf38fcab0cb2874ae6b47a8d2
İsmail ÇAĞDAŞ <ismailcagdas@gmail.com>
2020-12-14 14:37:59 +0300

    use dotnet nuget push when publishing nuget packages

--- nupkg/create-push.ps1
+++ nupkg/create-push.ps1
@@ -1,5 +1,5 @@
-$prefix = '"..\tools\nuget\NuGet.exe" "push" "'
-$suffix = '" API_KEY -Source https://api.nuget.org/v3/index.json'
+$prefix = 'dotnet nuget push '
+$suffix = ' API_KEY -Source https://api.nuget.org/v3/index.json'
 
 Get-ChildItem .\*.nupkg | Select-Object { $prefix + $_.Name + $suffix }  | Out-File -width 1000 .\push.bat -Force
 
