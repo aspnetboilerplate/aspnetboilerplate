9a8c07dba406d27f57c5d68c150a4b67d4d5272f
İsmail ÇAĞDAŞ <ismailcagdas@gmail.com>
2021-05-28 10:37:15 +0300

    manually create UnitOfWork in WebhookSubscriptionsStore

--- src/Abp.Zero.Common/Webhooks/WebhookSubscriptionsStore.cs
+++ src/Abp.Zero.Common/Webhooks/WebhookSubscriptionsStore.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Abp.Dependency;
 using Abp.Domain.Repositories;
+using Abp.Domain.Uow;
 using Abp.Linq;
 
 namespace Abp.Webhooks
@@ -16,126 +17,182 @@ namespace Abp.Webhooks
         public IAsyncQueryableExecuter AsyncQueryableExecuter { get; set; }
 
         private readonly IRepository<WebhookSubscriptionInfo, Guid> _webhookSubscriptionRepository;
+        private readonly IUnitOfWorkManager _unitOfWorkManager;
 
         public WebhookSubscriptionsStore(
-            IRepository<WebhookSubscriptionInfo, Guid> webhookSubscriptionRepository
-        )
+            IRepository<WebhookSubscriptionInfo, Guid> webhookSubscriptionRepository,
+            IUnitOfWorkManager unitOfWorkManager)
         {
             _webhookSubscriptionRepository = webhookSubscriptionRepository;
+            _unitOfWorkManager = unitOfWorkManager;
 
             AsyncQueryableExecuter = NullAsyncQueryableExecuter.Instance;
         }
 
-        public virtual Task<WebhookSubscriptionInfo> GetAsync(Guid id)
+        public virtual async Task<WebhookSubscriptionInfo> GetAsync(Guid id)
         {
-            return _webhookSubscriptionRepository.GetAsync(id);
+            return await _unitOfWorkManager.WithUnitOfWorkAsync(async () =>
+                await _webhookSubscriptionRepository.GetAsync(id));
         }
 
         public virtual WebhookSubscriptionInfo Get(Guid id)
         {
-            return _webhookSubscriptionRepository.Get(id);
+            return _unitOfWorkManager.WithUnitOfWork(() => _webhookSubscriptionRepository.Get(id));
         }
 
         public virtual async Task InsertAsync(WebhookSubscriptionInfo webhookInfo)
         {
-            await _webhookSubscriptionRepository.InsertAsync(webhookInfo);
+            await _unitOfWorkManager.WithUnitOfWorkAsync(async () =>
+            {
+                await _webhookSubscriptionRepository.InsertAsync(webhookInfo);
+            });
         }
 
         public virtual void Insert(WebhookSubscriptionInfo webhookInfo)
         {
-            _webhookSubscriptionRepository.Insert(webhookInfo);
+            _unitOfWorkManager.WithUnitOfWork(() => _webhookSubscriptionRepository.Insert(webhookInfo));
         }
 
         public virtual async Task UpdateAsync(WebhookSubscriptionInfo webhookSubscription)
         {
-            await _webhookSubscriptionRepository.UpdateAsync(webhookSubscription);
+            await _unitOfWorkManager.WithUnitOfWorkAsync(async () =>
+            {
+                await _webhookSubscriptionRepository.UpdateAsync(webhookSubscription);
+            });
         }
 
         public virtual void Update(WebhookSubscriptionInfo webhookSubscription)
         {
-            _webhookSubscriptionRepository.Update(webhookSubscription);
+            _unitOfWorkManager.WithUnitOfWork(() => _webhookSubscriptionRepository.Update(webhookSubscription));
         }
 
         public virtual async Task DeleteAsync(Guid id)
         {
-            await _webhookSubscriptionRepository.DeleteAsync(id);
+            await _unitOfWorkManager.WithUnitOfWorkAsync(async () =>
+            {
+                await _webhookSubscriptionRepository.DeleteAsync(id);
+            });
         }
 
         public virtual void Delete(Guid id)
         {
-            _webhookSubscriptionRepository.Delete(id);
+            _unitOfWorkManager.WithUnitOfWork(() => _webhookSubscriptionRepository.Delete(id));
         }
 
-        public virtual Task<List<WebhookSubscriptionInfo>> GetAllSubscriptionsAsync(int? tenantId)
+        public virtual async Task<List<WebhookSubscriptionInfo>> GetAllSubscriptionsAsync(int? tenantId)
         {
-            return _webhookSubscriptionRepository.GetAllListAsync(subscriptionInfo => subscriptionInfo.TenantId == tenantId);
+            return await _unitOfWorkManager.WithUnitOfWorkAsync(async () =>
+            {
+                return await _webhookSubscriptionRepository.GetAllListAsync(subscriptionInfo =>
+                    subscriptionInfo.TenantId == tenantId);
+            });
         }
 
         public virtual List<WebhookSubscriptionInfo> GetAllSubscriptions(int? tenantId)
         {
-            return _webhookSubscriptionRepository.GetAllList(subscriptionInfo => subscriptionInfo.TenantId == tenantId);
+            return _unitOfWorkManager.WithUnitOfWork(() =>
+            {
+                return _webhookSubscriptionRepository.GetAllList(subscriptionInfo =>
+                    subscriptionInfo.TenantId == tenantId);
+            });
         }
 
-        public virtual async Task<List<WebhookSubscriptionInfo>> GetAllSubscriptionsAsync(int? tenantId, string webhookName)
+        public virtual async Task<List<WebhookSubscriptionInfo>> GetAllSubscriptionsAsync(
+            int? tenantId,
+            string webhookName)
         {
-            return await _webhookSubscriptionRepository.GetAllListAsync(subscriptionInfo =>
-                subscriptionInfo.TenantId == tenantId &&
-                subscriptionInfo.IsActive &&
-                subscriptionInfo.Webhooks.Contains("\"" + webhookName + "\""));
-        }
-
-        public virtual List<WebhookSubscriptionInfo> GetAllSubscriptions(int? tenantId, string webhookName)
-        {
-            return _webhookSubscriptionRepository.GetAllList(subscriptionInfo =>
-                subscriptionInfo.TenantId == tenantId &&
-                subscriptionInfo.IsActive &&
-                subscriptionInfo.Webhooks.Contains("\"" + webhookName + "\""));
-        }
-
-        public virtual Task<List<WebhookSubscriptionInfo>> GetAllSubscriptionsOfTenantsAsync(int?[] tenantIds)
-        {
-            return _webhookSubscriptionRepository.GetAllListAsync(subscriptionInfo => tenantIds.Contains(subscriptionInfo.TenantId));
-        }
-
-        public virtual List<WebhookSubscriptionInfo> GetAllSubscriptionsOfTenants(int?[] tenantIds)
-        {
-            return _webhookSubscriptionRepository.GetAllList(subscriptionInfo => tenantIds.Contains(subscriptionInfo.TenantId));
-        }
-
-        public virtual async Task<List<WebhookSubscriptionInfo>> GetAllSubscriptionsOfTenantsAsync(int?[] tenantIds, string webhookName)
-        {
-            return await _webhookSubscriptionRepository.GetAllListAsync(subscriptionInfo =>
-                subscriptionInfo.IsActive &&
-                tenantIds.Contains(subscriptionInfo.TenantId) &&
-                subscriptionInfo.Webhooks.Contains("\"" + webhookName + "\""));
-        }
-
-        public virtual List<WebhookSubscriptionInfo> GetAllSubscriptionsOfTenants(int?[] tenantIds, string webhookName)
-        {
-            return _webhookSubscriptionRepository.GetAllList(subscriptionInfo =>
-                subscriptionInfo.IsActive &&
-                tenantIds.Contains(subscriptionInfo.TenantId) &&
-                subscriptionInfo.Webhooks.Contains("\"" + webhookName + "\""));
-        }
-
-        public virtual Task<bool> IsSubscribedAsync(int? tenantId, string webhookName)
-        {
-            return AsyncQueryableExecuter.AnyAsync(_webhookSubscriptionRepository.GetAll()
-                .Where(subscriptionInfo =>
-                    subscriptionInfo.TenantId == tenantId &&
-                    subscriptionInfo.IsActive &&
-                    subscriptionInfo.Webhooks.Contains("\"" + webhookName + "\"")
-                ));
-        }
-
-        public virtual bool IsSubscribed(int? tenantId, string webhookName)
-        {
-            return _webhookSubscriptionRepository.GetAll()
-                .Any(subscriptionInfo =>
+            return await _unitOfWorkManager.WithUnitOfWorkAsync(async () =>
+            {
+                return await _webhookSubscriptionRepository.GetAllListAsync(subscriptionInfo =>
                     subscriptionInfo.TenantId == tenantId &&
                     subscriptionInfo.IsActive &&
                     subscriptionInfo.Webhooks.Contains("\"" + webhookName + "\"")
                 );
+            });
+        }
+
+        public virtual List<WebhookSubscriptionInfo> GetAllSubscriptions(int? tenantId, string webhookName)
+        {
+            return _unitOfWorkManager.WithUnitOfWork(() =>
+            {
+                return _webhookSubscriptionRepository.GetAllList(subscriptionInfo =>
+                    subscriptionInfo.TenantId == tenantId &&
+                    subscriptionInfo.IsActive &&
+                    subscriptionInfo.Webhooks.Contains("\"" + webhookName + "\"")
+                );
+            });
+        }
+
+        public virtual async Task<List<WebhookSubscriptionInfo>> GetAllSubscriptionsOfTenantsAsync(int?[] tenantIds)
+        {
+            return await _unitOfWorkManager.WithUnitOfWorkAsync(async () =>
+            {
+                return await _webhookSubscriptionRepository.GetAllListAsync(subscriptionInfo =>
+                    tenantIds.Contains(subscriptionInfo.TenantId)
+                );
+            });
+        }
+
+        public virtual List<WebhookSubscriptionInfo> GetAllSubscriptionsOfTenants(int?[] tenantIds)
+        {
+            return _unitOfWorkManager.WithUnitOfWork(() =>
+            {
+                return _webhookSubscriptionRepository.GetAllList(subscriptionInfo =>
+                    tenantIds.Contains(subscriptionInfo.TenantId)
+                );
+            });
+        }
+
+        public virtual async Task<List<WebhookSubscriptionInfo>> GetAllSubscriptionsOfTenantsAsync(
+            int?[] tenantIds,
+            string webhookName)
+        {
+            return await _unitOfWorkManager.WithUnitOfWorkAsync(async () =>
+            {
+                return await _webhookSubscriptionRepository.GetAllListAsync(subscriptionInfo =>
+                    subscriptionInfo.IsActive &&
+                    tenantIds.Contains(subscriptionInfo.TenantId) &&
+                    subscriptionInfo.Webhooks.Contains("\"" + webhookName + "\"")
+                );
+            });
+        }
+
+        public virtual List<WebhookSubscriptionInfo> GetAllSubscriptionsOfTenants(int?[] tenantIds, string webhookName)
+        {
+            return _unitOfWorkManager.WithUnitOfWork(() =>
+            {
+                return _webhookSubscriptionRepository.GetAllList(subscriptionInfo =>
+                    subscriptionInfo.IsActive &&
+                    tenantIds.Contains(subscriptionInfo.TenantId) &&
+                    subscriptionInfo.Webhooks.Contains("\"" + webhookName + "\"")
+                );
+            });
+        }
+
+        public virtual async Task<bool> IsSubscribedAsync(int? tenantId, string webhookName)
+        {
+            return await _unitOfWorkManager.WithUnitOfWorkAsync(async () =>
+            {
+                return await AsyncQueryableExecuter.AnyAsync(_webhookSubscriptionRepository.GetAll()
+                    .Where(subscriptionInfo =>
+                        subscriptionInfo.TenantId == tenantId &&
+                        subscriptionInfo.IsActive &&
+                        subscriptionInfo.Webhooks.Contains("\"" + webhookName + "\"")
+                    ));
+            });
+        }
+
+        public virtual bool IsSubscribed(int? tenantId, string webhookName)
+        {
+            return _unitOfWorkManager.WithUnitOfWork(() =>
+            {
+                return _webhookSubscriptionRepository.GetAll()
+                    .Any(subscriptionInfo =>
+                        subscriptionInfo.TenantId == tenantId &&
+                        subscriptionInfo.IsActive &&
+                        subscriptionInfo.Webhooks.Contains("\"" + webhookName + "\"")
+                    );
+            });
         }
     }
 }
