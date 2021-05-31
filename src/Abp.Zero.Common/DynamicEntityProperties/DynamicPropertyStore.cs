0202bdb9a2f2bcfb4e81cdcafe518c667016cabd
İsmail ÇAĞDAŞ <ismailcagdas@gmail.com>
2021-05-31 19:20:04 +0300

    manually start unit of work in DynamicPropertyStore

--- src/Abp.Zero.Common/DynamicEntityProperties/DynamicPropertyStore.cs
+++ src/Abp.Zero.Common/DynamicEntityProperties/DynamicPropertyStore.cs
@@ -2,76 +2,108 @@
 using System.Threading.Tasks;
 using Abp.Dependency;
 using Abp.Domain.Repositories;
+using Abp.Domain.Uow;
 
 namespace Abp.DynamicEntityProperties
 {
     public class DynamicPropertyStore : IDynamicPropertyStore, ITransientDependency
     {
         private readonly IRepository<DynamicProperty> _dynamicPropertyRepository;
+        private readonly IUnitOfWorkManager _unitOfWorkManager;
 
-        public DynamicPropertyStore(IRepository<DynamicProperty> dynamicPropertyRepository)
+        public DynamicPropertyStore(
+            IRepository<DynamicProperty> dynamicPropertyRepository,
+            IUnitOfWorkManager unitOfWorkManager)
         {
             _dynamicPropertyRepository = dynamicPropertyRepository;
+            _unitOfWorkManager = unitOfWorkManager;
         }
 
         public virtual DynamicProperty Get(int id)
         {
-            return _dynamicPropertyRepository.Get(id);
+            return _unitOfWorkManager.WithUnitOfWork(() =>
+                _dynamicPropertyRepository.Get(id)
+            );
         }
 
-        public virtual Task<DynamicProperty> GetAsync(int id)
+        public virtual async Task<DynamicProperty> GetAsync(int id)
         {
-            return _dynamicPropertyRepository.GetAsync(id);
+            return await _unitOfWorkManager.WithUnitOfWorkAsync(async () =>
+                await _dynamicPropertyRepository.GetAsync(id)
+            );
         }
 
         public virtual DynamicProperty Get(string propertyName)
         {
-            return _dynamicPropertyRepository.FirstOrDefault(x => x.PropertyName == propertyName);
+            return _unitOfWorkManager.WithUnitOfWork(() =>
+            {
+                return _dynamicPropertyRepository.FirstOrDefault(x => x.PropertyName == propertyName);
+            });
         }
 
-        public virtual Task<DynamicProperty> GetAsync(string propertyName)
+        public virtual async Task<DynamicProperty> GetAsync(string propertyName)
         {
-            return _dynamicPropertyRepository.FirstOrDefaultAsync(x => x.PropertyName == propertyName);
+            return await _unitOfWorkManager.WithUnitOfWorkAsync(async () =>
+            {
+                return await _dynamicPropertyRepository.FirstOrDefaultAsync(x => x.PropertyName == propertyName);
+            });
         }
 
         public virtual List<DynamicProperty> GetAll()
         {
-            return _dynamicPropertyRepository.GetAllList();
+            return _unitOfWorkManager.WithUnitOfWork(() => _dynamicPropertyRepository.GetAllList());
         }
 
         public virtual async Task<List<DynamicProperty>> GetAllAsync()
         {
-            return await _dynamicPropertyRepository.GetAllListAsync();
+            return await _unitOfWorkManager.WithUnitOfWorkAsync(async () =>
+                await _dynamicPropertyRepository.GetAllListAsync()
+            );
         }
 
         public virtual void Add(DynamicProperty dynamicProperty)
         {
-            _dynamicPropertyRepository.Insert(dynamicProperty);
+            _unitOfWorkManager.WithUnitOfWork(() =>
+            {
+                _dynamicPropertyRepository.Insert(dynamicProperty);
+            });
         }
 
-        public virtual Task AddAsync(DynamicProperty dynamicProperty)
+        public virtual async Task AddAsync(DynamicProperty dynamicProperty)
         {
-            return _dynamicPropertyRepository.InsertAsync(dynamicProperty);
+            await _unitOfWorkManager.WithUnitOfWorkAsync(async () =>
+                await _dynamicPropertyRepository.InsertAsync(dynamicProperty)
+            );
         }
 
         public virtual void Update(DynamicProperty dynamicProperty)
         {
-            _dynamicPropertyRepository.Update(dynamicProperty);
+            _unitOfWorkManager.WithUnitOfWork(() =>
+            {
+                _dynamicPropertyRepository.Update(dynamicProperty);
+            });
         }
 
-        public virtual Task UpdateAsync(DynamicProperty dynamicProperty)
+        public virtual async Task UpdateAsync(DynamicProperty dynamicProperty)
         {
-            return _dynamicPropertyRepository.UpdateAsync(dynamicProperty);
+            await _unitOfWorkManager.WithUnitOfWorkAsync(async () =>
+                await _dynamicPropertyRepository.UpdateAsync(dynamicProperty)
+            );
         }
 
         public virtual void Delete(int id)
         {
-            _dynamicPropertyRepository.Delete(id);
+            _unitOfWorkManager.WithUnitOfWork(() =>
+            {
+                _dynamicPropertyRepository.Delete(id);
+            });
         }
 
-        public virtual Task DeleteAsync(int id)
+        public virtual async Task DeleteAsync(int id)
         {
-            return _dynamicPropertyRepository.DeleteAsync(id);
+            await _unitOfWorkManager.WithUnitOfWorkAsync(async () =>
+                await _dynamicPropertyRepository.DeleteAsync(id)
+            );
         }
     }
 }
