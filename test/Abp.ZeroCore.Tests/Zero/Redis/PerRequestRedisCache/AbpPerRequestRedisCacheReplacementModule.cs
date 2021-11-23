ec91418644aff8d29b7b9922b4a7fb39b60bca03
Musa Demir <demirmusa96@gmail.com>
2021-11-24 01:07:09 +0300

    implement replacement of AbpPerRequestRedisCache for ICacheManager

---
+++ test/Abp.ZeroCore.Tests/Zero/Redis/PerRequestRedisCache/AbpPerRequestRedisCacheReplacementModule.cs
@@ -0,0 +1,21 @@
+﻿using Abp.Modules;
+using Abp.Reflection.Extensions;
+using Abp.Runtime.Caching.Redis;
+
+namespace Abp.Zero.Redis.PerRequestRedisCache
+{
+    [DependsOn(typeof(AbpAspNetCorePerRequestRedisCacheModule))]
+    public class AbpPerRequestRedisCacheReplacementModule : AbpModule
+    {
+        public override void PreInitialize()
+        {
+            Configuration.Caching.UseRedis(usePerRequestRedisCache: true);
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(
+                typeof(AbpPerRequestRedisCacheReplacementModule).GetAssembly());
+        }
+    }
+}
