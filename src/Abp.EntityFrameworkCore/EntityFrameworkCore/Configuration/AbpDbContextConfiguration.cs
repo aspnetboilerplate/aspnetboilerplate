e27c0f8ff2fb18170889bc4aae3a6095c7a4208c
İsmail ÇAĞDAŞ <ismailcagdas@gmail.com>
2022-02-03 13:24:30 +0300

    create RegisterForEntity method for registering a single entity for generic repositories

--- src/Abp.EntityFrameworkCore/EntityFrameworkCore/Configuration/AbpDbContextConfiguration.cs
+++ src/Abp.EntityFrameworkCore/EntityFrameworkCore/Configuration/AbpDbContextConfiguration.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 using System.Data.Common;
+using Abp.Domain.Entities;
 
 namespace Abp.EntityFrameworkCore.Configuration
 {
@@ -11,7 +13,7 @@ namespace Abp.EntityFrameworkCore.Configuration
         public DbConnection ExistingConnection { get; internal set; }
 
         public DbContextOptionsBuilder<TDbContext> DbContextOptions { get; }
-        
+
         public AbpDbContextConfiguration(string connectionString, DbConnection existingConnection)
         {
             ConnectionString = connectionString;
