6376123584b6dae1767e0a6b78fbb6e09211483c
Musa Demir <demirmusa96@gmail.com>
2022-04-26 16:58:20 +0300

    Create serve.ps1

---
+++ doc/api/docfx/serve.ps1
@@ -0,0 +1,4 @@
+## Note: Required docfx to work. See:https://dotnet.github.io/docfx/tutorial/docfx_getting_started.html
+
+"{0}-SERVING PROJECT-" -f [environment]::NewLine
+docfx docfx_project\docfx.json --serve
