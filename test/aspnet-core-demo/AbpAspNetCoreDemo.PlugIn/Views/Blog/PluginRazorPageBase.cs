41bfc1820ae5944ead436c886369c28dfa822281
maliming <malimings@gmail.com>
2022-06-02 10:00:30 +0800

    Add  `CompiledRazorAssemblyPart`  of Razor projects to `ApplicationParts`.

---
+++ test/aspnet-core-demo/AbpAspNetCoreDemo.PlugIn/Views/Blog/PluginRazorPageBase.cs
@@ -0,0 +1,10 @@
+﻿using Abp.AspNetCore.Mvc.Views;
+using Abp.Runtime.Session;
+using Microsoft.AspNetCore.Mvc.Razor.Internal;
+
+namespace AbpAspNetCoreDemo.PlugIn.Views;
+
+public abstract class PluginRazorPageBase<TModel> : AbpRazorPage<TModel>
+{
+    [RazorInject] public IAbpSession AbpSession { get; set; }
+}
