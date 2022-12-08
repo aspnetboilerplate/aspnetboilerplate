a1e276f1a58b2178272ea4b1a08ec703f96aee93
İsmail ÇAĞDAŞ <ismailcagdas@gmail.com>
2022-12-08 14:26:23 +0300

    complete .NET 7 upgrade

--- test/Abp.TestBase.SampleApplication/Messages/GetMessagesWithFilterInput.cs
+++
@@ -1,9 +0,0 @@
-using Abp.Application.Services.Dto;
-
-namespace Abp.TestBase.SampleApplication.Messages
-{
-    public class GetMessagesWithFilterInput : PagedAndSortedResultRequestDto
-    {
-        public string Text { get; set; }
-    }
-}
--- test/Abp.TestBase.SampleApplication/Messages/IAsyncMessageAppService.cs
+++
@@ -1,9 +0,0 @@
-using Abp.Application.Services;
-
-namespace Abp.TestBase.SampleApplication.Messages
-{
-    public interface IAsyncMessageAppService : IAsyncCrudAppService<MessageDto>
-    {
-        
-    }
-}
