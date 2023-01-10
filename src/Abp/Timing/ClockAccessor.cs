babc6cba3c679c57665f621f03ea44d987db6a4f
Muhammed Ali ÖZKAYA <m_aliozkaya@hotmail.com>
2023-01-10 08:16:53 +0300

    Update clock accessor

--- src/Abp/Timing/ClockAccessor.cs
+++ src/Abp/Timing/ClockAccessor.cs
@@ -6,20 +6,24 @@ namespace Abp.Timing
     {
         private IClockProvider _provider;
 
-        public DateTime Now => Provider.Now;
-        public DateTimeKind Kind => Provider.Kind;
-        public bool SupportsMultipleTimezone => Provider.SupportsMultipleTimezone;
-
-        public DateTime Normalize(DateTime dateTime)
-        {
-            return Provider.Normalize(dateTime);
-        }
-        
         public IClockProvider Provider
         {
             get => _provider;
             set => _provider = value ?? throw new ArgumentNullException(nameof(value), "Can not set Clock.Provider to null!");
         }
+        
+        public DateTime Now => Provider.Now;
+        public DateTimeKind Kind => Provider.Kind;
+        public bool SupportsMultipleTimezone => Provider.SupportsMultipleTimezone;
 
+        public ClockAccessor()
+        {
+            Provider = ClockProviders.Unspecified;
+        }
+        
+        public DateTime Normalize(DateTime dateTime)
+        {
+            return Provider.Normalize(dateTime);
+        }
     }
 }
--- src/Abp/Timing/IClockAccessor.cs
+++ src/Abp/Timing/IClockAccessor.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Options;
 
 namespace Abp.Timing
 {
-    public interface IClockAccessor
+    public interface IClockAccessor : ISingletonDependency
     {
         DateTime Now { get; }
         DateTimeKind Kind { get; }
