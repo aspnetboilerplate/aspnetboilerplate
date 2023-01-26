ee7d248eebdf43b72958e213f8724315dadf28af
Muhammed Ali ÖZKAYA <m_aliozkaya@hotmail.com>
2023-01-26 15:26:41 +0300

    Add sanitizer test controller

---
+++ test/aspnet-core-demo/AbpAspNetCoreDemo.Tests/Tests/SanitizerControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NuGet.Protocol;
+using Shouldly;
+using Xunit;
+
+namespace AbpAspNetCoreDemo.IntegrationTests.Tests;
+
+public class SanitizerControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+{
+    private readonly WebApplicationFactory<Startup> _factory;
+    private const string BaseUrl = "/SanitizerTest/";
+
+    public SanitizerControllerTests(WebApplicationFactory<Startup> factory)
+    {
+        _factory = factory;
+    }
+    
+    [Theory]
+    [InlineData("<script>alert('hello')</script>", "alert('hello')")]
+    public async Task Sanitizer_Post_Test(string text, string expectedText)
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.PostAsync(BaseUrl + "SanitizeHtml", new FormUrlEncodedContent(new[]
+        {
+            new KeyValuePair<string, string>("html", text)
+        }));
+
+        // Assert
+
+        var result = await response.Content.ReadAsStringAsync();
+        var jsonResult = JObject.Parse(result);
+        jsonResult["result"].ShouldBe(expectedText);
+    }
+}
---
+++ test/aspnet-core-demo/AbpAspNetCoreDemo/Controllers/SanitizerTestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AbpAspNetCoreDemo.Controllers;
+
+public class SanitizerTestController : DemoControllerBase
+{
+    [HttpPost("sanitizerTest/sanitizeHtml")]
+    public string SanitizeHtml(string html)
+    {
+        return html;
+    }
+    
+}
