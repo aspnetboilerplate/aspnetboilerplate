dbe129443f1be92ec1f2b0c3e634709237d6f0cb
Muhammed Ali ÖZKAYA <m_aliozkaya@hotmail.com>
2023-02-06 13:54:54 +0300

    Update attribute description

--- src/Abp.HtmlSanitizer/HtmlSanitizer/SanitizeHtmlAttribute.cs
+++ src/Abp.HtmlSanitizer/HtmlSanitizer/SanitizeHtmlAttribute.cs
@@ -3,13 +3,13 @@
 namespace Abp.HtmlSanitizer.HtmlSanitizer
 {
     /// <summary>
-    /// Can be added to a method to enable auto validation if validation is disabled for it's class.
+    /// Can be added to a method to enable auto sanitize html.
     /// </summary>
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Parameter, AllowMultiple = true)]
     public class SanitizeHtmlAttribute : Attribute
     {
         public bool KeepChildNodes { get; set; }
-
+        
         public bool IsDisabled { get; set; }
     }
 }
