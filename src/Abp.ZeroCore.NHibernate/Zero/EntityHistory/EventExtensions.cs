329f71e980ebaff13f72bcfb7276de4a76b110c4
Bartłomiej Wójcik <bwojcik@enovatio.com>
2023-05-05 09:12:16 +0200

    handle soft delete -> delete state in entity history

--- src/Abp.ZeroCore.NHibernate/Zero/EntityHistory/EventExtensions.cs
+++ src/Abp.ZeroCore.NHibernate/Zero/EntityHistory/EventExtensions.cs
@@ -11,15 +11,15 @@ public static class EventExtensions
 
         foreach (var dirtyFieldIndex in dirtyFieldIndexes)
         {
-            var newValue = @event.State[dirtyFieldIndex]?.ToString() ?? string.Empty;
-            var oldValue = @event.OldState[dirtyFieldIndex]?.ToString() ?? string.Empty;
-
             var dirtyFieldProperty = @event.Persister.EntityMetamodel.Type.GetProperties()
                 .FirstOrDefault(p => p.Name == @event.Persister.PropertyNames[dirtyFieldIndex]);
 
             if (dirtyFieldProperty != null && dirtyFieldProperty.Name == "IsDeleted")
             {
-                if (oldValue != newValue && newValue == "1")
+                var newValue = @event.State[dirtyFieldIndex]?.ToString() ?? string.Empty;
+                var oldValue = @event.OldState[dirtyFieldIndex]?.ToString() ?? string.Empty;
+
+                if (oldValue.ToLowerInvariant() != newValue.ToLowerInvariant() && newValue.ToLowerInvariant() == bool.FalseString.ToLowerInvariant())
                 {
                     return true;
                 }
