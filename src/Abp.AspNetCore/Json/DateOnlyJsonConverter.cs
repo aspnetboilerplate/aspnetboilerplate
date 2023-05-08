8a7c18456d4bc51ee5fe3e39f48f2057e2c536e7
İsmail ÇAĞDAŞ <ismailcagdas@gmail.com>
2023-05-08 13:56:44 +0300

    minor code refactor on DateOnlyJsonConverter

--- src/Abp.AspNetCore/Json/DateOnlyJsonConverter.cs
+++ src/Abp.AspNetCore/Json/DateOnlyJsonConverter.cs
@@ -1,45 +1,49 @@
 ﻿using Newtonsoft.Json;
 using System;
 
-namespace Abp.Json;
-
-public class DateOnlyJsonConverter : JsonConverter<DateOnly>
+namespace Abp.Json
 {
-    public override void WriteJson(JsonWriter writer, DateOnly value, JsonSerializer serializer)
+    public class DateOnlyJsonConverter : JsonConverter<DateOnly>
     {
-        writer.WriteValue(value.ToString("O"));
-    }
-
-    public override DateOnly ReadJson(JsonReader reader, Type objectType, DateOnly existingValue, bool hasExistingValue, JsonSerializer serializer)
-    {
-        var v = reader.Value;
-        if (v == null)
+        public override void WriteJson(JsonWriter writer, DateOnly value, JsonSerializer serializer)
         {
-            return DateOnly.MinValue;
+            writer.WriteValue(value.ToString("O"));
         }
 
-        var vType = v.GetType();
-        if (vType == typeof(DateTimeOffset)) //when the object is from a property 
-                                             //in POST body. When used in service, 
-                                             //better to have 
-                                             //options.SerializerSettings.DateParseHandling = 
-                                             //Newtonsoft.Json.DateParseHandling.DateTimeOffset;
+        public override DateOnly ReadJson(
+            JsonReader reader,
+            Type objectType,
+            DateOnly existingValue,
+            bool hasExistingValue,
+            JsonSerializer serializer)
         {
-            return DateOnly.FromDateTime(((DateTimeOffset)v).DateTime);
-        }
+            if (reader.Value == null)
+            {
+                return DateOnly.MinValue;
+            }
 
-        if (vType == typeof(string))
-        {
-            return DateOnly.Parse((string)v); //DateOnly can parse 00001-01-01
-        }
+            var dateValueType = reader.Value.GetType();
+            if (dateValueType == typeof(DateTimeOffset)) //when the object is from a property 
+                //in POST body. When used in service, 
+                //better to have 
+                //options.SerializerSettings.DateParseHandling = 
+                //Newtonsoft.Json.DateParseHandling.DateTimeOffset;
+            {
+                return DateOnly.FromDateTime(((DateTimeOffset) reader.Value).DateTime);
+            }
 
-        if (vType == typeof(DateTime)) //when the object is from a property 
-                                       //in POST body from a TS client
-        {
-            return DateOnly.FromDateTime((DateTime)v);
-        }
+            if (dateValueType == typeof(string))
+            {
+                return DateOnly.Parse((string) reader.Value); // DateOnly can parse 00001-01-01
+            }
 
-        throw new NotSupportedException
-            ($"Not yet support {vType} in {this.GetType()}.");
+            if (dateValueType == typeof(DateTime)) // when the object is from a property 
+                //in POST body from a TS client
+            {
+                return DateOnly.FromDateTime((DateTime) reader.Value);
+            }
+
+            throw new NotSupportedException($"Not yet support {dateValueType} in {this.GetType()}.");
+        }
     }
 }
