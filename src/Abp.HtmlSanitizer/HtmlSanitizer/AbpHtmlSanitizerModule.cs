5e7e5d29f14dc723297db1a1e7e35d73869cf06b
İsmail ÇAĞDAŞ <ismailcagdas@gmail.com>
2023-05-10 16:07:53 +0300

    move IHtmlSanitizer registration to PreInitialize of AbpHtmlSanitizerModule

--- src/Abp.HtmlSanitizer/HtmlSanitizer/AbpHtmlSanitizerModule.cs
+++ src/Abp.HtmlSanitizer/HtmlSanitizer/AbpHtmlSanitizerModule.cs
@@ -12,12 +12,12 @@ namespace Abp.HtmlSanitizer
         public override void PreInitialize()
         {
             IocManager.Register<IHtmlSanitizerConfiguration, HtmlSanitizerConfiguration>();
+            IocManager.Register<IHtmlSanitizer, Ganss.Xss.HtmlSanitizer>(DependencyLifeStyle.Transient);
         }
 
         public override void Initialize()
         {
             IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
-            IocManager.Register<IHtmlSanitizer, Ganss.Xss.HtmlSanitizer>(DependencyLifeStyle.Transient);
         }
     }
 }
