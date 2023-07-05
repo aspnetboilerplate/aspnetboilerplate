0da84e8c4813560160d982809aebc99e09cdbdf7
netty2019 <47128297+netty2019@users.noreply.github.com>
2023-07-05 11:51:04 +0800

    Update EventBus.cs
    TriggerAsync adds the same processing to prevent Thread safety problems

--- src/Abp/Events/Bus/EventBus.cs
+++ src/Abp/Events/Bus/EventBus.cs
@@ -312,7 +312,7 @@ namespace Abp.Events.Bus
 
             foreach (var handlerFactories in GetHandlerFactories(eventType))
             {
-                foreach (var handlerFactory in handlerFactories.EventHandlerFactories)
+                foreach (var handlerFactory in handlerFactories.EventHandlerFactories.ToList())
                 {
                     var handlerType = handlerFactory.GetHandlerType();
 
