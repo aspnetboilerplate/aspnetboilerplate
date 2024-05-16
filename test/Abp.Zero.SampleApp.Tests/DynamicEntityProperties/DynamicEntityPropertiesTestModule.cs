5632fb1e92e17605b9cce9423dbebce1277e7091
Oğuzhan Ağır <90545225+oguzhanagir@users.noreply.github.com>
2024-05-16 15:35:12 +0300

    added ICancellationTokenProvider to the dependencies of the test module

--- test/Abp.Zero.SampleApp.Tests/DynamicEntityProperties/DynamicEntityPropertiesTestModule.cs
+++ test/Abp.Zero.SampleApp.Tests/DynamicEntityProperties/DynamicEntityPropertiesTestModule.cs
@@ -1,4 +1,6 @@
 ﻿using Abp.Modules;
+using Abp.Threading;
+using Castle.MicroKernel.Registration;
 
 namespace Abp.Zero.SampleApp.Tests.DynamicEntityProperties
 {
@@ -8,6 +10,11 @@ namespace Abp.Zero.SampleApp.Tests.DynamicEntityProperties
         public override void PreInitialize()
         {
             IocManager.RegisterAssemblyByConvention(typeof(DynamicEntityPropertiesTestModule).Assembly);
+
+            IocManager.IocContainer.Register(
+                Component.For<ICancellationTokenProvider>().Instance(NullCancellationTokenProvider.Instance)
+                    .LifestyleSingleton()
+            );
 
             Configuration.Authorization.Providers.Add<DynamicEntityPropertiesTestAuthorizationProvider>();
             Configuration.DynamicEntityProperties.Providers.Add<MyDynamicEntityPropertyDefinitionProvider>();
