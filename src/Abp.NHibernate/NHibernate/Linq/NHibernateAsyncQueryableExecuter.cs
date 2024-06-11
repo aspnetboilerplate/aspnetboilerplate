7bfc3b5614a0a4e5416428421f4fb52c17a3754e
İsmail ÇAĞDAŞ <ismailcagdas@gmail.com>
2024-06-11 15:10:48 +0300

    Update branch from dev

--- src/Abp.NHibernate/NHibernate/Linq/NHibernateAsyncQueryableExecuter.cs
+++ src/Abp.NHibernate/NHibernate/Linq/NHibernateAsyncQueryableExecuter.cs
@@ -3,30 +3,31 @@ using Abp.Linq;
 using NHibernate.Linq;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Abp.NHibernate.Linq
 {
     public class NHibernateAsyncQueryableExecuter : IAsyncQueryableExecuter, ISingletonDependency
     {
-        public Task<int> CountAsync<T>(IQueryable<T> queryable)
+        public Task<int> CountAsync<T>(IQueryable<T> queryable, CancellationToken cancellationToken = default)
         {
-            return queryable.CountAsync();
+            return queryable.CountAsync(cancellationToken);
         }
 
-        public Task<List<T>> ToListAsync<T>(IQueryable<T> queryable)
+        public Task<List<T>> ToListAsync<T>(IQueryable<T> queryable, CancellationToken cancellationToken = default)
         {
-            return queryable.ToListAsync();
+            return queryable.ToListAsync(cancellationToken);
         }
 
-        public Task<T> FirstOrDefaultAsync<T>(IQueryable<T> queryable)
+        public Task<T> FirstOrDefaultAsync<T>(IQueryable<T> queryable, CancellationToken cancellationToken = default)
         {
-            return queryable.FirstOrDefaultAsync();
+            return queryable.FirstOrDefaultAsync(cancellationToken);
         }
 
-        public Task<bool> AnyAsync<T>(IQueryable<T> queryable)
+        public Task<bool> AnyAsync<T>(IQueryable<T> queryable, CancellationToken cancellationToken = default)
         {
-            return queryable.AnyAsync();
+            return queryable.AnyAsync(cancellationToken);
         }
     }
 }
