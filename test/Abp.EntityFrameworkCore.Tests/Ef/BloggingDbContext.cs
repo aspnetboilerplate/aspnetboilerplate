efcb0d1cde8910a5ad852a550b30b0bf711aa0a6
Amos <hzh990094740@outlook.com>
2024-06-11 22:02:12 +0800

    Update BloggingDbContext.cs

--- test/Abp.EntityFrameworkCore.Tests/Ef/BloggingDbContext.cs
+++ test/Abp.EntityFrameworkCore.Tests/Ef/BloggingDbContext.cs
@@ -21,7 +21,7 @@ namespace Abp.EntityFrameworkCore.Tests.Ef
         public BloggingDbContext(DbContextOptions<BloggingDbContext> options)
             : base(options)
         {
-            
+            CurrentUnitOfWorkProvider=new AsyncLocalCurrentUnitOfWorkProvider();
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
