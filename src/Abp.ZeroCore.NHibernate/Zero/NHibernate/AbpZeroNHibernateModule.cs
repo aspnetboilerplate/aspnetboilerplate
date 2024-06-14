99eb6759c5d5d4bce01fdaee1dbbcdac05b4b1aa
İsmail ÇAĞDAŞ <ismailcagdas@gmail.com>
2024-06-14 11:09:48 +0300

    wrap AbpZeroCoreNHibernateModule  into its namespace

--- src/Abp.ZeroCore.NHibernate/Zero/NHibernate/AbpZeroNHibernateModule.cs
+++ src/Abp.ZeroCore.NHibernate/Zero/NHibernate/AbpZeroNHibernateModule.cs
@@ -10,49 +10,50 @@ using Castle.MicroKernel.Registration;
 using NHibernate.Event;
 using System.Reflection;
 
-namespace Abp.Zero.NHibernate;
-
-/// <summary>
-/// Startup class for ABP Zero NHibernate module.
-/// </summary>
-[DependsOn(typeof(AbpZeroCoreModule), typeof(AbpNHibernateModule))]
-public class AbpZeroCoreNHibernateModule : AbpModule
+namespace Abp.Zero.NHibernate
 {
-    public override void PreInitialize()
+    /// <summary>
+    /// Startup class for ABP Zero NHibernate module.
+    /// </summary>
+    [DependsOn(typeof(AbpZeroCoreModule), typeof(AbpNHibernateModule))]
+    public class AbpZeroCoreNHibernateModule : AbpModule
     {
-        Configuration.ReplaceService<IEntityHistoryStore, NhEntityHistoryStore>(DependencyLifeStyle.Transient);
-
-        IocManager.Register<IEntityHistoryHelper, NhEntityHistoryHelper>(DependencyLifeStyle.Transient);
-        IocManager.Register<IPreInsertEventListener, InsertEventListener>(DependencyLifeStyle.Transient);
-        IocManager.Register<IPreUpdateEventListener, UpdateEventListener>(DependencyLifeStyle.Transient);
-        IocManager.Register<IPreDeleteEventListener, DeleteEventListener>(DependencyLifeStyle.Transient);
-        IocManager.Register<IFlushEventListener, FlushEventListener>(DependencyLifeStyle.Transient);
-
-        Configuration.Modules.AbpNHibernate().FluentConfiguration
-            .Mappings(
-                m => m.FluentMappings.AddFromAssemblyOf<AbpZeroCoreNHibernateModule>()
-            )
-            .ExposeConfiguration(c =>
-            {
-                c.AppendListeners(ListenerType.PreInsert, new[] { IocManager.Resolve<IPreInsertEventListener>() });
-                c.AppendListeners(ListenerType.PreUpdate, new[] { IocManager.Resolve<IPreUpdateEventListener>() });
-                c.AppendListeners(ListenerType.PreDelete, new[] { IocManager.Resolve<IPreDeleteEventListener>() });
-                c.AppendListeners(ListenerType.Flush, new[] { IocManager.Resolve<IFlushEventListener>() });
-            });
-
-
-        Configuration.ReplaceService(typeof(IConnectionStringResolver), () =>
+        public override void PreInitialize()
         {
-            IocManager.IocContainer.Register(
-                Component.For<IConnectionStringResolver, IDbPerTenantConnectionStringResolver>()
-                    .ImplementedBy<DbPerTenantConnectionStringResolver>()
-                    .LifestyleTransient()
-            );
-        });
-    }
+            Configuration.ReplaceService<IEntityHistoryStore, NhEntityHistoryStore>(DependencyLifeStyle.Transient);
 
-    public override void Initialize()
-    {
-        IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+            IocManager.Register<IEntityHistoryHelper, NhEntityHistoryHelper>(DependencyLifeStyle.Transient);
+            IocManager.Register<IPreInsertEventListener, InsertEventListener>(DependencyLifeStyle.Transient);
+            IocManager.Register<IPreUpdateEventListener, UpdateEventListener>(DependencyLifeStyle.Transient);
+            IocManager.Register<IPreDeleteEventListener, DeleteEventListener>(DependencyLifeStyle.Transient);
+            IocManager.Register<IFlushEventListener, FlushEventListener>(DependencyLifeStyle.Transient);
+
+            Configuration.Modules.AbpNHibernate().FluentConfiguration
+                .Mappings(
+                    m => m.FluentMappings.AddFromAssemblyOf<AbpZeroCoreNHibernateModule>()
+                )
+                .ExposeConfiguration(c =>
+                {
+                    c.AppendListeners(ListenerType.PreInsert, new[] {IocManager.Resolve<IPreInsertEventListener>()});
+                    c.AppendListeners(ListenerType.PreUpdate, new[] {IocManager.Resolve<IPreUpdateEventListener>()});
+                    c.AppendListeners(ListenerType.PreDelete, new[] {IocManager.Resolve<IPreDeleteEventListener>()});
+                    c.AppendListeners(ListenerType.Flush, new[] {IocManager.Resolve<IFlushEventListener>()});
+                });
+
+
+            Configuration.ReplaceService(typeof(IConnectionStringResolver), () =>
+            {
+                IocManager.IocContainer.Register(
+                    Component.For<IConnectionStringResolver, IDbPerTenantConnectionStringResolver>()
+                        .ImplementedBy<DbPerTenantConnectionStringResolver>()
+                        .LifestyleTransient()
+                );
+            });
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+        }
     }
 }
