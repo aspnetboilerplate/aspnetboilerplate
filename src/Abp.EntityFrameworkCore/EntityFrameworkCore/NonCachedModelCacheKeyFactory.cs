4f390ad4355234f5ee95dd573b8c3348619b58fe
maliming <malimings@gmail.com>
2024-06-16 16:26:18 +0800

    Update NonCachedModelCacheKeyFactory.cs

--- src/Abp.EntityFrameworkCore/EntityFrameworkCore/NonCachedModelCacheKeyFactory.cs
+++ src/Abp.EntityFrameworkCore/EntityFrameworkCore/NonCachedModelCacheKeyFactory.cs
@@ -5,7 +5,6 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 namespace Abp.EntityFrameworkCore;
 
 /// <summary>
-/// Avoid unit test caching the configure of the entity.
 /// OnModelCreating will be executed multiple times
 /// </summary>
 public class NonCachedModelCacheKeyFactory : IModelCacheKeyFactory
