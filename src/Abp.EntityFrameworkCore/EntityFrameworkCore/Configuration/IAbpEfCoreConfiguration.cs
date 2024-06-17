1960d3b3406622b744f81c636717a0e5283b1d09
maliming <malimings@gmail.com>
2024-06-17 13:58:54 +0800

    Update IAbpEfCoreConfiguration.cs

--- src/Abp.EntityFrameworkCore/EntityFrameworkCore/Configuration/IAbpEfCoreConfiguration.cs
+++ src/Abp.EntityFrameworkCore/EntityFrameworkCore/Configuration/IAbpEfCoreConfiguration.cs
@@ -18,7 +18,7 @@ namespace Abp.EntityFrameworkCore.Configuration
         /// </summary>
         public static NullAbpEfCoreConfiguration Instance { get; } = new NullAbpEfCoreConfiguration();
 
-        public bool UseAbpQueryCompiler { get; set; } = true;
+        public bool UseAbpQueryCompiler { get; set; }
 
         public void AddDbContext<TDbContext>(Action<AbpDbContextConfiguration<TDbContext>> action) where TDbContext : DbContext
         {
