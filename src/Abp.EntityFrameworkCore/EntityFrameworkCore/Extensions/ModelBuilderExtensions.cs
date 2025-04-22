973374ba11209746e824b211daa7f0cfbeab06e9
Oğuzhan Ağır <90545225+oguzhanagir@users.noreply.github.com>
2025-04-22 15:57:58 +0300

    Refactor Db function to use SqlConstantExpression for better type compatibility

--- src/Abp.EntityFrameworkCore/EntityFrameworkCore/Extensions/ModelBuilderExtensions.cs
+++ src/Abp.EntityFrameworkCore/EntityFrameworkCore/Extensions/ModelBuilderExtensions.cs
@@ -12,7 +12,6 @@ public static class ModelBuilderExtensions
         modelBuilder.HasDbFunction(methodInfo)
             .HasTranslation(args =>
             {
-                // (bool isDeleted, bool boolParam)
                 var isDeleted = args[0];
                 var boolParam = args[1];
 
@@ -22,13 +21,18 @@ public static class ModelBuilderExtensions
                     return new SqlBinaryExpression(
                         ExpressionType.Equal,
                         isDeleted,
-                        new SqlFragmentExpression("0"),
-                        boolParam.Type,
-                        boolParam.TypeMapping);
+                        new SqlConstantExpression(false, isDeleted.TypeMapping),
+                        isDeleted.Type,
+                        isDeleted.TypeMapping);
                 }
 
                 // empty where sql
-                return new SqlFragmentExpression("1=1");
+                return new SqlBinaryExpression(
+                    ExpressionType.Equal,
+                    new SqlConstantExpression(true, boolParam.TypeMapping),
+                    new SqlConstantExpression(true, boolParam.TypeMapping),
+                    boolParam.Type,
+                    boolParam.TypeMapping);
             });
 
         return modelBuilder;
@@ -39,24 +43,26 @@ public static class ModelBuilderExtensions
         modelBuilder.HasDbFunction(methodInfo)
             .HasTranslation(args =>
             {
-                // (int? tenantId, int? currentTenantId, bool boolParam)
                 var tenantId = args[0];
                 var currentTenantId = args[1];
                 var boolParam = args[2];
 
                 if (abpEfCoreCurrentDbContext.Context?.IsMayHaveTenantFilterEnabled == true)
                 {
-                    // TenantId == CurrentTenantId
                     return new SqlBinaryExpression(
                         ExpressionType.Equal,
                         tenantId,
                         currentTenantId,
-                        boolParam.Type,
-                        boolParam.TypeMapping);
+                        tenantId.Type,
+                        tenantId.TypeMapping);
                 }
 
-                // empty where sql
-                return new SqlFragmentExpression("1=1");
+                return new SqlBinaryExpression(
+                    ExpressionType.Equal,
+                    new SqlConstantExpression(true, boolParam.TypeMapping),
+                    new SqlConstantExpression(true, boolParam.TypeMapping),
+                    boolParam.Type,
+                    boolParam.TypeMapping);
             });
 
         return modelBuilder;
@@ -67,24 +73,26 @@ public static class ModelBuilderExtensions
         modelBuilder.HasDbFunction(methodInfo)
             .HasTranslation(args =>
             {
-                // (int tenantId, int? currentTenantId, bool boolParam)
                 var tenantId = args[0];
                 var currentTenantId = args[1];
                 var boolParam = args[2];
 
                 if (abpEfCoreCurrentDbContext.Context?.IsMustHaveTenantFilterEnabled == true)
                 {
-                    // TenantId == CurrentTenantId
                     return new SqlBinaryExpression(
                         ExpressionType.Equal,
                         tenantId,
                         currentTenantId,
-                        boolParam.Type,
-                        boolParam.TypeMapping);
+                        tenantId.Type,
+                        tenantId.TypeMapping);
                 }
 
-                // empty where sql
-                return new SqlFragmentExpression("1=1");
+                return new SqlBinaryExpression(
+                    ExpressionType.Equal,
+                    new SqlConstantExpression(true, boolParam.TypeMapping),
+                    new SqlConstantExpression(true, boolParam.TypeMapping),
+                    boolParam.Type,
+                    boolParam.TypeMapping);
             });
 
         return modelBuilder;
